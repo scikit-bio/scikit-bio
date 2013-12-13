d880b070a3fba2b1458079a7d892a8f3665451af
Greg Caporaso <gregcaporaso@gmail.com>
2013-12-13 09:39:02 -0700

    stubbed out __init__ files

---
+++ bipy/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, The BiPy Developers.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+# credits are (name, GitHub id) tuples
+__credits__ = [("Greg Caporaso", "@gregcaporaso"), 
+               ("Rob Knight", "@rob-knight"), 
+               ("Daniel McDonald", "@wasade"),
+               ("Jai Ram Rideout", "@jrrideout"), 
+               ("Antiono Gonzalez", "@antgonza"),
+               ("Yoshiki Vázquez Baeza", "@ElDeveloper"),
+               ("Emily TerAvest", "@teravest")]
+__version__ = "0.0.0-dev"
