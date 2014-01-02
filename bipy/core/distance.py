7bb4ae3607ddc59d807d14678b8c9d1c5760b7cb
Jai Ram Rideout <jai.rideout@gmail.com>
2014-01-02 14:40:47 -0500

    Add more efficient parser (np.loadtxt hogs memory)

--- bipy/core/distance.py
+++ bipy/core/distance.py
@@ -12,7 +12,9 @@ from __future__ import division
 import csv
 from itertools import izip
 from StringIO import StringIO
+
 import numpy as np
+from scipy.spatial.distance import squareform
 
 class MissingHeaderError(Exception):
     pass
@@ -21,10 +23,47 @@ class DistanceMatrix(object):
 
     @classmethod
     def from_file(cls, dm_f, delimiter='\t'):
-        sample_ids = cls._extract_sample_ids(dm_f, delimiter)
-        data = np.loadtxt(dm_f, delimiter=delimiter, skiprows=1,
-                          usecols=range(1, len(sample_ids) + 1), ndmin=2)
-        return cls(data, sample_ids)
+        # We aren't using np.loadtxt because it uses *way* too much memory. See
+        # http://mail.scipy.org/pipermail/numpy-tickets/2012-August/006749.html
+        #sample_ids = cls._extract_sample_ids(dm_f, delimiter)
+        #
+        #data = np.loadtxt(dm_f, delimiter=delimiter, skiprows=1,
+        #                  usecols=range(1, len(sample_ids) + 1), ndmin=2)
+        #return cls(data, sample_ids)
+
+        sids = cls._extract_sample_ids(dm_f, delimiter)
+        num_sids = len(sids)
+        data = np.empty((num_sids, num_sids))
+
+        for line_idx, line in enumerate(dm_f):
+            tokens = map(lambda e: e.strip(), line.strip().split(delimiter))
+
+            if line_idx < num_sids:
+                if tokens[0] == sids[line_idx]:
+                    row_data = map(float, tokens[1:])
+
+                    if len(row_data) == num_sids:
+                        data[line_idx, :] = row_data
+                    else:
+                        raise InvalidDistanceMatrixFormatError("The number of "
+                                "values in row number %d doesn't match the "
+                                "number of sample IDs in the header." %
+                                line_idx + 1)
+                else:
+                    raise SampleIdMismatchError("Encountered mismatched "
+                            "sample IDs while parsing the distance matrix "
+                            "file. Please ensure the sample IDs match between "
+                            "the distance matrix header (first row) and the "
+                            "row labels (first column).")
+            else:
+                if ''.join(tokens):
+                    # If it isn't a blank line, raise an error because we
+                    # shouldn't ignore extra data.
+                    raise InvalidDistanceMatrixFormatError("Encountered extra "
+                            "rows without corresponding sample IDs in the "
+                            "header.")
+
+        return cls(data, sids)
 
     def __init__(self, data, sample_ids):
         data = np.asarray(data)
@@ -42,6 +81,13 @@ class DistanceMatrix(object):
     @property
     def shape(self):
         return self.data.shape
+
+    @property
+    def size(self):
+        return self.data.size
+
+    def condensed_data(self):
+        return squareform(self.data, force='tovector')
 
     def __str__(self):
         return '%dx%d distance matrix\nSample IDs: %s\n' % (self.shape[0],
@@ -107,13 +153,13 @@ class DistanceMatrix(object):
                 header_line = line
                 break
 
-        try:
-            dm_f.seek(0)
-        except AttributeError:
-            pass
+        #try:
+        #    dm_f.seek(0)
+        #except AttributeError:
+        #    pass
 
         if header_line is None:
             raise MissingHeaderError("Could not find a valid header line "
                     "containing sample IDs in the distance matrix file.")
         else:
-            return header_line.split(delimiter)
+            return map(lambda e: e.strip(), header_line.split(delimiter))
--- tests/test_core/test_distance.py
+++ tests/test_core/test_distance.py
@@ -9,7 +9,10 @@ from __future__ import division
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+from itertools import izip
 from StringIO import StringIO
+
+import numpy as np
 
 from bipy.core.distance import DistanceMatrix
 from bipy.util.unit_test import TestCase, main
@@ -22,36 +25,77 @@ class DistanceMatrixTests(TestCase):
         self.dm_1x1_f = StringIO(DM_1x1_F)
         self.dm_1x1 = DistanceMatrix([[0.0]], ['a'])
 
+        self.dm_2x2_f = StringIO(DM_2x2_F)
+        self.dm_2x2 = DistanceMatrix([[0.0, 0.123], [0.123, 0.0]], ['a', 'b'])
+
+        self.dm_strs = [DM_1x1_F, DM_2x2_F]
+        self.dm_fs = [self.dm_1x1_f, self.dm_2x2_f]
+        self.dms = [self.dm_1x1, self.dm_2x2]
+        self.dm_shapes = [(1, 1), (2, 2)]
+        self.dm_sizes = [1, 4]
+        self.dm_condensed_data = [np.array([]), np.array([0.123])]
+
     def test_round_trip_read_write(self):
         """Test reading, writing, and reading again works as expected."""
-        # Read.
-        dm1 = DistanceMatrix.from_file(self.dm_1x1_f)
+        for dm_f in self.dm_fs:
+            # Read.
+            dm1 = DistanceMatrix.from_file(dm_f)
 
-        # Write.
-        out_f = StringIO()
-        dm1.to_file(out_f)
-        out_f.seek(0)
+            # Write.
+            out_f = StringIO()
+            dm1.to_file(out_f)
+            out_f.seek(0)
 
-        # Read.
-        dm2 = DistanceMatrix.from_file(out_f)
-        self.assertEqual(dm1, dm2)
+            # Read.
+            dm2 = DistanceMatrix.from_file(out_f)
+            self.assertEqual(dm1, dm2)
 
     def test_from_file(self):
         """Should parse and return a valid DistanceMatrix given a file."""
-        obs = DistanceMatrix.from_file(self.dm_1x1_f)
-        self.assertEqual(obs, self.dm_1x1)
+        for dm_f, dm in izip(self.dm_fs, self.dms):
+            obs = DistanceMatrix.from_file(dm_f)
+            self.assertEqual(obs, dm)
 
     def test_to_file(self):
         """Should serialize a DistanceMatrix to file."""
-        for memory_efficient in True, False:
-            obs_f = StringIO()
-            self.dm_1x1.to_file(obs_f, memory_efficient=memory_efficient)
-            obs = obs_f.getvalue()
-            obs_f.close()
+        for dm_str, dm in izip(self.dm_strs, self.dms):
+            for memory_efficient in True, False:
+                obs_f = StringIO()
+                dm.to_file(obs_f, memory_efficient=memory_efficient)
+                obs = obs_f.getvalue()
+                obs_f.close()
 
-            self.assertEqual(obs, DM_1x1_F)
+                self.assertEqual(obs, dm_str)
 
+    def test_dtype(self):
+        """Test retrieving dtype of data matrix."""
+        for dm in self.dms:
+            self.assertEqual(dm.dtype, np.float64)
+
+    def test_shape(self):
+        """Test retrieving shape of data matrix."""
+        for dm, shape in izip(self.dms, self.dm_shapes):
+            self.assertEqual(dm.shape, shape)
+
+    def test_size(self):
+        """Test retrieving size of data matrix."""
+        for dm, size in izip(self.dms, self.dm_sizes):
+            self.assertEqual(dm.size, size)
+
+    def test_condensed_data(self):
+        """Test retrieving the data matrix in condensed form."""
+        for dm, condensed in izip(self.dms, self.dm_condensed_data):
+            obs = dm.condensed_data()
+            self.assertTrue(np.array_equal(obs, condensed))
+
+# 1x1:
+#     0.0
 DM_1x1_F = "\ta\na\t0.0\n"
+
+# 2x2:
+#     0.0   0.123
+#     0.123 0.0
+DM_2x2_F = "\ta\tb\na\t0.0\t0.123\nb\t0.123\t0.0\n"
 
 if __name__ == '__main__':
     main()
