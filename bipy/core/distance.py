10028b4d50b8fb5a5f03032e8f4611ff32cde0e8
Jai Ram Rideout <jai.rideout@gmail.com>
2014-01-03 13:08:09 -0500

    Add several more methods/properties
    copy, __ne__, sample_ids, data, T, transpose, and unit tests

--- bipy/core/distance.py
+++ bipy/core/distance.py
@@ -10,30 +10,39 @@ from __future__ import division
 #-----------------------------------------------------------------------------
 
 import csv
+from copy import deepcopy
 from itertools import izip
 from StringIO import StringIO
 
 import numpy as np
-from scipy.spatial.distance import squareform
+from scipy.spatial.distance import is_valid_dm, squareform
 
 class DistanceMatrixError(Exception):
+    pass
+
+class MissingSampleIDError(Exception):
     pass
 
 class DistanceMatrixFormatError(Exception):
     pass
 
 class SampleIDMismatchError(Exception):
-    pass
+    def __init__(self):
+        self.args = ("Encountered mismatched sample IDs while parsing the "
+                     "distance matrix file. Please ensure that the sample IDs "
+                     "match between the distance matrix header (first row) "
+                     "and the row labels (first column).",)
 
 class MissingHeaderError(Exception):
-    pass
+    def __init__(self):
+        self.args = ("Could not find a header line containing sample IDs in "
+                     "the distance matrix file. Please verify that the file "
+                     "is not empty.",)
 
 class DistanceMatrix(object):
 
     @classmethod
     def from_file(cls, dm_f, delimiter='\t'):
-        sids = None
-
         # We aren't using np.loadtxt because it uses *way* too much memory
         # (e.g, a 2GB matrix eats up 10GB, which then isn't freed after parsing
         # has finished). See:
@@ -47,6 +56,7 @@ class DistanceMatrix(object):
         #         - fill in only lower-triangular values (exclude the diagonal)
         #         - populate the corresponding row in the ndarray
         #     - fill in the upper triangle based on the lower triangle
+        sids = None
         for line_idx, line in enumerate(dm_f):
             tokens = map(lambda e: e.strip(), line.strip().split(delimiter))
 
@@ -71,36 +81,44 @@ class DistanceMatrix(object):
 
                     data[row_idx,:] = row_data
                 else:
-                    raise SampleIDMismatchError("Encountered mismatched "
-                            "sample IDs while parsing the distance matrix "
-                            "file. Please ensure the sample IDs match between "
-                            "the distance matrix header (first row) and the "
-                            "row labels (first column).")
+                    raise SampleIDMismatchError
             else:
                 if ''.join(tokens):
                     # If it isn't a blank line, raise an error because we
                     # shouldn't ignore extra data.
-                    raise InvalidDistanceMatrixFormatError("Encountered extra "
-                            "rows without corresponding sample IDs in the "
-                            "header.")
+                    raise DistanceMatrixFormatError("Encountered extra rows "
+                            "without corresponding sample IDs in the header.")
 
         if sids is None:
-            raise MissingHeaderError("Could not find a header line containing "
-                    "sample IDs in the distance matrix file. Is the file "
-                    "empty?")
+            raise MissingHeaderError
 
         data += data.T
 
         return cls(data, sids)
 
     def __init__(self, data, sample_ids):
-        data = np.asarray(data)
+        data = np.asarray(data, dtype='float')
         sample_ids = tuple(sample_ids)
         self._validate(data, sample_ids)
 
-        self.data = data
-        self.sample_ids = sample_ids
-        self._sample_index = self._index_list(self.sample_ids)
+        self._data = data
+        self._sample_ids = sample_ids
+        self._sample_index = self._index_list(self._sample_ids)
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def sample_ids(self):
+        return self._sample_ids
+
+    @sample_ids.setter
+    def sample_ids(self, sample_ids_):
+        sample_ids_ = tuple(sample_ids_)
+        self._validate(self.data, sample_ids_)
+        self._sample_ids = sample_ids_
+        self._sample_index = self._index_list(self._sample_ids)
 
     @property
     def dtype(self):
@@ -111,14 +129,31 @@ class DistanceMatrix(object):
         return self.data.shape
 
     @property
+    def num_samples(self):
+        """Returns the number of samples (i.e. number of rows or columns)."""
+        return len(self.sample_ids)
+
+    @property
     def size(self):
         return self.data.size
+
+    @property
+    def T(self):
+        return self.transpose()
+
+    def transpose(self):
+        return self
 
     def condensed_form(self):
         return squareform(self.data, force='tovector')
 
     def redundant_form(self):
         return self.data
+
+    def copy(self):
+        # We deepcopy sample IDs in case the tuple contains mutable objects at
+        # some point in the future.
+        return self.__class__(self.data.copy(), deepcopy(self.sample_ids))
 
     def __str__(self):
         return '%dx%d distance matrix\nSample IDs:\n%s\nData:\n' % (
@@ -145,6 +180,16 @@ class DistanceMatrix(object):
 
         return eq
 
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __getitem__(self, sample_id):
+        if sample_id in self._sample_index:
+            return self.data[self._sample_index[sample_id]]
+        else:
+            raise MissingSampleIDError("The sample ID '%s' is not in the "
+                                       "distance matrix." % sample_id)
+
     def to_file(self, out_f, delimiter='\t', conserve_memory=False):
         formatted_sids = self._format_sample_ids(delimiter)
         out_f.write(formatted_sids)
@@ -166,7 +211,20 @@ class DistanceMatrix(object):
             temp_f.close()
 
     def _validate(self, data, sample_ids):
-        pass
+        # Accepts arguments instead of inspecting instance attributes because
+        # we don't want to create an invalid distance matrix before raising an
+        # error (because then it could be used after the exception is caught).
+        num_sids = len(sample_ids)
+
+        if num_sids != len(set(sample_ids)):
+            raise DistanceMatrixError("Sample IDs must be unique.")
+        elif not is_valid_dm(data):
+            raise DistanceMatrixError("Data must be an array that is "
+                    "2-dimensional, square, symmetric, hollow, and contains "
+                    "only floating point values.")
+        elif num_sids != data.shape[0]:
+            raise DistanceMatrixError("The number of sample IDs must match "
+                    "the number of rows/columns in the data.")
 
     def _index_list(self, l):
         return dict([(id_, idx) for idx, id_ in enumerate(l)])
--- tests/test_core/test_distance.py
+++ tests/test_core/test_distance.py
@@ -14,7 +14,8 @@ from StringIO import StringIO
 
 import numpy as np
 
-from bipy.core.distance import DistanceMatrix
+from bipy.core.distance import (DistanceMatrix, DistanceMatrixError,
+                                MissingSampleIDError)
 from bipy.util.unit_test import TestCase, main
 
 class DistanceMatrixTests(TestCase):
@@ -34,7 +35,7 @@ class DistanceMatrixTests(TestCase):
         self.dm_3x3_f = StringIO(DM_3x3_F)
         self.dm_3x3 = DistanceMatrix(dm_3x3_data, ['a', 'b', 'c'])
 
-        self.dm_strs = [DM_1x1_F, DM_2x2_F, DM_3x3_F]
+        self.dm_f_lines = [DM_1x1_F, DM_2x2_F, DM_3x3_F]
         self.dm_fs = [self.dm_1x1_f, self.dm_2x2_f, self.dm_3x3_f]
         self.dms = [self.dm_1x1, self.dm_2x2, self.dm_3x3]
         self.dm_shapes = [(1, 1), (2, 2), (3, 3)]
@@ -68,14 +69,82 @@ class DistanceMatrixTests(TestCase):
 
     def test_to_file(self):
         """Should serialize a DistanceMatrix to file."""
-        for dm_str, dm in izip(self.dm_strs, self.dms):
+        for dm_f_line, dm in izip(self.dm_f_lines, self.dms):
             for conserve_memory in True, False:
                 obs_f = StringIO()
                 dm.to_file(obs_f, conserve_memory=conserve_memory)
                 obs = obs_f.getvalue()
                 obs_f.close()
 
-                self.assertEqual(obs, dm_str)
+                self.assertEqual(obs, dm_f_line)
+
+    def test_init_invalid_input(self):
+        """Raises error on invalid distance matrix data / sample IDs."""
+        # Empty data.
+        with self.assertRaises(DistanceMatrixError):
+            _ = DistanceMatrix([], [])
+
+        # Invalid number of dimensions.
+        with self.assertRaises(DistanceMatrixError):
+            _ = DistanceMatrix([1, 2, 3], ['a'])
+
+        # Dimensions don't match.
+        with self.assertRaises(DistanceMatrixError):
+            _ = DistanceMatrix([[1, 2, 3]], ['a'])
+
+        data = [[0, 1], [1, 0]]
+
+        # Duplicate sample IDs.
+        with self.assertRaises(DistanceMatrixError):
+            _ = DistanceMatrix(data, ['a', 'a'])
+
+        # Number of sample IDs don't match dimensions.
+        with self.assertRaises(DistanceMatrixError):
+            _ = DistanceMatrix(data, ['a', 'b', 'c'])
+
+        # Non-hollow.
+        data = [[0.0, 1.0], [1.0, 0.01]]
+        with self.assertRaises(DistanceMatrixError):
+            _ = DistanceMatrix(data, ['a', 'b'])
+
+        # Nonsymmetric.
+        data = [[0.0, 2.0], [1.0, 0.0]]
+        with self.assertRaises(DistanceMatrixError):
+            _ = DistanceMatrix(data, ['a', 'b'])
+
+    def test_data(self):
+        """Test retrieving/setting data matrix."""
+        for dm, exp in izip(self.dms, self.dm_redundant_forms):
+            obs = dm.data
+            self.assertTrue(np.array_equal(obs, exp))
+
+        with self.assertRaises(AttributeError):
+            self.dm_3x3.data = 'foo'
+
+    def test_sample_ids(self):
+        """Test retrieving/setting sample IDs."""
+        obs = self.dm_3x3.sample_ids
+        self.assertEqual(obs, ('a', 'b', 'c'))
+
+        # Test that we overwrite the existing sample IDs and that the sample
+        # index is correctly rebuilt.
+        new_sids = ['foo', 'bar', 'baz']
+        self.dm_3x3.sample_ids = new_sids
+        obs = self.dm_3x3.sample_ids
+        self.assertEqual(obs, tuple(new_sids))
+        self.assertTrue(np.array_equal(self.dm_3x3['bar'],
+                                       np.array([0.01, 0.0, 12.0])))
+        with self.assertRaises(MissingSampleIDError):
+            _ = self.dm_3x3['b']
+
+    def test_sample_ids_invalid_input(self):
+        """Test setting invalid sample IDs raises an error."""
+        with self.assertRaises(DistanceMatrixError):
+            self.dm_3x3.sample_ids = ['foo', 'bar']
+        # Make sure that we can still use the distance matrix after trying to
+        # be evil.
+        obs = self.dm_3x3.sample_ids
+        self.assertEqual(obs, ('a', 'b', 'c'))
 
     def test_dtype(self):
         """Test retrieving dtype of data matrix."""
@@ -87,10 +156,22 @@ class DistanceMatrixTests(TestCase):
         for dm, shape in izip(self.dms, self.dm_shapes):
             self.assertEqual(dm.shape, shape)
 
+    def test_num_samples(self):
+        """Test retrieving the number of samples in the distance matrix."""
+        for dm, shape in izip(self.dms, self.dm_shapes):
+            self.assertEqual(dm.num_samples, shape[0])
+
     def test_size(self):
         """Test retrieving size of data matrix."""
         for dm, size in izip(self.dms, self.dm_sizes):
             self.assertEqual(dm.size, size)
+
+    def test_transpose(self):
+        """Test retrieving transpose of distance matrix."""
+        for dm in self.dms:
+            self.assertEqual(dm.T, dm)
+            self.assertEqual(dm.transpose(), dm)
+            self.assertTrue(dm.transpose() is dm)
 
     def test_condensed_form(self):
         """Test retrieving the data matrix in condensed form."""
@@ -103,6 +184,98 @@ class DistanceMatrixTests(TestCase):
         for dm, redundant in izip(self.dms, self.dm_redundant_forms):
             obs = dm.redundant_form()
             self.assertTrue(np.array_equal(obs, redundant))
+
+    def test_copy(self):
+        """Test correct copying of a DistanceMatrix."""
+        copy = self.dm_2x2.copy()
+        self.assertEqual(copy, self.dm_2x2)
+        self.assertFalse(copy.data is self.dm_2x2.data)
+        # deepcopy doesn't actually create a copy of the IDs because it is a
+        # tuple of strings, which is fully immutable.
+        self.assertTrue(copy.sample_ids is self.dm_2x2.sample_ids)
+
+        new_ids = ['hello', 'world']
+        copy.sample_ids = new_ids
+        self.assertNotEqual(copy.sample_ids, self.dm_2x2.sample_ids)
+
+        copy = self.dm_2x2.copy()
+        copy.data[0,1] = 0.0001
+        self.assertFalse(np.array_equal(copy.data, self.dm_2x2.data))
+
+    def test_str(self):
+        """Test retrieving string representation of a DistanceMatrix."""
+        for dm in self.dms:
+            obs = str(dm)
+            # Do some very light testing here to make sure we're getting a
+            # non-empty string back. We don't want to test the exact
+            # formatting.
+            self.assertTrue(obs)
+
+    def test_eq(self):
+        """Test DistanceMatrix equality test functions correctly."""
+        for dm in self.dms:
+            copy = dm.copy()
+            self.assertTrue(dm == dm)
+            self.assertTrue(copy == copy)
+            self.assertTrue(dm == copy)
+            self.assertTrue(copy == dm)
+
+        self.assertFalse(self.dm_1x1 == self.dm_3x3)
+
+    def test_ne(self):
+        """Test unequal dms are identified as such."""
+        # Wrong class.
+        self.assertTrue(self.dm_3x3 != 'foo')
+
+        # Wrong shape.
+        self.assertTrue(self.dm_3x3 != self.dm_1x1)
+
+        # Wrong sample IDs.
+        other = self.dm_3x3.copy()
+        other.sample_ids = ['foo', 'bar', 'baz']
+        self.assertTrue(self.dm_3x3 != other)
+
+        # Wrong data.
+        other = self.dm_3x3.copy()
+        other.data[1,0] = 42.42
+        self.assertTrue(self.dm_3x3 != other)
+
+        self.assertFalse(self.dm_2x2 != self.dm_2x2)
+
+    def test_getitem(self):
+        """Test retrieving vectors by sample ID."""
+        obs = self.dm_1x1['a']
+        self.assertTrue(np.array_equal(obs, np.array([0.0])))
+
+        obs = self.dm_3x3['c']
+        self.assertTrue(np.array_equal(obs, np.array([4.2, 12.0, 0.0])))
+
+        with self.assertRaises(MissingSampleIDError):
+            _ = self.dm_2x2['c']
+
+    def test_validate(self):
+        """Empty stub: DistanceMatrix._validate already tested elsewhere."""
+        pass
+
+    def test_index_list(self):
+        """Empty stub: DistanceMatrix._index_list already tested elsewhere."""
+        pass
+
+    def test_format_sample_ids(self):
+        """Empty stub: DistanceMatrix._format_sample_ids tested elsewhere."""
+        pass
+
+    def test_pprint_sample_ids(self):
+        """Test pretty-print formatting of sample IDs."""
+        # No truncation.
+        exp = 'a, b, c'
+        obs = self.dm_3x3._pprint_sample_ids()
+        self.assertEqual(obs, exp)
+
+        # Truncation.
+        exp = 'a, b, ...'
+        obs = self.dm_3x3._pprint_sample_ids(max_chars=5)
+        self.assertEqual(obs, exp)
 
 # 1x1:
 #     0.0
