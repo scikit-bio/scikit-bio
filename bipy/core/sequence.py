6c07a667c50a4fb76c004b8c084c5e86ba395e8f
Greg Caporaso <gregcaporaso@gmail.com>
2014-01-23 17:14:56 +0000

    initial methods for base class

--- bipy/core/sequence.py
+++ bipy/core/sequence.py
@@ -13,11 +13,34 @@ from collections import Sequence
 class BiologicalSequence(Sequence):
     """ Base class for biological sequences """
     
-    def __new__(list,sequence):
-        self = sequence
+    def __init__(self, sequence, identifier="", description=""):
+        self._sequence = sequence
+        self._identifier = identifier
+        self._description = description
     
-    def __getitem__(self,i):
-        return self[i]
+    def __getitem__(self, i):
+        return self._sequence[i]
     
     def __len__(self):
-        return len(self)
+        return len(self._sequence)
+
+    def __str__(self):
+        return str(self._sequence)
+
+    def __repr__(self):
+        return str(self)
+
+    def __iter__(self):
+        return iter(self._sequence)
+
+    def toFasta(self, terminal_character="\n"):
+        """ return the sequence as a fasta-formatted string
+          
+            terminal_character: the last character to be included in the
+             string (default: \n (i.e., newline); if you don't want a trailing
+             newline in the string, you can pass terminal_character="")
+        """
+        return '>%s %s\n%s%s' % (self._identifier, self._description, 
+                                 self._sequence, terminal_character)
+
+
