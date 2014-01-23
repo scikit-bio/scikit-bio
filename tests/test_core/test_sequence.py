d92b21d1d0d39b2fe3984b6b3083d64d3f2e4198
Greg Caporaso <gregcaporaso@gmail.com>
2014-01-23 16:30:09 +0000

    added test framework

---
+++ tests/test_core/test_sequence.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, The BiPy Developers.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from unit_test import TestCase, main
+
+from bipy.core.sequence import BiologicalSequence
+
+if __name__ == "__main__":
+    main()
