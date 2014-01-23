33c177325014053cef236686d3aa6a79abbd964d
Greg Caporaso <gregcaporaso@gmail.com>
2014-01-23 18:46:55 +0000

    added basic tests

--- tests/test_core/test_sequence.py
+++ tests/test_core/test_sequence.py
@@ -8,9 +8,48 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-from unit_test import TestCase, main
+from unittest import TestCase, main
 
-from bipy.core.sequence import BiologicalSequence
+from bipy.core.sequence import (
+    BiologicalSequence, NucleotideSequence, DNASequence, RNASequence,
+    DnaSequence, RnaSequence, BiologicalSequenceError)
+
+class BiologicalSequenceTests(TestCase):
+    """ Tests of the BiologicalSequence class """
+
+    def setup(self):
+        """ Initialize values to be used in tests
+        """
+        pass
+
+    def test_init(self):
+        """ Initialization functions as expected with varied input types
+        """
+        # init as string
+        b = BiologicalSequence('ACCGGXZY')
+        self.assertEqual(str(b),'ACCGGXZY') 
+        self.assertEqual(b.Identifier,"")
+        self.assertEqual(b.Description,"")
+        
+        # init as string with optional values
+        b = BiologicalSequence(
+         'ACCGGXZY','test-seq-1','The first test sequence')
+        self.assertEqual(str(b),'ACCGGXZY') 
+        self.assertEqual(b.Identifier,"test-seq-1")
+        self.assertEqual(b.Description,"The first test sequence")
+
+        # init as list
+        b = BiologicalSequence(list('ACCGGXZY'))
+        self.assertEqual(str(b),'ACCGGXZY') 
+        self.assertEqual(b.Identifier,"")
+        self.assertEqual(b.Description,"")
+        
+        # init as tuple
+        b = BiologicalSequence(tuple('ACCGGXZY'))
+        self.assertEqual(str(b),'ACCGGXZY') 
+        self.assertEqual(b.Identifier,"")
+        self.assertEqual(b.Description,"")
+
 
 if __name__ == "__main__":
     main()
