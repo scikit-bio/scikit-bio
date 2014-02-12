1f8452529ca8647bec2db01bc7bc8c2ebd19ff0e
Jorge Cañardo Alastuey <jorgecanardo@gmail.com>
2014-02-12 11:11:32 -0700

    STY: minor changes to make pep8 happy

--- bipy/maths/stats/ordination/test/test_ordination.py
+++ bipy/maths/stats/ordination/test/test_ordination.py
@@ -44,27 +44,27 @@ class TestCAResults(object):
     def test_scaling2(self):
         scores = self.ordination.scores(scaling=2)
         # p. 460 L&L 1998
-        F_hat = np.array([[ 0.40887, -0.06955],
+        F_hat = np.array([[0.40887, -0.06955],
                           [-0.11539,  0.29977],
                           [-0.30997, -0.18739]])
         npt.assert_almost_equal(*normalize_signs(F_hat, scores.species),
                                 decimal=5)
         V_hat = np.array([[-0.84896, -0.88276],
                           [-0.22046,  1.34482],
-                          [ 1.66697, -0.47032]])
+                          [1.66697, -0.47032]])
         npt.assert_almost_equal(*normalize_signs(V_hat, scores.site),
                                 decimal=5)
 
     def test_scaling1(self):
         scores = self.ordination.scores(scaling=1)
         # p. 458
-        V = np.array([[ 1.31871, -0.34374],
+        V = np.array([[1.31871, -0.34374],
                       [-0.37215,  1.48150],
                       [-0.99972, -0.92612]])
         npt.assert_almost_equal(*normalize_signs(V, scores.species), decimal=5)
         F = np.array([[-0.26322, -0.17862],
                       [-0.06835,  0.27211],
-                      [ 0.51685, -0.09517]])
+                      [0.51685, -0.09517]])
         npt.assert_almost_equal(*normalize_signs(F, scores.site), decimal=5)
 
 
@@ -158,18 +158,18 @@ class TestPCoAResultsExtensive(object):
 
     def test_values(self):
         expected = np.array(
-            [[ -2.85970001e-02,   2.29038532e-01,   7.05527166e-02,
-                2.61635763e-01,   2.83986686e-01,  -6.25284356e-09],
-             [  3.74940557e-01,   2.23340550e-01,  -2.08929136e-01,
-                5.05739521e-02,  -1.87103662e-01,  -6.25284356e-09],
-             [ -3.35175925e-01,  -2.38559794e-01,  -3.09988697e-01,
-                1.15217868e-01,  -5.02155335e-02,  -6.25284356e-09],
-             [  2.54123944e-01,  -4.12346397e-01,   2.33436419e-01,
-                6.40316842e-02,  -4.82607521e-03,  -6.25284356e-09],
-             [ -2.82568441e-01,   1.86069108e-01,   2.88756308e-01,
-                -6.45563520e-02,  -2.11416317e-01,  -6.25284356e-09],
-             [  1.72768652e-02,   1.24580018e-02,  -7.38276099e-02,
-                -4.26902915e-01,   1.69574901e-01,  -6.25284356e-09]])
+            [[-2.85970001e-02,   2.29038532e-01,   7.05527166e-02,
+              2.61635763e-01,   2.83986686e-01,  -6.25284356e-09],
+             [3.74940557e-01,   2.23340550e-01,  -2.08929136e-01,
+              5.05739521e-02,  -1.87103662e-01,  -6.25284356e-09],
+             [-3.35175925e-01,  -2.38559794e-01,  -3.09988697e-01,
+              1.15217868e-01,  -5.02155335e-02,  -6.25284356e-09],
+             [2.54123944e-01,  -4.12346397e-01,   2.33436419e-01,
+              6.40316842e-02,  -4.82607521e-03,  -6.25284356e-09],
+             [-2.82568441e-01,   1.86069108e-01,   2.88756308e-01,
+              -6.45563520e-02,  -2.11416317e-01,  -6.25284356e-09],
+             [1.72768652e-02,   1.24580018e-02,  -7.38276099e-02,
+              -4.26902915e-01,   1.69574901e-01,  -6.25284356e-09]])
         actual = self.ordination.scores().species
         npt.assert_almost_equal(*normalize_signs(expected, actual))
 
@@ -181,8 +181,8 @@ class TestPCoAPrivateMethods(object):
 
     def test_E_matrix(self):
         E = PCoA._E_matrix(self.matrix)
-        expected_E = np.array([[ -0.5,  -2. ,  -4.5],
-                               [ -8. , -12.5, -18. ]])
+        expected_E = np.array([[-0.5,  -2.,  -4.5],
+                               [-8., -12.5, -18.]])
         npt.assert_almost_equal(E, expected_E)
 
     def test_F_matrix(self):
