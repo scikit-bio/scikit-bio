48af7a2c1b6c25130da03fc446bbd3808db841c7
Jai Ram Rideout <jai.rideout@gmail.com>
2014-03-03 13:21:57 -0500

    minor perf improvements to permanova
    uses integer grouping vector

--- bipy/maths/stats/distance/permanova.py
+++ bipy/maths/stats/distance/permanova.py
@@ -28,8 +28,9 @@ class PERMANOVA(object):
             raise ValueError("Grouping vector size must match the number of "
                              "sample IDs in the distance matrix.")
 
-        grouping = np.asarray(grouping)
-        groups = np.unique(grouping)
+        # Find the group labels and convert grouping to an integer vector
+        # (factor).
+        groups, grouping = np.unique(grouping, return_inverse=True)
 
         if len(groups) == len(grouping):
             raise ValueError("All values in the grouping vector are unique. "
@@ -48,6 +49,10 @@ class PERMANOVA(object):
         self._grouping = grouping
         self._groups = groups
         self._num_groups = len(self._groups)
+
+        # Calculate number of samples in each group.
+        self._unique_n = np.bincount(self._grouping)
+
         self._distances = self._dm.condensed_form()
         self._s_T = (self._distances ** 2).sum() / self._dm.num_samples
         self._tri_idxs = np.triu_indices(self._dm.num_samples, k=1)
@@ -78,31 +83,28 @@ class PERMANOVA(object):
         """Compute PERMANOVA pseudo-F statistic."""
         # Create grouping matrix.
         grouping_matrix = -1 * np.ones(self._dm.shape, dtype=int)
-        for group_idx, group in enumerate(self._groups):
+        for group_idx in range(len(self._groups)):
             within_indices = self._index_combinations(
-                np.where(grouping == group)[0])
+                np.where(grouping == group_idx)[0])
             grouping_matrix[within_indices] = group_idx
 
         # Extract upper triangle.
         grouping_tri = grouping_matrix[self._tri_idxs]
 
-        # Calculate number of samples in each group.
-        unique_n = np.bincount(np.unique(grouping, return_inverse=True)[1])
-
-        return self._compute_f_stat(grouping_tri, unique_n)
+        return self._compute_f_stat(grouping_tri)
 
     def _index_combinations(self, indices):
         # Modified from http://stackoverflow.com/a/11144716
         return np.tile(indices, len(indices)), np.repeat(indices, len(indices))
 
-    def _compute_f_stat(self, grouping_tri, unique_n):
+    def _compute_f_stat(self, grouping_tri):
         a = self._num_groups
         N = self._dm.num_samples
 
         # Calculate s_W for each group, accounting for different group sizes.
         s_W = 0
         for i in range(a):
-            s_W += (self._distances[grouping_tri == i] ** 2).sum() / unique_n[i]
+            s_W += (self._distances[grouping_tri == i] ** 2).sum() / self._unique_n[i]
 
         s_A = self._s_T - s_W
         return (s_A / (a - 1)) / (s_W / (N - a))
