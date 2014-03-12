baf50e4b7de37b98e50037f826de7a8b129ef33b
johnjohnchase <llcooljohn@gmail.com>
2014-03-12 16:14:03 -0600

    made pep8 compliant

--- bipy/core/exception.py
+++ bipy/core/exception.py
@@ -94,6 +94,7 @@ class MissingDataError(Exception):
         super(MissingDataError, self).__init__()
         self.args = ("Expected %d row(s) of data, but found %d." % (expected,
                                                                     actual),)
+
+
 class FastqParseError(Exception):
     pass
-    
