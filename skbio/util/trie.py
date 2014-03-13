0c15b56a879dd4b6dbc9fa93ff58a36c50022b40
Jose Navas <josenavasmolina@gmail.com>
2014-03-13 14:43:25 -0600

    Refactor of Trie class

---
+++ skbio/util/trie.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+r"""
+Trie (:mod:`skbio.util.trie`)
+============================
+
+.. currentmodule:: skbio.util.trie
+
+This module provides functionality for working with tries.
+
+Classes
+-------
+
+..autosummary::
+    :toctree: generated/
+
+    Compressed_Trie
+
+Functions
+---------
+
+.. autosummary::
+   :toctree: generated/
+
+   build_trie
+   build_prefix_map
+"""
+from __future__ import division
+
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013--, scikit-bio development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+class _Compressed_Node(object):
+    """docstring for _Compressed_Node"""
+
+    def __init__(self, key, values=None):
+        """Creates a new node"""
+        self.values = values or []
+        self.key = key
+        self.children = {}
+
+    def __nonzero__(self):
+        """Checks if Node contains any data."""
+        return (self.key != "" or len(self.values) > 0
+                or len(self.children.keys()) > 0)
+
+    def __len__(self):
+        """Counts the number of sequences in the Trie"""
+        l = 0
+        for n in self.children.values():
+            l += len(n)
+        l += len(self.values)
+        return l
+
+    def _to_string(self, depth=0):
+        """Debugging method to display the Node's content.
+
+        depth: indentation level
+        """
+        s = ["\n%skey %s" % (depth * '\t', self.key)]
+        #s = ["\n" + depth * '\t' + "key %s" % self.key]
+        if self.values:
+            s.append("%s" % str(self.values))
+        s.append("\n")
+
+        for n in self.children.values():
+            s.append("%s{%s" % (depth * '\t', n._to_string(depth + 1)))
+            s.append("%s}\n" % (depth * '\t'))
+            # s.append(depth*'\t' + "{%s" % (n._to_string(depth+1)))
+            # s.append(depth*'\t'+"}\n")
+        return "".join(s)
+
+    def size(self):
+        """Returns number of nodes."""
+        s = 1
+        for n in self.children.values():
+            s += n.size()
+        return s
+
+    def insert(self, key, value):
+        """Insert a key into the Trie.
+
+        key: The key of the entry
+        value: The value that should be stored for this key
+        """
+        node_key_len = len(self.key)
+        length = min(node_key_len, len(key))
+        # Follow the key into the tree
+        split_node = False
+        index = -1
+        while index < length and not split_node:
+            index += 1
+            split_node = key[index] != self.key[index]
+
+        if split_node:
+            # We need to split up the node pointed by index
+            # Get the key for the new node
+            new_key_node = _Compressed_Node(key[index:], [value])
+            # Get a new node for the old key node
+            old_key_node = _Compressed_Node(self.key[index:], self.values)
+            old_key_node.children = self.children
+            self.children = {key[index]: new_key_node,
+                             self.key[index]: old_key_node}
+            self.key = self.key[:index]
+            self.values = []
+        elif index == len(self.key) and index == len(key):
+            # The new key matches node key exactly
+            self.values.append(value)
+        elif index < node_key_len:
+            # Key shorter than node key
+            lower_node = _Compressed_Node(self.key[index:], self.values)
+            lower_node.children = self.children
+            self.children = {self.key[index]: lower_node}
+            self.key = key
+            self.values = [value]
+        else:
+            # New key longer than current node key
+            node = self.children.get(key[index])
+            if node:
+                # insert into next node
+                node.insert(key[index:], value)
+            else:
+                # Create new node
+                new_node = _Compressed_Node(key[index:], [value])
+                self.children[key[index]] = new_node
+
+    def find(self, key):
+        """Searches for key and returns values stored for the key.
+
+        key: the key whose values should be retuned.
+        """
+        #key exhausted
+        if len(key) == 0:
+            return self.values
+
+        #find matching part of key and node_key
+        min_length = min(len(key), len(self.key))
+        keys_diff = False
+        index = -1
+        while index < min_length and not keys_diff:
+            index += 1
+            keys_diff = key[index] != self.key[index]
+
+        if keys_diff:
+            return []
+        elif index == len(key):
+            #key and node_key match exactly
+            return self.values
+        else:
+            node = self.children.get(key[index])
+            if node:
+                # descend to next node
+                return node.find(key[index:])
+        return []
+
+    def prefixMap(self):
+        """Builds a prefix map from sequences stored in Trie."""
+        mapping = {}
+
+        if len(self.children) == 0:
+            # we have a leaf
+            mapping = {self.values[0]: self.values[1:]}
+        else:
+            # we are at an internal node
+            for child in self.children.values():
+                mapping.update(child.prefixMap())
+            # get largest group
+            n = -1
+            key_largest = None
+            for key, value in mapping.iteritems():
+                if len(value) > n:
+                    n = len(value)
+                    key_largest = key
+            # append this node's values
+            mapping[key_largest].extend(self.values)
+
+        return mapping
+
+
+class Compressed_Trie:
+    """ A compressed Trie for a list of (key, value) pairs
+
+    Parameters
+    ----------
+    pair_list : list of tuples, optional
+        List of (key, value) pairs to initialize the Trie
+
+    Attributes:
+    -----------
+    size
+    prefix_map
+    """
+
+    def __init__(self, pair_list=None):
+        self._root = _Compressed_Node("")
+        if pair_list:
+            for value, seq in pair_list:
+                self.insert(seq, value)
+
+    def __str__(self):
+        return self._root._to_string()
+
+    def __nonzero__(self):
+        return (self._root.__nonzero__())
+
+    def __len__(self):
+        return len(self._root)
+
+    @property
+    def size(self):
+        """int with the number of nodes in the Trie"""
+        return self._root.size()
+
+    @property
+    def prefix_map(self):
+        """Dict with the prefix map
+
+        Dictionary of {value: list of values} containing the prefix map
+        """
+        return self._root.prefixMap()
+
+    def insert(self, key, value):
+        """Inserts key with value in Trie
+
+        Parameters
+        ----------
+        key : string
+
+        value : string
+        """
+        self._root.insert(key, value)
+
+    def find(self, key):
+        """Returns value for key in Trie."""
+        return self._root.find(key)
