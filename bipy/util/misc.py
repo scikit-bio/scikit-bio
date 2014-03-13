d3e5671919238d735602980457d43d3fd8532ed0
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-03-12 18:53:24 -0600

    Fix title length

--- bipy/util/misc.py
+++ bipy/util/misc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 r"""
 Miscellaneous Utilities (:mod:`bipy.util.misc`)
-============================
+===============================================
 
 .. currentmodule:: bipy.util.misc
 
