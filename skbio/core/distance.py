a521ffc387b4064360b9066a4fbf7753302bddff
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2014-03-17 17:46:04 -0600

    Fix typos, import format for numpy, and var name
    Conflicts:
    	skbio/core/distance.py

--- skbio/core/distance.py
+++ skbio/core/distance.py
@@ -147,31 +147,35 @@ class DistanceMatrix(object):
     The data are not checked for symmetry, nor guaranteed/assumed to be
     symmetric.
 
+    References
+    ----------
+    .. [1] http://docs.scipy.org/doc/scipy/reference/spatial.distance.html
+
     Examples
     --------
-    Initialize a `skbio.core.distnace.DistanceMatrix` object from an existing
-    ``numpy.array`` that represents the distances between  three samples with
-    IDs a, b, and c.
+    Initialize a `skbio.core.distance.DistanceMatrix` object from an existing
+    ``numpy.array`` that represents the distances between three samples with
+    IDs a, b, and c:
 
-    >>> from numpy import array
+        0.0\t0.5\t1.0
+        0.5\t0.0\t0.75
+        1.0\t0.75\t0.0
+
+    >>> import numpy as np
     >>> from skbio.core.distance import DistanceMatrix
-    >>> data = array([[0.0,0.5,1.0],
-    ...               [0.5,0.0,0.75],
-    ...               [1.0,0.75,0.0]])
-    >>> IDs = ["a", "b", "c"]
-    >>> dm = DistanceMatrix(data, IDs)
+    >>> data = np.array([[0.0,0.5,1.0],
+    ...                  [0.5,0.0,0.75],
+    ...                  [1.0,0.75,0.0]])
+    >>> ids = ["a", "b", "c"]
+    >>> dm = DistanceMatrix(data, ids)
     >>> print dm
     3x3 distance matrix
-    IDs:
+    ids:
     a, b, c
     Data:
     [[ 0.    0.5   1.  ]
      [ 0.5   0.    0.75]
      [ 1.    0.75  0.  ]]
-
-    References
-    ----------
-    .. [1] http://docs.scipy.org/doc/scipy/reference/spatial.distance.html
 
     """
 
