4d83ab3399eeeb71de246b58bf73892f3f7206cb
Greg Caporaso <gregcaporaso@gmail.com>
2014-03-17 20:58:51 -0600

    doc fix based on comment from my bro @ElBrogrammer

--- skbio/core/alignment.py
+++ skbio/core/alignment.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 r"""
 Sequence collections and alignments (:mod:`skbio.core.alignment`)
-================================================================
+=================================================================
 
 .. currentmodule:: skbio.core.alignment
 
