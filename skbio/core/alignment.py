first pass at entropy calculation bug fix

skbio/core/alignment.py
@@ -1164,7 +1164,8 @@ class Alignment(SequenceCollection):
             result.append(current_freqs)
         return result
 
-    def position_entropies(self, base=None):
+    def position_entropies(self, base=None,
+                           nan_on_non_iupac_standard_chars=True):
         """Return Shannon entropy of positions in Alignment
 
         Parameters
@@ -1198,12 +1199,17 @@ class Alignment(SequenceCollection):
         ...              DNA('TT-C', identifier="seq3")]
         >>> a1 = Alignment(sequences)
         >>> print a1.position_entropies()
-        [0.63651416829481278, 0.63651416829481278, -0.0, 0.63651416829481278]
+        [0.63651416829481278, 0.63651416829481278, -0.0, np.nan]
 
         """
         result = []
+        iupac_standard_characters = self[0].iupac_standard_characters()
         for f in self.position_frequencies():
-            result.append(entropy(f.values(), base=base))
+            if (nan_on_non_iupac_standard_chars and
+                    len(set(f.keys()) - iupac_standard_characters) > 0):
+                result.append(np.nan)
+            else:
+                result.append(entropy(f.values(), base=base))
         return result
 
     def sequence_frequencies(self):

skbio/core/tests/test_alignment.py
@@ -497,11 +497,11 @@ class AlignmentTests(TestCase):
         tested by calculating values as described in this post:
          http://stackoverflow.com/a/15476958/3424666
         """
-        expected = [0.69314, 0.69314, 0.69314, 0.0, 0.69314]
+        expected = [0.69314, 0.69314, 0.69314, 0.0, np.nan]
         np.testing.assert_almost_equal(self.a2.position_entropies(),
                                        expected, 5)
 
-        expected = [1.0, 1.0, 1.0, 0.0, 1.0]
+        expected = [1.0, 1.0, 1.0, 0.0, np.nan]
         np.testing.assert_almost_equal(self.a2.position_entropies(base=2),
                                        expected, 5)
 