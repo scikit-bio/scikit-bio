a7ffb8cf84aee9c06efbe20b5040d21917c85da8
Jai Ram Rideout <jai.rideout@gmail.com>
2014-03-28 13:02:59 -0400

    change conf back to sphinx 1.1

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../sphinxext/numpydoc'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.2.2'
+needs_sphinx = '1.1'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
