e4e90bb1e0df27b407398e5974fec2cf10377255
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-03-28 02:02:10 -0600

    TST: Add tests for the Rfam related code

--- skbio/parse/tests/test_sequences.py
+++ skbio/parse/tests/test_sequences.py
@@ -8,6 +8,14 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
+from skbio.parse.sequences import (MinimalRfamParser, RfamFinder,
+                                   ChangedSequence, is_empty_or_html)
+from skbio.core.alignment import Alignment
+
+
+#from cogent.struct.rna2d import WussStructure
+#from cogent.core.moltype import BYTES
+#Sequence = BYTES.Sequence
 
 from skbio.core.exception import FastqParseError, RecordError
 from skbio.parse.sequences import parse_fastq
@@ -109,22 +117,358 @@ class ParseFastaTests(GenericFastaTest):
 class ParseFastqTests(TestCase):
     def setUp(self):
         """ Initialize variables to be used by the tests """
-        self.fastq_example = fastq_example.split('\n')
-        self.fastq_example_2 = fastq_example_2.split('\n')
+        self.FASTQ_EXAMPLE = FASTQ_EXAMPLE.split('\n')
+        self.FASTQ_EXAMPLE_2 = FASTQ_EXAMPLE_2.split('\n')
 
     def test_parse(self):
         """sequence and info objects should correctly match"""
-        for label, seq, qual in parse_fastq(self.fastq_example):
-            self.assertTrue(label in data)
-            self.assertEqual(seq, data[label]["seq"])
-            self.assertEqual(qual, data[label]["qual"])
+        for label, seq, qual in parse_fastq(self.FASTQ_EXAMPLE):
+            self.assertTrue(label in DATA)
+            self.assertEqual(seq, DATA[label]["seq"])
+            self.assertEqual(qual, DATA[label]["qual"])
 
     def test_parse_error(self):
         """Does this raise a FastqParseError with incorrect input?"""
         with self.assertRaises(FastqParseError):
-            list(parse_fastq(self.fastq_example_2, strict=True))
+            list(parse_fastq(self.FASTQ_EXAMPLE_2, strict=True))
 
-data = {
+class RfamParserTests(TestCase):
+    """ Tests componenets of the rfam parser, in the rfam.py file """
+
+    def setUp(self):
+        """ Construct some fake data for testing purposes """
+
+        self._fake_headers = []
+        temp = list(fake_headers.split('\n'))
+        for line in temp:
+            self._fake_headers.append(line.strip())
+        del temp
+        
+        self._fake_record_no_headers =\
+            list(fake_record_no_headers.split('\n'))
+
+        self._fake_record_no_sequences =\
+            list(fake_record_no_sequences.split('\n'))
+
+        self._fake_record_no_structure =\
+            list(fake_record_no_structure.split('\n'))
+
+        self._fake_two_records =\
+            list(fake_two_records.split('\n'))
+            
+        self._fake_record =\
+            list(fake_record.split('\n'))
+
+        self._fake_record_bad_header_1 =\
+            list(fake_record_bad_header_1.split('\n'))
+            
+        self._fake_record_bad_header_2 =\
+            list(fake_record_bad_header_2.split('\n'))
+
+        self._fake_record_bad_sequence_1 =\
+            list(fake_record_bad_sequence_1.split('\n'))
+
+        self._fake_record_bad_structure_1 =\
+            list(fake_record_bad_structure_1.split('\n'))                                                    
+        self._fake_record_bad_structure_2 =\
+            list(fake_record_bad_structure_2.split('\n'))
+
+        self.single_family = single_family.split('\n')
+            
+    def test_is_empty_or_html(self):
+        """is_empty_or_html: should ignore empty and HTML line"""
+        line = '        '
+        self.assertEqual(is_empty_or_html(line), True)
+        line = '\n\n'
+        self.assertEqual(is_empty_or_html(line), True)
+        line = '<pre>'
+        self.assertEqual(is_empty_or_html(line), True)
+        line = '</pre>\n\n'
+        self.assertEqual(is_empty_or_html(line), True)
+        line = '\t<//\n'
+        self.assertEqual(is_empty_or_html(line), False)
+
+    def test_MinimalRfamParser_strict_missing_fields(self):
+        """MinimalRfamParser: toggle strict functions w/ missing fields"""
+        # strict = True
+        
+        self.assertRaises(RecordError,list,\
+            MinimalRfamParser(self._fake_record_no_sequences))
+        
+        self.assertRaises(RecordError,list,\
+            MinimalRfamParser(self._fake_record_no_structure))
+
+        # strict = False
+        # no header shouldn't be a problem
+        self.assertEqual(list(MinimalRfamParser(self._fake_record_no_headers,\
+            strict=False)), [([],{'Z11765.1/1-89':'GGUC'},'............>>>')])
+        # should get empty on missing sequence or missing structure
+        self.assertEqual(list(MinimalRfamParser(self._fake_record_no_sequences,\
+            strict=False)), [])
+        self.assertEqual(list(MinimalRfamParser(self._fake_record_no_structure,\
+            strict=False)), [])
+
+    def test_MinimalRfamParser_strict_invalid_sequence(self):
+        """MinimalRfamParser: toggle strict functions w/ invalid seq
+        """
+        #strict = True
+        self.assertRaises(RecordError,list,\
+            MinimalRfamParser(self._fake_record_bad_sequence_1))
+
+        # strict = False
+        # you expect to get back as much information as possible, also
+        # half records or sequences
+        result = MinimalRfamParser(self._fake_record_bad_sequence_1,strict=False)
+        self.assertEqual(len(list(MinimalRfamParser(\
+            self._fake_record_bad_sequence_1,strict=False))[0][1].NamedSeqs), 3)            
+
+    def test_MinimalRfamParser_strict_invalid_structure(self):
+        """MinimalRfamParser: toggle strict functions w/ invalid structure
+        """
+        #strict = True
+        self.assertRaises(RecordError,list,\
+            MinimalRfamParser(self._fake_record_bad_structure_1))
+
+        # strict = False
+        self.assertEqual(list(MinimalRfamParser(\
+            self._fake_record_bad_structure_1,strict=False))[0][2],None)                                
+
+    def test_MinimalRfamParser_w_valid_data(self):
+        """MinimalRfamParser: integrity of output """
+
+        # Some ugly constructions here, but this is what the output of
+        # parsing fake_two_records should be
+        headers = ['#=GF AC   RF00014','#=GF AU   Mifsud W']
+        sequences =\
+        {'U17136.1/898-984':\
+        ''.join(['AACACAUCAGAUUUCCUGGUGUAACGAAUUUUUUAAGUGCUUCUUGCUUA',\
+            'AGCAAGUUUCAUCCCGACCCCCUCAGGGUCGGGAUUU']),\
+        'M15749.1/155-239':\
+        ''.join(['AACGCAUCGGAUUUCCCGGUGUAACGAA-UUUUCAAGUGCUUCUUGCAUU',\
+            'AGCAAGUUUGAUCCCGACUCCUG-CGAGUCGGGAUUU']),\
+        'AF090431.1/222-139':\
+        ''.join(['CUCACAUCAGAUUUCCUGGUGUAACGAA-UUUUCAAGUGCUUCUUGCAUA',\
+            'AGCAAGUUUGAUCCCGACCCGU--AGGGCCGGGAUUU'])}
+
+        structure = WussStructure(''.join(\
+        ['...<<<<<<<.....>>>>>>>....................<<<<<...',\
+        '.>>>>>....<<<<<<<<<<.....>>>>>>>>>>..']))
+        
+        data = []
+        for r in MinimalRfamParser(self._fake_two_records, strict=False):
+            data.append(r)
+        self.assertEqual(data[0],(headers,sequences,structure))
+        assert isinstance(data[0][1],Alignment)
+
+        # This line tests that invalid entries are ignored when strict=False
+        # Note, there are two records in self._fake_two_records, but 2nd is
+        # invalid
+        self.assertEqual(len(data),1)            
+            
+    def test_RfamFinder(self):
+        """RfamFinder: integrity of output """
+        fake_record = ['a','//','b','b','//']
+        num_records = 0
+        data = []
+        for r in RfamFinder(fake_record):
+            data.append(r)
+            num_records += 1
+        self.assertEqual(num_records, 2)
+        self.assertEqual(data[0], ['a','//'])
+        self.assertEqual(data[1], ['b','b','//'])
+
+    def test_ChangedSequence(self):
+        """ChangedSequence: integrity of output"""
+        # Made up input, based on a line that would look like:
+        # U17136.1/898-984  AACA..CAU..CAGAUUUCCU..GGUGUAA.CGAA
+        
+        s_in = 'AACA..CAU..CAGAUUUCCU..GGUGUAA.CGAA'
+        s_out = 'AACA--CAU--CAGAUUUCCU--GGUGUAA-CGAA'
+        sequence = ChangedSequence(s_in)
+        
+        self.assertEqual(sequence, s_out)
+
+        # test some extremes on the seq
+        # sequence of all blanks
+        s_in = '.' * 5
+        s_out = '-' * 5
+        sequence = ChangedSequence(s_in)
+
+        self.assertEqual(sequence, s_out)
+
+        # sequence of no blanks
+        s_in = 'U' * 5
+        s_out = 'U' * 5
+        sequence = ChangedSequence(s_in)
+
+        self.assertEqual(sequence, s_out)
+       
+
+# This is an altered version of some header info from Rfam.seed modified to
+# incorporate different cases for testing
+fake_headers = """#=GF AC   RF00001
+#=GF AU   Griffiths-Jones SR
+#=GF ID   5S_rRNA
+#=GF RT   5S Ribosomal RNA Database.
+#=GF DR   URL; http://oberon.fvms.ugent.be:8080/rRNA/ssu/index.html;
+#=GF DR   URL; http://rdp.cme.msu.edu/html/;
+#=GF CC   This is a short
+#=GF CC   comment
+#=GF SQ   606
+#=GF PK   not real"""
+
+fake_record_no_headers ="""Z11765.1/1-89                        GGUC
+#=GC SS_cons                         ............>>>
+//"""
+
+fake_record_no_sequences ="""#=GF AC   RF00006
+#=GC SS_cons                         ............>
+//"""
+
+fake_record_no_structure ="""#=GF AC   RF00006
+
+Z11765.1/1-89                        GGUCAGC
+//"""
+
+fake_two_records ="""# STOCKHOLM 1.0
+
+#=GF AC   RF00014
+#=GF AU   Mifsud W
+
+U17136.1/898-984               AACACAUCAGAUUUCCUGGUGUAACGAAUUUUUUAAGUGCUUCUUGCUUA
+M15749.1/155-239               AACGCAUCGGAUUUCCCGGUGUAACGAA.UUUUCAAGUGCUUCUUGCAUU
+AF090431.1/222-139             CUCACAUCAGAUUUCCUGGUGUAACGAA.UUUUCAAGUGCUUCUUGCAUA
+#=GC SS_cons                   ...<<<<<<<.....>>>>>>>....................<<<<<...
+#=GC RF                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+U17136.1/898-984               AGCAAGUUUCAUCCCGACCCCCUCAGGGUCGGGAUUU
+M15749.1/155-239               AGCAAGUUUGAUCCCGACUCCUG.CGAGUCGGGAUUU
+AF090431.1/222-139             AGCAAGUUUGAUCCCGACCCGU..AGGGCCGGGAUUU
+#=GC SS_cons                   .>>>>>....<<<<<<<<<<.....>>>>>>>>>>..
+#=GC RF                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//
+#=GF AC   RF00015
+//"""
+
+fake_record ="""# STOCKHOLM 1.0
+
+#=GF AC   RF00014
+#=GF AU   Mifsud W
+
+U17136.1/898-984               AACACAUCAGAUUUCCUGGUGUAACGAAUUUUUUAAGUGCUUCUUGCUUA
+M15749.1/155-239               AACGCAUCGGAUUUCCCGGUGUAACGAA.UUUUCAAGUGCUUCUUGCAUU
+AF090431.1/222-139             CUCACAUCAGAUUUCCUGGUGUAACGAA.UUUUCAAGUGCUUCUUGCAUA
+#=GC SS_cons                   ...<<<<<<<.....>>>>>>>....................<<<<<...
+#=GC RF                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+U17136.1/898-984               AGCAAGUUUCAUCCCGACCCCCUCAGGGUCGGGAUUU
+M15749.1/155-239               AGCAAGUUUGAUCCCGACUCCUG.CGAGUCGGGAUUU
+AF090431.1/222-139             AGCAAGUUUGAUCCCGACCCGU..AGGGCCGGGAUUU
+#=GC SS_cons                   .>>>>>....<<<<<<<<<<.....>>>>>>>>>>..
+#=GC RF                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//"""
+
+fake_record_bad_header_1 ="""# STOCKHOLM 1.0
+
+#=GF AC   RF00014
+#=GF AUMifsud W
+
+U17136.1/898-984               AACACAUCAGAUUUCCUGGUGUAACGAAUUUUUUAAGUGCUUCUUGCUUA
+M15749.1/155-239               AACGCAUCGGAUUUCCCGGUGUAACGAA.UUUUCAAGUGCUUCUUGCAUU
+AF090431.1/222-139             CUCACAUCAGAUUUCCUGGUGUAACGAA.UUUUCAAGUGCUUCUUGCAUA
+#=GC SS_cons                   ...<<<<<<<.....>>>>>>>....................<<<<<...
+#=GC RF                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+U17136.1/898-984               AGCAAGUUUCAUCCCGACCCCCUCAGGGUCGGGAUUU
+M15749.1/155-239               AGCAAGUUUGAUCCCGACUCCUG.CGAGUCGGGAUUU
+AF090431.1/222-139             AGCAAGUUUGAUCCCGACCCGU..AGGGCCGGGAUUU
+#=GC SS_cons                   .>>>>>....<<<<<<<<<<.....>>>>>>>>>>..
+#=GC RF                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//"""
+
+fake_record_bad_header_2 ="""# STOCKHOLM 1.0
+
+#=GF AC   RF00014
+#=GFAUMifsud W
+
+U17136.1/898-984               AACACAUCAGAUUUCCUGGUGUAACGAAUUUUUUAAGUGCUUCUUGCUUA
+M15749.1/155-239               AACGCAUCGGAUUUCCCGGUGUAACGAA.UUUUCAAGUGCUUCUUGCAUU
+AF090431.1/222-139             CUCACAUCAGAUUUCCUGGUGUAACGAA.UUUUCAAGUGCUUCUUGCAUA
+#=GC SS_cons                   ...<<<<<<<.....>>>>>>>....................<<<<<...
+#=GC RF                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+U17136.1/898-984               AGCAAGUUUCAUCCCGACCCCCUCAGGGUCGGGAUUU
+M15749.1/155-239               AGCAAGUUUGAUCCCGACUCCUG.CGAGUCGGGAUUU
+AF090431.1/222-139             AGCAAGUUUGAUCCCGACCCGU..AGGGCCGGGAUUU
+#=GC SS_cons                   .>>>>>....<<<<<<<<<<.....>>>>>>>>>>..
+#=GC RF                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//"""
+
+fake_record_bad_sequence_1 ="""# STOCKHOLM 1.0
+
+#=GF AC   RF00014
+#=GF AU   Mifsud W
+
+U17136.1/898-984AACACAUCAGAUUUCCUGGUGUAACGAAUUUUUUAAGUGCUUCUUGCUUA
+M15749.1/155-239               AACGCAUCGGAUUUCCCGGUGUAACGAA.UUUUCAAGUGCUUCUUGCAUU
+AF090431.1/222-139             CUCACAUCAGAUUUCCUGGUGUAACGAA.UUUUCAAGUGCUUCUUGCAUA
+#=GC SS_cons                   ...<<<<<<<.....>>>>>>>....................<<<<<...
+#=GC RF                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+U17136.1/898-984               AGCAAGUUUCAUCCCGACCCCCUCAGGGUCGGGAUUU
+M15749.1/155-239               AGCAAGUUUGAUCCCGACUCCUG.CGAGUCGGGAUUU
+AF090431.1/222-139             AGCAAGUUUGAUCCCGACCCGU..AGGGCCGGGAUUU
+#=GC SS_cons                   .>>>>>....<<<<<<<<<<.....>>>>>>>>>>..
+#=GC RF                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//"""
+
+fake_record_bad_structure_1 ="""# STOCKHOLM 1.0
+
+#=GF AC   RF00014
+#=GF AU   Mifsud W
+
+U17136.1/898-984               AACACAUCAGAUUUCCUGGUGUAACGAAUUUUUUAAGUGCUUCUUGCUUA
+M15749.1/155-239               AACGCAUCGGAUUUCCCGGUGUAACGAA.UUUUCAAGUGCUUCUUGCAUU
+AF090431.1/222-139             CUCACAUCAGAUUUCCUGGUGUAACGAA.UUUUCAAGUGCUUCUUGCAUA
+#=GC SS_cons...<<<<<<<.....>>>>>>>....................<<<<<...
+#=GC RF                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+U17136.1/898-984               AGCAAGUUUCAUCCCGACCCCCUCAGGGUCGGGAUUU
+M15749.1/155-239               AGCAAGUUUGAUCCCGACUCCUG.CGAGUCGGGAUUU
+AF090431.1/222-139             AGCAAGUUUGAUCCCGACCCGU..AGGGCCGGGAUUU
+#=GC SS_cons                   .>>>>>....<<<<<<<<<<.....>>>>>>>>>>..
+#=GC RF                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//"""
+
+fake_record_bad_structure_2 ="""# STOCKHOLM 1.0
+
+#=GF AC   RF00014
+#=GF AU   Mifsud W
+
+U17136.1/898-984               AACACAUCAGAUUUCCUGGUGUAACGAAUUUUUUAAGUGCUUCUUGCUUA
+M15749.1/155-239               AACGCAUCGGAUUUCCCGGUGUAACGAA.UUUUCAAGUGCUUCUUGCAUU
+AF090431.1/222-139             CUCACAUCAGAUUUCCUGGUGUAACGAA.UUUUCAAGUGCUUCUUGCAUA
+#=GC SS_cons                   ...<<<<<<<.....>>>>>>>....................<<<<<!!!
+#=GC RF                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+U17136.1/898-984               AGCAAGUUUCAUCCCGACCCCCUCAGGGUCGGGAUUU
+M15749.1/155-239               AGCAAGUUUGAUCCCGACUCCUG.CGAGUCGGGAUUU
+AF090431.1/222-139             AGCAAGUUUGAUCCCGACCCGU..AGGGCCGGGAUUU
+#=GC SS_cons                   .>>>>>....<<<<<<<<<<.....>>>>>>>>>>..
+#=GC RF                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//"""
+
+single_family=\
+"""K02120.1/628-682      AUGGGAAAUUCCCCCUCCUAUAACCCCCCCGCUGGUAUCUCCCCCUCAGA
+D00647.1/629-683      AUGGGAAACUCCCCCUCCUAUAACCCCCCCGCUGGCAUCUCCCCCUCAGA
+#=GC SS_cons          <<<<<<.........>>>>>>.........<<<<<<.............>
+
+K02120.1/628-682      CUGGC
+D00647.1/629-683      CUGGC
+#=GC SS_cons          >>>>>
+//"""
+
+DATA = {
     "GAPC_0015:6:1:1259:10413#0/1":
     dict(seq='AACACCAAACTTCTCCACCACGTGAGCTACAAAAG',
          qual=r'````Y^T]`]c^cabcacc`^Lb^ccYT\T\Y\WF'),
@@ -158,7 +502,7 @@ data = {
          qual=r"\\\ZTYTSaLbb``\_UZ_bbcc`cc^[ac\a\Tc ".strip())
 }
 
-fastq_example = r"""@GAPC_0015:6:1:1259:10413#0/1
+FASTQ_EXAMPLE = r"""@GAPC_0015:6:1:1259:10413#0/1
 AACACCAAACTTCTCCACCACGTGAGCTACAAAAG
 +GAPC_0015:6:1:1259:10413#0/1
 ````Y^T]`]c^cabcacc`^Lb^ccYT\T\Y\WF
@@ -199,7 +543,7 @@ TTGTTTCCACTTGGTTGATTTCACCCCTGAGTTTG
 +GAPC_0015:6:1:1317:3403#0/1
 \\\ZTYTSaLbb``\_UZ_bbcc`cc^[ac\a\Tc"""
 
-fastq_example_2 = r"""@GAPC_0017:6:1:1259:10413#0/1
+FASTQ_EXAMPLE_2 = r"""@GAPC_0017:6:1:1259:10413#0/1
 AACACCAAACTTCTCCACCACGTGAGCTACAAAAG
 +GAPC_0015:6:1:1259:10413#0/1
 ````Y^T]`]c^cabcacc`^Lb^ccYT\T\Y\WF
