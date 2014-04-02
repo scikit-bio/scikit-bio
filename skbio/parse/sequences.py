51bac6c2e834748e27ed31181eb6a02a0f226443
Joshua Shorenstein <squirrelo@gmail.com>
2014-04-02 14:06:37 -0600

    stockholm parser initial commit

--- skbio/parse/sequences.py
+++ skbio/parse/sequences.py
@@ -15,6 +15,7 @@ Functions
 
     parse_fasta
     parse_fastq
+    parse_stockholm
 
 
 """
@@ -27,8 +28,192 @@ Functions
 # The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
-from skbio.core.exception import FastqParseError, RecordError
+from skbio.core.exception import (FastqParseError, RecordError,
+                                  StockholmParseError)
 from skbio.parse.record_finder import LabeledRecordFinder
+from skbio.format.stockholm import Stockholm
+from skbio.core.alignment import Alignment
+
+
+def _parse_gf_info(lines):
+    """Takes care of parsing GF lines in stockholm including special cases"""
+    parsed = {}
+    #needed for making each multi-line RT and NH one string
+    rt = []
+    nh = []
+    lastline = ""
+    for line in lines:
+        try:
+            init, feature, content = line.split(None, 2)
+        except ValueError:
+            raise StockholmParseError("Malformed GF line encountered!\n%s" %
+                                      line.split(None, 2))
+        if init != "#=GF":
+            raise StockholmParseError("Non-GF line encountered!")
+
+        #take care of adding multiline RT to the parsed information
+        if lastline == "RT" and feature != "RT":
+            #add rt line to the parsed dictionary
+            rtline = " ".join(rt)
+            rt = []
+            if "RT" in parsed:
+                parsed["RT"].append(rtline)
+            else:
+                parsed["RT"] = [rtline]
+        elif feature == "RT":
+            rt.append(content)
+            lastline = feature
+            continue
+        #Take care of adding multiline NH to the parsed dictionary
+        elif lastline == "NH" and feature != "NH":
+            nhline = " ".join(nh)
+            nh = []
+            if "NH" in parsed:
+                parsed["NH"].append(nhline)
+            else:
+                parsed["NH"] = [nhline]
+        elif feature == "NH":
+            nh.append(content)
+            lastline = feature
+            continue
+
+        #add current feature to the parsed information
+        if feature in parsed:
+            parsed[feature].append(content)
+        else:
+            parsed[feature] = [content]
+        lastline = feature
+
+    #clean up parsed info by removing unneccessary lists
+    for feature in parsed:
+        #list of multi-line features to join into single string if necessary
+        if feature in ["CC"]:
+            parsed[feature] = ' '.join(parsed[feature])
+        elif len(parsed[feature]) == 1:
+            parsed[feature] = parsed[feature][0]
+    return parsed
+
+
+def _parse_gc_info(lines, strict=False, seqlen=-1):
+    """Takes care of parsing GC lines in stockholm format"""
+    parsed = {}
+    for line in lines:
+        try:
+            init, feature, content = line.split(None, 2)
+        except ValueError:
+            raise StockholmParseError("Malformed GC line encountered!\n%s" %
+                                      line.split(None, 2))
+        if init != "#=GC":
+            raise StockholmParseError("Non-GC line encountered!")
+
+        #add current feature to the parsed information
+        if feature in parsed:
+            if strict:
+                raise StockholmParseError("Should not have multiple lines "
+                                          "with same feature: %s" % feature)
+            parsed[feature].append(content)
+        else:
+            parsed[feature] = [content]
+
+    #clean up parsed info by removing unneccessary lists
+    for feature in parsed:
+        parsed[feature] = ''.join(parsed[feature])
+        if strict:
+            if len(parsed[feature]) != seqlen:
+                raise StockholmParseError("GR must have exactly one char "
+                                          "per position in alignment!")
+
+    return parsed
+
+
+def _parse_gs_gr_info(lines, strict=False, seqlen=-1):
+    """Takes care of parsing GS and GR lines in stockholm format"""
+    parsed = {}
+    for line in lines:
+        try:
+            init, label, feature, content = line.split(None, 3)
+        except ValueError:
+            raise StockholmParseError("Malformed GS/GR line encountered!\n%s" %
+                                      line.split(None, 3))
+        if init != "#=GS" and init != "#=GR":
+                raise StockholmParseError("Non-GS/GR line encountered!")
+
+        #parse each line, taking into account we can have interleaved format
+        if label in parsed and feature in parsed[label]:
+                #interleaved format, so need list of content
+                parsed[label][feature].append(content)
+        else:
+            parsed[label] = {feature: [content]}
+
+    #join all the crazy lists created during parsing
+    for label in parsed:
+        for feature, content in parsed[label].items():
+            parsed[label][feature] = ''.join(content)
+            if strict:
+                if len(parsed[label][feature]) != seqlen:
+                    raise StockholmParseError("GR must have exactly one char "
+                                              "per position in alignment!")
+    return parsed
+
+def parse_stockholm(infile, seq_constructor, strict=False):
+    r"""yields Stockholm objects from a stockholm file.
+
+    Parameters
+    ----------
+    infile : open file object
+        An open stockholm file.
+
+    seq_constructor : BiologicalSequence object
+        The biologicalsequence object that corresponds to what the stockholm
+        file holds. See skbio.core.sequence
+
+    Returns
+    -------
+    sto : named tuple
+        yields in Stockholm named tuple format. For more information, see
+        skbio.format.stockholm.Stockholm
+    """
+    #make sure first line is corect
+    line = infile.readline()
+    if not line.startswith("# STOCKHOLM 1.0"):
+        raise StockholmParseError("Incorrect header found")
+    gs_lines = []
+    gf_lines = []
+    gr_lines = []
+    gc_lines = []
+    seqs = {}
+    for line in infile:
+        line = line.strip()
+        if line == "":
+            #skip blank lines
+            continue
+        elif line == "//":
+            #create alignment from stockholm file
+            aln = Alignment.from_fasta_records(seqs.items(), seq_constructor)
+            seqlen = len(aln)
+            #parse information lines
+            GF = _parse_gf_info(gf_lines)
+            GS = _parse_gs_gr_info(gs_lines)
+            GR = _parse_gs_gr_info(gr_lines, strict, seqlen)
+            GC = _parse_gc_info(gc_lines, strict, seqlen)
+            #yield the actual stockholm object
+            yield Stockholm(aln, GF, GS, GR, GC)
+        elif line.startswith("#=GF"):
+            gf_lines.append(line)
+        elif line.startswith("#=GS"):
+            gs_lines.append(line)
+        elif line.startswith("#=GR"):
+            gr_lines.append(line)
+        elif line.startswith("#=GC"):
+            gc_lines.append(line)
+        else:
+            lineinfo = line.split()
+            #assume sequence since nothing else in format is left
+            #in case of interleaved format, need to do check
+            if lineinfo[0] in seqs:
+                seqs[lineinfo[0]] += lineinfo[1]
+            else:
+                seqs[lineinfo[0]] = lineinfo[1]
 
 
 def is_fasta_label(x):
