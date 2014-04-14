17d0223031d3717530bcc9c8abbea7969ff9efdc
Jorge Cañardo Alastuey <jorgecanardo@gmail.com>
2014-04-15 00:19:08 +0200

    Shebang and future import (per @ElBrogrammer's comment)

--- skbio/util/tests/test_testing.py
+++ skbio/util/tests/test_testing.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+from __future__ import division
 # ----------------------------------------------------------------------------
 # Copyright (c) 2013--, scikit-bio development team.
 #
