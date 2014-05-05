a92f3b2afef6b333cbe51f7c895984363152ca07
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-05-05 17:06:08 -0600

    TST: Fix tests to work with the new structure

--- skbio/format/sequences/tests/test_fasta.py
+++ skbio/format/sequences/tests/test_fasta.py
@@ -2,7 +2,8 @@
 """Tests for FASTA sequence format writer.
 """
 from unittest import TestCase, main
-from skbio.format.fasta import fasta_from_sequences, fasta_from_alignment
+from skbio.format.sequences.fasta import (fasta_from_sequences,
+                                          fasta_from_alignment)
 
 
 class FastaTests(TestCase):
