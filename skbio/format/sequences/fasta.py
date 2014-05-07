4ef405eea08fc2c1bbb32ed46a550e3d684936d9
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-05-07 13:30:23 -0600

    DOC: Add numpy docstring to fasta_from_alignment
    Includes everything, Parameters, Returns, See Also and Examples. I
    updated the code to be aware of the latest skbio core objects.

--- skbio/format/sequences/fasta.py
+++ skbio/format/sequences/fasta.py
@@ -9,6 +9,8 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 
+from skbio.core.alignment import Alignment
+
 
 class _fake_seq(str):
 
@@ -99,32 +101,66 @@ def fasta_from_sequences(seqs, make_seqlabel=None, line_wrap=None):
 
 
 def fasta_from_alignment(aln, make_seqlabel=None, line_wrap=None, sort=True):
-    """Returns a FASTA string given an alignment.
+    """Returns a FASTA string given an alignment object
 
-        - aln can be an Alignment object or dict.
-        - make_seqlabel: callback function that takes the seq object and
-          returns a label str
-        - line_wrap: a integer for maximum line width
+    Parameters
+    ----------
+    aln : Alignment, dict
+        alignment or dictionary where the keys are the sequence identifiers and
+        the values are the sequences themselves.
+    make_seqlabel : function, optional
+        callback function that takes the seq object and returns a label ``str``
+        . If ``None`` is passed, the following attributes will try to be
+        retrieved in this order and the first to exist will be used:
+        ``identifier``, ``Label`` or ``Name``. In any other case an integer
+        with the position of the sequence object will be used.
+    line_wrap : int, optional
+        line_wrap: a integer for maximum line width, if ``None`` is passed the
+        full sequence will be used.
+    sort : bool, optional
+        Whether or not the sequences should be sorted by their sequence
+        identifier, default value is ``True``.
+
+    Returns
+    -------
+    str
+        FASTA formatted string composed of the objects passed in via `seqs`.
+
+    See Also
+    --------
+    skbio.parse.sequences.parse_fasta
+    skbio.core.alignment.Alignment
+
+    Examples
+    --------
+    Formatting a sequence alignment object into a FASTA file.
+
+    >>> from skbio.core.alignment import Alignment
+    >>> from skbio.core.sequence import DNA
+    >>> from skbio.format.sequences import fasta_from_alignment
+    >>> seqs = [DNA("ACC--G-GGTA..", identifier="seq1"),
+    ...         DNA("TCC--G-GGCA..", identifier="seqs2")]
+    >>> a1 = Alignment(seqs)
+    >>> fasta_from_alignment(a1)
+    '>seq1\nACC--G-GGTA..\n>seqs2\nTCC--G-GGCA..'
+
+
     """
-    # get seq output order
-    try:
-        order = aln.Names[:]
-    except AttributeError:
+    # check if it's an Alignment object or a dictionary
+    if isinstance(aln, Alignment):
+        order = aln.identifiers()
+    else:
         order = aln.keys()
 
     if sort:
         order = sorted(order)
 
-    try:
-        seq_dict = aln.NamedSeqs
-    except AttributeError:
-        seq_dict = aln
-
     ordered_seqs = []
     for label in order:
-        seq = seq_dict[label]
+        seq = aln[label]
         if isinstance(seq, str):
             seq = _fake_seq(label, seq)
         ordered_seqs.append(seq)
     return fasta_from_sequences(ordered_seqs, make_seqlabel=make_seqlabel,
                                 line_wrap=line_wrap)
+
