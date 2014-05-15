b9eaa6d469fd47315efcd2d99990a74ae9da1285
Jai Ram Rideout <jai.rideout@gmail.com>
2014-05-15 12:44:10 -0700

    Add docstring to EfficiencyWarning
    Thanks @gregcaporaso and @ElDeveloper!!!!

--- skbio/core/warning.py
+++ skbio/core/warning.py
@@ -29,4 +29,13 @@ from __future__ import absolute_import, division, print_function
 
 
 class EfficiencyWarning(Warning):
+    """Warn about potentially accidental use of inefficient code.
+
+    For example, if a user doesn't have an optimized version of a
+    function/algorithm available in their scikit-bio installation, a slower,
+    pure-Python implementation may be used instead. This warning can be used to
+    let the user know they are using a version of the function that could be
+    potentially orders of magnitude slower.
+
+    """
     pass
