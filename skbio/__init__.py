1758e6a0b228eaf7f1f4c68ff1ff7075a43fbeee
Jai Ram Rideout <jai.rideout@gmail.com>
2014-05-16 18:38:57 -0700

    Change Fork me logo color to match new theme

--- skbio/__init__.py
+++ skbio/__init__.py
@@ -10,7 +10,7 @@ from __future__ import print_function
 # ----------------------------------------------------------------------------
 
 __credits__ = "https://github.com/biocore/scikit-bio/graphs/contributors"
-__version__ = "0.1.1-dev"
+__version__ = "0.1.1"
 
 mottos = [
     # 03/15/2014
