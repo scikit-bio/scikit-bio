e85e5a394992ee25a9151984791c6d235eb415bf
Jai Ram Rideout <jai.rideout@gmail.com>
2014-05-23 16:33:42 -0400

    Move bioenv function into base module
    Due to bug in nose:
    
    https://github.com/nose-devs/nose/issues/92
    
    Also fix subtle bug in scale calculations that was discovered while doctesting. It appears that R's scale and sklearn's scale don't work the same way.
    
    Still some minor cleanup to do, and need more unit tests to cover the scale bug.

--- skbio/math/stats/distance/base.py
+++ skbio/math/stats/distance/base.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python
-
 # ----------------------------------------------------------------------------
 # Copyright (c) 2013--, scikit-bio development team.
 #
@@ -8,13 +6,16 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 
-from __future__ import absolute_import, division
+from __future__ import absolute_import, division, print_function
 from future.utils.six import StringIO
 
 import csv
+from itertools import combinations
 
 import numpy as np
 import pandas as pd
+from scipy.spatial.distance import pdist
+from scipy.stats import spearmanr
 
 from skbio.core.distance import DistanceMatrix
 
@@ -274,3 +275,205 @@ class CategoricalStatsResults(object):
             result = ('%1.' + '%df' % decimal_places) % p_value
 
         return result
+
+
+# This function is defined here instead of in its own bioenv.py module since
+# running 'nosetests --with-doctest' fails if a function is defined in a module
+# with the same name. When the following issue is fixed in nose, consider
+# moving this to its own module:
+#
+#     https://github.com/nose-devs/nose/issues/92
+#
+# Either way, this shouldn't affect users as the public API/import stays the
+# same regardless of where we put the function.
+def bioenv(distance_matrix, data_frame, columns=None):
+    """Find subset of variables maximally correlated with community distances.
+
+    Finds subsets of numeric variables (e.g., environmental variables) whose
+    Euclidean distances are maximally rank-correlated with the distance matrix
+    (e.g., community distance matrix). Correlation between the community
+    distance matrix and Euclidean environmental distance matrix is computed
+    using Spearman's rank correlation coefficient (:math:`\\rho`).
+
+    Subset sizes of environmental variables range from 1 to the total number of
+    variables (inclusive). For example, if there are 3 variables, the "best"
+    variable subsets will be computed for subset sizes 1, 2, and 3.
+
+    The "best" subset is chosen by computing the correlation between the
+    community distance matrix and all possible Euclidean environmental distance
+    matrices at the given subset size. The subset with maximum correlation is
+    chosen as the "best" subset.
+
+    Parameters
+    ----------
+    distance_matrix : DistanceMatrix
+        Distance matrix containing distances between objects (e.g., distances
+        between samples of microbial communities).
+    data_frame : pandas.DataFrame
+        Contains columns of variables (e.g., numeric environmental variables
+        such as pH) associated with the objects in `distance_matrix`. Must be
+        indexed by the IDs in `distance_matrix` (i.e., the row labels must be
+        distance matrix IDs), but the order of IDs between `distance_matrix`
+        and `data_frame` need not be the same. All IDs in the distance matrix
+        must be present in `data_frame`. Extra rows in `data_frame` are allowed
+        (they are ignored in the calculations).
+    columns : iterable of strs, optional
+        Column names in `data_frame` to include as variables in the
+        calculations. If not provided, defaults to all columns in `data_frame`.
+        Each column must be numeric.
+
+    Returns
+    -------
+    pandas.DataFrame
+        Data frame containing the "best" subset of variables at each subset
+        size, as well as the correlation coefficient.
+
+    Raises
+    ------
+    TypeError
+        If invalid input types are provided, or if one or more specified
+        columns in `data_frame` are not numeric.
+    ValueError
+        If column name(s) or `distance_matrix` IDs cannot be found in
+        `data_frame`, or if there is missing data (``NaN``) in the
+        environmental variables.
+
+    See Also
+    --------
+    scipy.stats.spearmanr
+
+    Notes
+    -----
+    See [1]_ for the original method reference (originally called BIO-ENV).
+    The general algorithm and interface are similar to ``vegan::bioenv``,
+    available in R's vegan package [2]_. This method can also be found in
+    PRIMER-E [3]_ (originally called BIO-ENV, but is now called BEST).
+
+    .. warning:: This method can take a *long* time to run if a large number of
+       variables are specified, as all possible subsets are evaluated at each
+       subset size.
+
+    References
+    ----------
+    .. [1] Clarke, K. R & Ainsworth, M. 1993. "A method of linking multivariate
+       community structure to environmental variables". Marine Ecology Progress
+       Series, 92, 205-219.
+
+    .. [2] http://cran.r-project.org/web/packages/vegan/index.html
+
+    .. [3] http://www.primer-e.com/primer.htm
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> from skbio.core.distance import DistanceMatrix
+    >>> from skbio.math.stats.distance import bioenv
+
+    Load a 4x4 community distance matrix:
+
+    >>> dm = DistanceMatrix([[0.0, 0.5, 0.25, 0.75],
+    ...                      [0.5, 0.0, 0.1, 0.42],
+    ...                      [0.25, 0.1, 0.0, 0.33],
+    ...                      [0.75, 0.42, 0.33, 0.0]],
+    ...                     ['A', 'B', 'C', 'D'])
+
+    Load a ``pandas.DataFrame`` with two environmental variables, pH and
+    elevation:
+
+    >>> df = pd.DataFrame([[7.0, 400],
+    ...                    [8.0, 530],
+    ...                    [7.5, 450],
+    ...                    [8.5, 810]],
+    ...                   index=['A','B','C','D'],
+    ...                   columns=['pH', 'Elevation'])
+
+    Note that the data frame is indexed with the same IDs (``'A'``, ``'B'``,
+    ``'C'``, and ``'D'``) that are in the distance matrix. This is necessary in
+    order to link the environmental variables (metadata) to each of the objects
+    in the distance matrix. In this example, the IDs appear in the same order
+    in both the distance matrix and data frame, but this is not necessary.
+
+    Find the best subsets of environmental variables that are correlated with
+    community distances:
+
+    >>> bioenv(dm, df)
+                   size  correlation
+    pH                1     0.771517
+    pH, Elevation     2     0.714286
+    <BLANKLINE>
+    [2 rows x 2 columns]
+
+    We see that in this simple example, pH alone is maximally rank-correlated
+    with the community distances.
+
+    """
+    if not isinstance(distance_matrix, DistanceMatrix):
+        raise TypeError("Must provide a DistanceMatrix as input.")
+    if not isinstance(data_frame, pd.DataFrame):
+        raise TypeError("Must provide a pandas.DataFrame as input.")
+
+    if columns is None:
+        columns = data_frame.columns.values.tolist()
+
+    if len(set(columns)) != len(columns):
+        raise ValueError("Duplicate column names are not supported.")
+
+    if len(columns) < 1:
+        raise ValueError("Must provide at least one column.")
+
+    for column in columns:
+        if column not in data_frame:
+            raise ValueError("Column '%s' not in data frame." % column)
+
+    vars_df = data_frame.loc[distance_matrix.ids, columns]
+
+    if vars_df.isnull().any().any():
+        raise ValueError("One or more IDs in the distance matrix are not "
+                         "in the data frame, or there is missing data in the "
+                         "data frame.")
+
+    try:
+        vars_df = vars_df.astype(float)
+    except ValueError:
+        raise TypeError("All specified columns in the data frame must be "
+                        "numeric.")
+
+    # From http://stackoverflow.com/a/18017059
+    #vars_df = pd.DataFrame(scale(vars_df), index=vars_df.index,
+    #                       columns=vars_df.columns)
+    vars_df = scale(vars_df)
+    #print(vars_df)
+    dm_flat = distance_matrix.condensed_form()
+
+    num_vars = len(columns)
+    var_idxs = np.arange(num_vars)
+    max_rhos = []
+    max_vars = []
+    for subset_size in range(1, num_vars + 1):
+        # TODO performance test this way vs. generating all rhos in np array
+        # and choosing max
+        max_rho = (None, None)
+        for subset_idxs in combinations(var_idxs, subset_size):
+            vars_array = vars_df.iloc[:, subset_idxs].values
+            vars_dm_flat = pdist(vars_array, metric='euclidean')
+            rho = spearmanr(dm_flat, vars_dm_flat)[0]
+
+            if max_rho == (None, None) or rho > max_rho[0]:
+                max_rho = (rho, subset_idxs)
+        max_rhos.append((subset_size, max_rho[0]))
+        max_vars.append(', '.join([columns[i] for i in max_rho[1]]))
+
+    return pd.DataFrame.from_records(max_rhos, index=max_vars,
+                                     columns=('size', 'correlation'))
+
+
+def scale(y, c=True, sc=True):
+    x = y.copy()
+
+    if c:
+        x -= x.mean()
+    if sc and c:
+        x /= x.std()
+    elif sc:
+        x /= np.sqrt(x.pow(2).sum().div(x.count() - 1))
+    return x
--- skbio/math/stats/distance/tests/test_base.py
+++ skbio/math/stats/distance/tests/test_base.py
@@ -8,15 +8,19 @@
 # The full license is in the file COPYING.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 
-from __future__ import division
+from __future__ import absolute_import, division, print_function
 from future.utils.six import StringIO
 from unittest import TestCase, main
 
+import numpy as np
 import pandas as pd
+from pandas.util.testing import assert_frame_equal
+
 
 from skbio.core.distance import DissimilarityMatrix, DistanceMatrix
 from skbio.math.stats.distance.base import (CategoricalStats,
-                                            CategoricalStatsResults)
+                                            CategoricalStatsResults, bioenv)
+from skbio.util.testing import get_data_path
 
 
 class CategoricalStatsTests(TestCase):
@@ -139,6 +143,140 @@ class CategoricalStatsResultsTests(TestCase):
     def test_format_p_value_none(self):
         obs = self.results._format_p_value(None, 0)
         self.assertEqual(obs, 'N/A')
+
+
+class BIOENVTests(TestCase):
+    """Results were verified with R 3.0.2 and vegan 2.0-10 (vegan::bioenv)."""
+
+    def setUp(self):
+        # The test dataset used here is a subset of the Lauber et al. 2009
+        # "88 Soils" dataset. It has been altered to exercise various aspects
+        # of the code, including (but not limited to):
+        #
+        # - order of distance matrix IDs and IDs in data frame (metadata) are
+        #   not exactly the same
+        # - data frame has an extra sample that is not in the distance matrix
+        # - this extra sample has non-numeric and missing values in some of its
+        #   cells
+        #
+        # Additional variations of the distance matrix and data frame are used
+        # to test different orderings of rows/columns, extra non-numeric data
+        # frame columns, etc.
+        #
+        # This dataset is also useful because it is non-trivial in size (6
+        # samples, 11 environment variables) and it includes positive/negative
+        # floats and integers in the data frame.
+
+        self.dm = DistanceMatrix.from_file(get_data_path('dm.txt'))
+
+        # Reordered rows and columns (i.e., different ID order). Still
+        # conceptually the same distance matrix.
+        self.dm_reordered = DistanceMatrix.from_file(
+            get_data_path('dm_reordered.txt'))
+
+        self.df = pd.read_csv(get_data_path('df.txt'), sep='\t',
+                                       index_col=0)
+
+        # Similar to the above data frame, except that it has an extra
+        # non-numeric column, and some of the other rows and columns have been
+        # reordered.
+        self.df_extra_column = pd.read_csv(
+            get_data_path('df_extra_column.txt'), sep='\t', index_col=0)
+
+        # All columns in the original data frame (these are all numeric
+        # columns).
+        self.cols = self.df.columns.tolist()
+
+        self.exp_results = pd.read_csv(get_data_path('exp_results.txt'),
+                                       sep='\t', index_col=0)
+        self.exp_results_single_column = pd.read_csv(
+            get_data_path('exp_results_single_column.txt'), sep='\t',
+            index_col=0)
+        self.exp_results_different_column_order = pd.read_csv(
+            get_data_path('exp_results_different_column_order.txt'), sep='\t',
+            index_col=0)
+
+    def test_bioenv_all_columns_implicit(self):
+        # Test with all columns in data frame (implicitly).
+        obs = bioenv(self.dm, self.df)
+        assert_frame_equal(obs, self.exp_results)
+
+        # Should get the same results if order of rows/cols in distance matrix
+        # is changed.
+        obs = bioenv(self.dm_reordered, self.df)
+        assert_frame_equal(obs, self.exp_results)
+
+    def test_bioenv_all_columns_explicit(self):
+        # Test with all columns being specified.
+        obs = bioenv(self.dm, self.df, columns=self.cols)
+        assert_frame_equal(obs, self.exp_results)
+
+        # Test against a data frame that has an extra non-numeric column and
+        # some of the rows and columns reordered (we should get the same
+        # result since we're specifying the same columns in the same order).
+        obs = bioenv(self.dm, self.df_extra_column, columns=self.cols)
+        assert_frame_equal(obs, self.exp_results)
+
+    def test_bioenv_single_column(self):
+        obs = bioenv(self.dm, self.df, columns=['PH'])
+        assert_frame_equal(obs, self.exp_results_single_column)
+
+    def test_bioenv_different_column_order(self):
+        # Specifying columns in a different order will change the row labels in
+        # the results data frame as the column subsets will be reordered, but
+        # the actual results (e.g., correlation coefficients) shouldn't change.
+        obs = bioenv(self.dm, self.df, columns=self.cols[::-1])
+        assert_frame_equal(obs, self.exp_results_different_column_order)
+
+    def test_bioenv_no_side_effects(self):
+        # Deep copies of both primary inputs.
+        dm_copy = self.dm.copy()
+        df_copy = self.df.copy(deep=True)
+
+        bioenv(self.dm, self.df)
+
+        # Make sure we haven't modified the primary input in some way (e.g.,
+        # with scaling, type conversions, etc.).
+        self.assertEqual(self.dm, dm_copy)
+        assert_frame_equal(self.df, df_copy)
+
+    def test_bioenv_no_distance_matrix(self):
+        with self.assertRaises(TypeError):
+            bioenv('breh', self.df)
+
+    def test_bioenv_no_data_frame(self):
+        with self.assertRaises(TypeError):
+            bioenv(self.dm, None)
+
+    def test_bioenv_duplicate_columns(self):
+        with self.assertRaises(ValueError):
+            bioenv(self.dm, self.df, columns=self.cols + ['PH'])
+
+    def test_bioenv_no_columns(self):
+        with self.assertRaises(ValueError):
+            bioenv(self.dm, self.df, columns=[])
+
+    def test_bioenv_missing_columns(self):
+        with self.assertRaises(ValueError):
+            bioenv(self.dm, self.df, columns=self.cols + ['brofist'])
+
+    def test_bioenv_missing_distance_matrix_ids(self):
+        df = self.df[1:]
+        with self.assertRaises(ValueError):
+            bioenv(self.dm, df)
+
+    def test_bioenv_nans(self):
+        df = self.df.replace(53.9, np.nan)
+        with self.assertRaises(ValueError):
+            bioenv(self.dm, df)
+
+    def test_bioenv_nonnumeric_columns(self):
+        df = self.df.replace(2400, 'no cog yay')
+        with self.assertRaises(TypeError):
+            bioenv(self.dm, df)
+
+        with self.assertRaises(TypeError):
+            bioenv(self.dm, self.df_extra_column)
 
 
 if __name__ == '__main__':
