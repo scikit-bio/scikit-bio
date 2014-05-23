3fac4f714449fcccd62284a6b1646d43da28039c
Jai Ram Rideout <jai.rideout@gmail.com>
2014-05-23 13:18:00 -0400

    Rename test data and test vars
    Shorter, simpler names since I ended up just using a single test dataset with minor variations.

--- skbio/math/stats/distance/tests/test_bioenv.py
+++ skbio/math/stats/distance/tests/test_bioenv.py
@@ -26,115 +26,109 @@ class BIOENVTests(TestCase):
 
     def setUp(self):
         # TODO add description of test dataset
-        self.dm_88_soils = DistanceMatrix.from_file(
-            get_data_path('88_soils_dm.txt'))
-        self.dm_88_soils_reordered = DistanceMatrix.from_file(
-            get_data_path('88_soils_dm_reordered.txt'))
+        self.dm = DistanceMatrix.from_file(get_data_path('dm.txt'))
+        self.dm_reordered = DistanceMatrix.from_file(
+            get_data_path('dm_reordered.txt'))
 
-        self.df_88_soils = pd.read_csv(get_data_path('88_soils_df.txt'),
-                                       sep='\t', index_col=0)
-        self.df_88_soils_extra_column = pd.read_csv(
-            get_data_path('88_soils_df_extra_column.txt'), sep='\t',
-            index_col=0)
+        self.df = pd.read_csv(get_data_path('df.txt'), sep='\t',
+                                       index_col=0)
+        self.df_extra_column = pd.read_csv(
+            get_data_path('df_extra_column.txt'), sep='\t', index_col=0)
         self.cols = ['TOT_ORG_CARB', 'SILT_CLAY', 'ELEVATION',
                      'SOIL_MOISTURE_DEFICIT', 'CARB_NITRO_RATIO',
                      'ANNUAL_SEASON_TEMP', 'ANNUAL_SEASON_PRECPT', 'PH',
                      'CMIN_RATE', 'LONGITUDE', 'LATITUDE']
 
-        self.exp_88_soils = pd.read_csv(
-            get_data_path('88_soils_exp_results.txt'), sep='\t', index_col=0)
-        self.exp_88_soils_single_column = pd.read_csv(
-            get_data_path('88_soils_single_column_exp_results.txt'), sep='\t',
+        self.exp_results = pd.read_csv(get_data_path('exp_results.txt'),
+                                       sep='\t', index_col=0)
+        self.exp_results_single_column = pd.read_csv(
+            get_data_path('exp_results_single_column.txt'), sep='\t',
             index_col=0)
-        self.exp_88_soils_different_column_order = pd.read_csv(
-            get_data_path('88_soils_different_column_order_exp_results.txt'),
-            sep='\t', index_col=0)
+        self.exp_results_different_column_order = pd.read_csv(
+            get_data_path('exp_results_different_column_order.txt'), sep='\t',
+            index_col=0)
 
     def test_bioenv_all_columns_implicit(self):
         # Test with all columns in data frame (implicitly).
-        obs = bioenv(self.dm_88_soils, self.df_88_soils)
-        assert_frame_equal(obs, self.exp_88_soils)
+        obs = bioenv(self.dm, self.df)
+        assert_frame_equal(obs, self.exp_results)
 
         # Should get the same results if order of rows/cols in distance matrix
         # is changed.
-        obs = bioenv(self.dm_88_soils_reordered, self.df_88_soils)
-        assert_frame_equal(obs, self.exp_88_soils)
+        obs = bioenv(self.dm_reordered, self.df)
+        assert_frame_equal(obs, self.exp_results)
 
     def test_bioenv_all_columns_explicit(self):
         # Test with all columns being specified.
-        obs = bioenv(self.dm_88_soils, self.df_88_soils, columns=self.cols)
-        assert_frame_equal(obs, self.exp_88_soils)
+        obs = bioenv(self.dm, self.df, columns=self.cols)
+        assert_frame_equal(obs, self.exp_results)
 
         # Test against a data frame that has an extra non-numeric column and
         # some of the rows and columns reordered (we should get the same
         # result since we're specifying the same columns in the same order).
-        obs = bioenv(self.dm_88_soils, self.df_88_soils_extra_column,
-                     columns=self.cols)
-        assert_frame_equal(obs, self.exp_88_soils)
+        obs = bioenv(self.dm, self.df_extra_column, columns=self.cols)
+        assert_frame_equal(obs, self.exp_results)
 
     def test_bioenv_single_column(self):
-        obs = bioenv(self.dm_88_soils, self.df_88_soils, columns=['PH'])
-        assert_frame_equal(obs, self.exp_88_soils_single_column)
+        obs = bioenv(self.dm, self.df, columns=['PH'])
+        assert_frame_equal(obs, self.exp_results_single_column)
 
     def test_bioenv_different_column_order(self):
         # Specifying columns in a different order will change the row labels in
         # the results data frame as the column subsets will be reordered, but
         # the actual results (e.g., correlation coefficients) shouldn't change.
-        obs = bioenv(self.dm_88_soils, self.df_88_soils,
-                     columns=self.cols[::-1])
-        assert_frame_equal(obs, self.exp_88_soils_different_column_order)
+        obs = bioenv(self.dm, self.df, columns=self.cols[::-1])
+        assert_frame_equal(obs, self.exp_results_different_column_order)
 
     def test_bioenv_no_side_effects(self):
         # Deep copies of both primary inputs.
-        dm_copy = self.dm_88_soils.copy()
-        df_copy = self.df_88_soils.copy(deep=True)
+        dm_copy = self.dm.copy()
+        df_copy = self.df.copy(deep=True)
 
-        bioenv(self.dm_88_soils, self.df_88_soils)
+        bioenv(self.dm, self.df)
 
         # Make sure we haven't modified the primary input in some way (e.g.,
         # with scaling, type conversions, etc.).
-        self.assertEqual(self.dm_88_soils, dm_copy)
-        assert_frame_equal(self.df_88_soils, df_copy)
+        self.assertEqual(self.dm, dm_copy)
+        assert_frame_equal(self.df, df_copy)
 
     def test_bioenv_no_distance_matrix(self):
         with self.assertRaises(TypeError):
-            bioenv('breh', self.df_88_soils)
+            bioenv('breh', self.df)
 
     def test_bioenv_no_data_frame(self):
         with self.assertRaises(TypeError):
-            bioenv(self.dm_88_soils, None)
+            bioenv(self.dm, None)
 
     def test_bioenv_duplicate_columns(self):
         with self.assertRaises(ValueError):
-            bioenv(self.dm_88_soils, self.df_88_soils,
-                   columns=self.cols + ['PH'])
+            bioenv(self.dm, self.df, columns=self.cols + ['PH'])
 
     def test_bioenv_no_columns(self):
         with self.assertRaises(ValueError):
-            bioenv(self.dm_88_soils, self.df_88_soils, columns=[])
+            bioenv(self.dm, self.df, columns=[])
 
     def test_bioenv_missing_columns(self):
         with self.assertRaises(ValueError):
-            bioenv(self.dm_88_soils, self.df_88_soils,
-                   columns=self.cols + ['brofist'])
+            bioenv(self.dm, self.df, columns=self.cols + ['brofist'])
 
     def test_bioenv_missing_distance_matrix_ids(self):
-        df = self.df_88_soils[1:]
+        df = self.df[1:]
         with self.assertRaises(ValueError):
-            bioenv(self.dm_88_soils, df)
+            bioenv(self.dm, df)
 
     def test_bioenv_nans(self):
-        df = self.df_88_soils.replace(53.9, np.nan)
+        df = self.df.replace(53.9, np.nan)
         with self.assertRaises(ValueError):
-            bioenv(self.dm_88_soils, df)
+            bioenv(self.dm, df)
 
     def test_bioenv_nonnumeric_columns(self):
-        df = self.df_88_soils.replace(2400, 'no cog yay')
+        df = self.df.replace(2400, 'no cog yay')
         with self.assertRaises(TypeError):
-            bioenv(self.dm_88_soils, df)
+            bioenv(self.dm, df)
 
         with self.assertRaises(TypeError):
-            bioenv(self.dm_88_soils, self.df_88_soils_extra_column)
+            bioenv(self.dm, self.df_extra_column)
 
 
 if __name__ == '__main__':
