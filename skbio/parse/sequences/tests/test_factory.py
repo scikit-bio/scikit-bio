89be689ef3ceccddea992548430faabcbd0f3ed8
charudatta-navare <charudatta.navare@gmail.com>
2014-06-11 17:23:53 -0400

    _factory.py

--- skbio/parse/sequences/tests/test_factory.py
+++ skbio/parse/sequences/tests/test_factory.py
@@ -67,7 +67,6 @@ class SequenceLoadTests(TestCase):
             self.assertEqual(o['QualID'], e['QualID'])
             self.assertTrue((o['Qual'] == e['Qual']).all())
 
-        map(lambda x: x.close(), it)
 
     def test_multiple_files(self):
         """load should handle multiple files of different types"""
@@ -110,7 +109,6 @@ class SequenceLoadTests(TestCase):
         self.assertEqual(o['QualID'], e['QualID'])
         self.assertEqual(o['Qual'], e['Qual'])
 
-        map(lambda x: x.close(), it)
 
     def test_transform(self):
         """load should pass transform methods to the iterators"""
@@ -157,7 +155,6 @@ class SequenceLoadTests(TestCase):
         self.assertEqual(o['QualID'], e['QualID'])
         self.assertEqual(o['Qual'], e['Qual'])
 
-        map(lambda x: x.close(), it)
 
     def test_force_constructor(self):
         it = load([self.noext], constructor=FastaIterator)
@@ -167,7 +164,7 @@ class SequenceLoadTests(TestCase):
                {'Sequence': 'ATATA', 'SequenceID': 'seq2',
                 'Qual': None, 'QualID': None}]
         self.assertEqual(obs, exp)
-        map(lambda x: x.close(), it)
+
 
 
 if __name__ == '__main__':
