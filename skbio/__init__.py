833813401070efba425ed9ae9d6bac6976e13cc8
Greg Caporaso <gregcaporaso@gmail.com>
2014-06-16 20:21:29 -0700

    ENH: porting python alignment functions from IAB
    see #457. this commit provides first working version of ported smith-waterman
    w/ affine gap alignment. a lot of work to do from here to get it ready for
    scikit-bio.

--- skbio/__init__.py
+++ skbio/__init__.py
@@ -62,6 +62,8 @@ from skbio.core.sequence import (
 from skbio.core.distance import DistanceMatrix
 from skbio.core.alignment import (
     align_striped_smith_waterman, SequenceCollection, Alignment)
+from skbio.core.alignment.pairwise import (
+    local_pairwise_align_nucleotide, local_pairwise_align_protein)
 from skbio.core.tree import (
     TreeNode, nj)
 from skbio.parse.sequences import (
---
+++ skbio/core/alignment/pairwise.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------------
+# Copyright (c) 2013--, scikit-bio development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from __future__ import absolute_import, division, print_function
+
+
+blosum50 = {'A': {'A': 5, 'C': -1, 'D': -2, 'E': -1, 'F': -3, 'G': 0, 'H': -2, 'I': -1, 'K': -1, 'L': -2, 'M': -1, 'N': -1, 'P': -1, 'Q': -1, 'R': -2, 'S': 1, 'T': 0, 'V': 0, 'W': -3, 'Y': -2},
+'C': {'A': -1, 'C': 13, 'D': -4, 'E': -3, 'F': -2, 'G': -3, 'H': -3, 'I': -2, 'K': -3, 'L': -2, 'M': -2, 'N': -2, 'P': -4, 'Q': -3, 'R': -4, 'S': -1, 'T': -1, 'V': -1, 'W': -5, 'Y': -3},
+'D': {'A': -2, 'C': -4, 'D': 8, 'E': 2, 'F': -5, 'G': -1, 'H': -1, 'I': -4, 'K': -1, 'L': -4, 'M': -4, 'N': 2, 'P': -1, 'Q': 0, 'R': -2, 'S': 0, 'T': -1, 'V': -4, 'W': -5, 'Y': -3},
+'E': {'A': -1, 'C': -3, 'D': 2, 'E': 6, 'F': -3, 'G': -3, 'H': 0, 'I': -4, 'K': 1, 'L': -3, 'M': -2, 'N': 0, 'P': -1, 'Q': 2, 'R': 0, 'S': -1, 'T': -1, 'V': -3, 'W': -3, 'Y': -2},
+'F': {'A': -3, 'C': -2, 'D': -5, 'E': -3, 'F': 8, 'G': -4, 'H': -1, 'I': 0, 'K': -4, 'L': 1, 'M': 0, 'N': -4, 'P': -4, 'Q': -4, 'R': -3, 'S': -3, 'T': -2, 'V': -1, 'W': 1, 'Y': 4},
+'G': {'A': 0, 'C': -3, 'D': -1, 'E': -3, 'F': -4, 'G': 8, 'H': -2, 'I': -4, 'K': -2, 'L': -4, 'M': -3, 'N': 0, 'P': -2, 'Q': -2, 'R': -3, 'S': 0, 'T': -2, 'V': -4, 'W': -3, 'Y': -3},
+'H': {'A': -2, 'C': -3, 'D': -1, 'E': 0, 'F': -1, 'G': -2, 'H': 10, 'I': -4, 'K': 0, 'L': -3, 'M': -1, 'N': 1, 'P': -2, 'Q': 1, 'R': 0, 'S': -1, 'T': -2, 'V': -4, 'W': -3, 'Y': 2},
+'I': {'A': -1, 'C': -2, 'D': -4, 'E': -4, 'F': 0, 'G': -4, 'H': -4, 'I': 5, 'K': -3, 'L': 2, 'M': 2, 'N': -3, 'P': -3, 'Q': -3, 'R': -4, 'S': -3, 'T': -1, 'V': 4, 'W': -3, 'Y': -1},
+'K': {'A': -1, 'C': -3, 'D': -1, 'E': 1, 'F': -4, 'G': -2, 'H': 0, 'I': -3, 'K': 6, 'L': -3, 'M': -2, 'N': 0, 'P': -1, 'Q': 2, 'R': 3, 'S': 0, 'T': -1, 'V': -3, 'W': -3, 'Y': -2},
+'L': {'A': -2, 'C': -2, 'D': -4, 'E': -3, 'F': 1, 'G': -4, 'H': -3, 'I': 2, 'K': -3, 'L': 5, 'M': 3, 'N': -4, 'P': -4, 'Q': -2, 'R': -3, 'S': -3, 'T': -1, 'V': 1, 'W': -2, 'Y': -1},
+'M': {'A': -1, 'C': -2, 'D': -4, 'E': -2, 'F': 0, 'G': -3, 'H': -1, 'I': 2, 'K': -2, 'L': 3, 'M': 7, 'N': -2, 'P': -3, 'Q': 0, 'R': -2, 'S': -2, 'T': -1, 'V': 1, 'W': -1, 'Y': 0},
+'N': {'A': -1, 'C': -2, 'D': 2, 'E': 0, 'F': -4, 'G': 0, 'H': 1, 'I': -3, 'K': 0, 'L': -4, 'M': -2, 'N': 7, 'P': -2, 'Q': 0, 'R': -1, 'S': 1, 'T': 0, 'V': -3, 'W': -4, 'Y': -2},
+'P': {'A': -1, 'C': -4, 'D': -1, 'E': -1, 'F': -4, 'G': -2, 'H': -2, 'I': -3, 'K': -1, 'L': -4, 'M': -3, 'N': -2, 'P': 10, 'Q': -1, 'R': -3, 'S': -1, 'T': -1, 'V': -3, 'W': -4, 'Y': -3},
+'Q': {'A': -1, 'C': -3, 'D': 0, 'E': 2, 'F': -4, 'G': -2, 'H': 1, 'I': -3, 'K': 2, 'L': -2, 'M': 0, 'N': 0, 'P': -1, 'Q': 7, 'R': 1, 'S': 0, 'T': -1, 'V': -3, 'W': -1, 'Y': -1},
+'R': {'A': -2, 'C': -4, 'D': -2, 'E': 0, 'F': -3, 'G': -3, 'H': 0, 'I': -4, 'K': 3, 'L': -3, 'M': -2, 'N': -1, 'P': -3, 'Q': 1, 'R': 7, 'S': -1, 'T': -1, 'V': -3, 'W': -3, 'Y': -1},
+'S': {'A': 1, 'C': -1, 'D': 0, 'E': -1, 'F': -3, 'G': 0, 'H': -1, 'I': -3, 'K': 0, 'L': -3, 'M': -2, 'N': 1, 'P': -1, 'Q': 0, 'R': -1, 'S': 5, 'T': 2, 'V': -2, 'W': -4, 'Y': -2},
+'T': {'A': 0, 'C': -1, 'D': -1, 'E': -1, 'F': -2, 'G': -2, 'H': -2, 'I': -1, 'K': -1, 'L': -1, 'M': -1, 'N': 0, 'P': -1, 'Q': -1, 'R': -1, 'S': 2, 'T': 5, 'V': 0, 'W': -3, 'Y': -2},
+'V': {'A': 0, 'C': -1, 'D': -4, 'E': -3, 'F': -1, 'G': -4, 'H': -4, 'I': 4, 'K': -3, 'L': 1, 'M': 1, 'N': -3, 'P': -3, 'Q': -3, 'R': -3, 'S': -2, 'T': 0, 'V': 5, 'W': -3, 'Y': -1},
+'W': {'A': -3, 'C': -5, 'D': -5, 'E': -3, 'F': 1, 'G': -3, 'H': -3, 'I': -3, 'K': -3, 'L': -2, 'M': -1, 'N': -4, 'P': -4, 'Q': -1, 'R': -3, 'S': -4, 'T': -3, 'V': -3, 'W': 15, 'Y': 2},
+'Y': {'A': -2, 'C': -3, 'D': -3, 'E': -2, 'F': 4, 'G': -3, 'H': 2, 'I': -1, 'K': -2, 'L': -1, 'M': 0, 'N': -2, 'P': -3, 'Q': -1, 'R': -1, 'S': -2, 'T': -2, 'V': -1, 'W': 2, 'Y': 8}}
+
+nt_substitution_matrix = {'A': {'A':  1, 'C': -2, 'G': -2, 'T': -2, 'N': 0},
+                          'C': {'A': -2, 'C':  1, 'G': -2, 'T': -2, 'N': 0},
+                          'G': {'A': -2, 'C': -2, 'G':  1, 'T': -2, 'N': 0},
+                          'T': {'A': -2, 'C': -2, 'G': -2, 'T':  1, 'N': 0},
+                          'N': {'A':  0, 'C':  0, 'G':  0, 'T':  0, 'N': 0 }}
+
+
+def local_pairwise_align_nucleotide(sequence1, sequence2, gap_open_penalty=5,
+                                    gap_extend_penalty=2,
+                                    substitution_matrix=None):
+    """Locally align nucleotide seqs using Smith-Waterman w/ affine gap scoring
+
+       Parameters
+       ----------
+       sequence1 : str or BiologicalSequence
+           The first unaligned sequence
+       sequence2 : str or BiologicalSequence
+           The second unaligned sequence
+       gap_open_penalty : int, float, optional
+           penalty for opening a gap (this is substracted from previous best
+           alignment score, so is typically positive)
+       gap_extend_penalty : int, float, optional
+           penalty for extending a gap (this is substracted from previous best
+           alignment score, so is typically positive)
+       substitution_matrix: 2D dict (or similar), optional
+           lookup for substitution scores (these values are added to the
+           previous best alignment score); default is nt_substitution_matrix
+
+       Returns
+       -------
+       string
+          The first aligned sequence
+       string
+          The second aligned sequence
+       float
+          The score of the alignment
+       int
+          The start position of the alignment in sequence 1
+       int
+          The start position of the alignment in sequence 2
+
+       Examples
+       --------
+       >>> from skbio import local_pairwise_align_nucleotide
+       >>> s1 = "GCGTGCCTAAGGTATGCAAG"
+       >>> s2 = "ACGTGCCTAGGTACGCAAG"
+       >>> r = local_pairwise_align_nucleotide(s1, s2)
+       >>> print(r[0])
+       CGTGCCTAAGGTATGCAAG
+       >>> print(r[1])
+       CGTGCCT-AGGTACGCAAG
+
+    """
+    if substitution_matrix is None:
+        substitution_matrix = nt_substitution_matrix
+
+    return local_pairwise_align(sequence1, sequence2, gap_open_penalty,
+                                gap_extend_penalty, substitution_matrix)
+
+
+def local_pairwise_align_protein(sequence1, sequence2, gap_open_penalty=11,
+                                 gap_extend_penalty=1,
+                                 substitution_matrix=None):
+    """Locally align two protein seqs (Smith-Waterman w affine gap scoring)
+
+       Parameters
+       ----------
+       sequence1 : string
+           The first unaligned sequence
+       sequence2 : string
+           The second unaligned sequence
+       gap_open_penalty : int, float, optional
+           penalty for opening a gap (this is substracted from previous best
+           alignment score, so is typically positive)
+       gap_extend_penalty : int, float, optional
+           penalty for extending a gap (this is substracted from previous best
+           alignment score, so is typically positive)
+       substitution_matrix: 2D dict (or similar), optional
+           lookup for substitution scores (these values are added to the
+           previous best alignment score)
+
+       Returns
+       -------
+       string
+          The first aligned sequence
+       string
+          The second aligned sequence
+       float
+          The score of the alignment
+       int
+          The start position of the alignment in sequence 1
+       int
+          The start position of the alignment in sequence 2
+
+    """
+    if substitution_matrix is None:
+        substitution_matrix = blosum50
+
+    return local_pairwise_align(sequence1, sequence2, gap_open_penalty,
+                                gap_extend_penalty, substitution_matrix)
+
+
+def local_pairwise_align(sequence1, sequence2, gap_open_penalty,
+                         gap_extend_penalty, substitution_matrix):
+    """Locally align sequences using Smith-Waterman w/ affine gap scoring
+
+       Parameters
+       ----------
+       sequence1 : str or BiologicalSequence
+           The first unaligned sequence
+       sequence2 : str or BiologicalSequence
+           The second unaligned sequence
+       gap_open_penalty : int, float
+           penalty for opening a gap (this is substracted from previous best
+           alignment score, so is typically positive)
+       gap_extend_penalty : int, float
+           penalty for extending a gap (this is substracted from previous best
+           alignment score, so is typically positive)
+       substitution_matrix: 2D dict (or similar)
+           lookup for substitution scores (these values are added to the
+           previous best alignment score); default is nt_substitution_matrix
+
+       Returns
+       -------
+       string
+          The first aligned sequence
+       string
+          The second aligned sequence
+       float
+          The score of the alignment
+       int
+          The start position of the alignment in sequence 1
+       int
+          The start position of the alignment in sequence 2
+    """
+    sw_matrix, traceback_matrix = \
+        _local_dynamic_programming_and_traceback(sequence1,
+                                                 sequence2,
+                                                 gap_open_penalty,
+                                                 gap_extend_penalty,
+                                                 substitution_matrix)
+
+    return _sw_traceback(traceback_matrix,sw_matrix,sequence1,sequence2)
+
+def _local_dynamic_programming_and_traceback(seq1, seq2, gap_open_penalty,
+                                             gap_extend_penalty,
+                                             substitution_matrix):
+    """Return dynamic programming and traceback matrices
+
+    These are generated for local alignment using Smith-Waterman.
+    """
+    # Initialize a matrix to use for scoring the alignment and for tracing
+    # back the best alignment
+    sw_matrix = [[0 for i in range(0,len(seq1)+1)]]
+    traceback_matrix = [[None] + [None for i in range(0,len(seq1))]]
+    # Iterate over the amino acids in sequence two (which will correspond
+    # to the vertical sequence in the matrix)
+    # Note that i corresponds to column numbers, as in 'Biological Sequence
+    # Analysis'
+    for i,aa2 in zip(range(1,len(seq2)+1),seq2):
+        # Initialize the current row of the matrix
+        current_row = [0]
+        current_traceback_matrix_row = [None]
+        # Iterate over the amino acids in sequence one (which will
+        # correspond to the horizontal sequence in the matrix)
+        # Note that j corresponds to row numbers, as in 'Biological Sequence
+        # Analysis'
+        new_alignment_score = (0,None)
+        for j,aa1 in zip(range(1,len(seq1)+1),seq1):
+            substitution_score = substitution_matrix[aa1][aa2]
+            diag_score = (sw_matrix[i-1][j-1] + substitution_score,'\\')
+            if traceback_matrix[i-1][j] == '|':
+                # gap extend, because the cell above was also a gap
+                up_score = (sw_matrix[i-1][j] - gap_extend_penalty,'|')
+            else:
+                # gap open, because the cell above was not a gap
+                up_score = (sw_matrix[i-1][j] - gap_open_penalty,'|')
+            if current_traceback_matrix_row[-1] == '-':
+                # gap extend, because the cell to the left was also a gap
+                left_score = (current_row[-1] - gap_extend_penalty,'-')
+            else:
+                # gap open, because the cell to the left was not a gap
+                left_score = (current_row[-1] - gap_open_penalty,'-')
+            best_score = max(diag_score,up_score,left_score,new_alignment_score)
+            current_row.append(best_score[0])
+            current_traceback_matrix_row.append(best_score[1])
+        # append the current row to the matrix
+        sw_matrix.append(current_row)
+        traceback_matrix.append(current_traceback_matrix_row)
+    return sw_matrix, traceback_matrix
+
+def _sw_traceback(traceback_matrix,sw_matrix,seq1,seq2,gap_character='-'):
+
+    aligned_seq1 = []
+    aligned_seq2 = []
+
+    current_row = None
+    current_col = None
+    best_score = 0
+    for i in range(len(sw_matrix[0])):
+        for j in range(len(sw_matrix)):
+            current_score = sw_matrix[j][i]
+            if current_score > best_score:
+                best_score = current_score
+                current_row = j
+                current_col = i
+
+    while True:
+        current_value = traceback_matrix[current_row][current_col]
+
+        if current_value == '\\':
+            aligned_seq1.append(seq1[current_col-1])
+            aligned_seq2.append(seq2[current_row-1])
+            current_row -= 1
+            current_col -= 1
+        elif current_value == '|':
+            aligned_seq1.append('-')
+            aligned_seq2.append(seq2[current_row-1])
+            current_row -= 1
+        elif current_value == '-':
+            aligned_seq1.append(seq1[current_col-1])
+            aligned_seq2.append('-')
+            current_col -= 1
+        elif current_value == None:
+            break
+        else:
+            raise ValueError, "Invalid value in traceback matrix: %s" % current_value
+
+    return (''.join(aligned_seq1[::-1]), ''.join(aligned_seq2[::-1]),
+             best_score, current_col, current_row)
