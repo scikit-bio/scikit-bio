33cc3cc21a300ff39ed564204b8149ef6ad800e3
Greg Caporaso <gregcaporaso@gmail.com>
2014-08-07 17:38:41 -0700

    REV: reverting deprecation of PCoA, see #579 for discussion of this

--- skbio/stats/ordination/tests/test_ordination.py
+++ skbio/stats/ordination/tests/test_ordination.py
@@ -457,7 +457,6 @@ class TestPCoAResults(object):
         right"."""
         with warnings.catch_warnings():
             warnings.filterwarnings('ignore', category=RuntimeWarning)
-            warnings.filterwarnings('ignore', category=UserWarning)
             ordination = PCoA(self.dist_matrix)
         scores = ordination.scores()
 
@@ -482,9 +481,7 @@ class TestPCoAResultsExtensive(object):
         matrix = np.loadtxt(get_data_path('PCoA_sample_data_2'))
         self.ids = [str(i) for i in range(matrix.shape[0])]
         dist_matrix = DistanceMatrix(matrix, self.ids)
-        with warnings.catch_warnings():
-            warnings.filterwarnings('ignore', category=UserWarning)
-            self.ordination = PCoA(dist_matrix)
+        self.ordination = PCoA(dist_matrix)
 
     def test_values(self):
         results = self.ordination.scores()
@@ -521,9 +518,7 @@ class TestPCoAEigenResults(object):
         with open(get_data_path('PCoA_sample_data_3'), 'U') as lines:
             dist_matrix = DistanceMatrix.from_file(lines)
 
-        with warnings.catch_warnings():
-            warnings.filterwarnings('ignore', category=UserWarning)
-            self.ordination = PCoA(dist_matrix)
+        self.ordination = PCoA(dist_matrix)
 
         self.ids = ['PC.636', 'PC.635', 'PC.356', 'PC.481', 'PC.354', 'PC.593',
                     'PC.355', 'PC.607', 'PC.634']
@@ -569,10 +564,8 @@ class TestPCoAPrivateMethods(object):
 
 class TestPCoAErrors(object):
     def test_input(self):
-        with warnings.catch_warnings():
-            warnings.filterwarnings('ignore', category=UserWarning)
-            with npt.assert_raises(TypeError):
-                PCoA([[1, 2], [3, 4]])
+        with npt.assert_raises(TypeError):
+            PCoA([[1, 2], [3, 4]])
 
 
 class TestOrdinationResults(object):
