483156bbee136324f1f4a38e65af6b98ac2db7a5
Jai Ram Rideout <jai.rideout@gmail.com>
2014-08-22 10:18:28 -0700

    Add comment describing the workaround and linking to the Python 3 issue

--- setup.py
+++ setup.py
@@ -49,6 +49,12 @@ extensions = [
     Extension("skbio.alignment._ssw._ssw_wrapper",
               ["skbio/alignment/_ssw/_ssw_wrapper" + ext,
                "skbio/alignment/_ssw/ssw.c"],
+              # There's a bug in some versions of Python 3.4 that propagates
+              # -Werror=declaration-after-statement to extensions, instead of
+              # just affecting the compilation of the interpreter. See
+              # http://bugs.python.org/issue21121 for details. This acts as a
+              # workaround until the next Python 3 release -- thanks
+              # Wolfgang Maier (wolma) for the workaround!
               extra_compile_args=["-Wno-error=declaration-after-statement"])
 ]
 
