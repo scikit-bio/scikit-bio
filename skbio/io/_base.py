6a9111720851c00412726e04dee99e85317a31ab
Jai Ram Rideout <jai.rideout@gmail.com>
2014-10-23 15:25:57 -0700

    Refactor encoder and decoder to reduce duplication

--- skbio/io/_base.py
+++ skbio/io/_base.py
@@ -26,40 +26,15 @@ def _chunk_str(s, n, char):
 
 
 def _decode_qual_to_phred(qual_str, variant=None, phred_offset=None):
-    if variant is None and phred_offset is None:
-        raise ValueError(
-            "Must provide either `variant` or `phred_offset` in order to "
-            "decode quality scores.")
-    if variant is not None and phred_offset is not None:
-        raise ValueError(
-            "Cannot provide both `variant` and `phred_offset`.")
-
-    if variant is not None:
-        if variant == 'sanger':
-            phred_offset = 33
-            phred_range = (0, 93)
-        elif variant == 'illumina1.3':
-            phred_offset = 64
-            phred_range = (0, 62)
-        elif variant == 'illumina1.8':
-            phred_offset = 33
-            phred_range = (0, 62)
-        elif variant == 'solexa':
-            phred_offset = 64
-            phred_range = (-5, 62)
-            raise NotImplementedError(
-                "Decoding Solexa quality scores is not currently supported, "
-                "as quality scores are always stored as Phred scores in "
-                "scikit-bio. Please see the following scikit-bio issue to "
-                "track progress on this:\n\t"
-                "https://github.com/biocore/scikit-bio/issues/719")
-        else:
-            raise ValueError("Unrecognized variant %r." % variant)
-    else:
-        # Phred scores cannot be less than zero. There is no theoretical max to
-        # a Phred score, but more than a byte of information doesn't make
-        # sense.
-        phred_range = (0, 255)
+    phred_offset, phred_range = _get_phred_offset_and_range(
+        variant, phred_offset,
+        ["Must provide either `variant` or `phred_offset` in order to decode "
+         "quality scores.",
+         "Decoding Solexa quality scores is not currently supported, "
+         "as quality scores are always stored as Phred scores in "
+         "scikit-bio. Please see the following scikit-bio issue to "
+         "track progress on this:\n\t"
+         "https://github.com/biocore/scikit-bio/issues/719"])
 
     phred = []
     for c in qual_str:
@@ -73,39 +48,14 @@ def _decode_qual_to_phred(qual_str, variant=None, phred_offset=None):
 
 
 def _encode_phred_to_qual(phred, variant=None, phred_offset=None):
-    if variant is None and phred_offset is None:
-        raise ValueError(
-            "Must provide either `variant` or `phred_offset` in order to "
-            "encode Phred scores.")
-    if variant is not None and phred_offset is not None:
-        raise ValueError(
-            "Cannot provide both `variant` and `phred_offset`.")
-
-    if variant is not None:
-        if variant == 'sanger':
-            phred_offset = 33
-            phred_range = (0, 93)
-        elif variant == 'illumina1.3':
-            phred_offset = 64
-            phred_range = (0, 62)
-        elif variant == 'illumina1.8':
-            phred_offset = 33
-            phred_range = (0, 62)
-        elif variant == 'solexa':
-            phred_offset = 64
-            phred_range = (-5, 62)
-            raise NotImplementedError(
-                "Encoding Solexa quality scores is not currently supported. "
-                "Please see the following scikit-bio issue to track progress "
-                "on this:\n\t"
-                "https://github.com/biocore/scikit-bio/issues/719")
-        else:
-            raise ValueError("Unrecognized variant %r." % variant)
-    else:
-        # Phred scores cannot be less than zero. There is no theoretical max to
-        # a Phred score, but more than a byte of information doesn't make
-        # sense.
-        phred_range = (0, 255)
+    phred_offset, phred_range = _get_phred_offset_and_range(
+        variant, phred_offset,
+        ["Must provide either `variant` or `phred_offset` in order to encode "
+         "Phred scores.",
+         "Encoding Solexa quality scores is not currently supported. "
+         "Please see the following scikit-bio issue to track progress "
+         "on this:\n\t"
+         "https://github.com/biocore/scikit-bio/issues/719"])
 
     qual_chars = []
     for score in phred:
@@ -120,3 +70,35 @@ def _encode_phred_to_qual(phred, variant=None, phred_offset=None):
             score = phred_range[1]
         qual_chars.append(chr(score + phred_offset))
     return ''.join(qual_chars)
+
+
+def _get_phred_offset_and_range(variant, phred_offset, errors):
+    if variant is None and phred_offset is None:
+        raise ValueError(errors[0])
+    if variant is not None and phred_offset is not None:
+        raise ValueError(
+            "Cannot provide both `variant` and `phred_offset`.")
+
+    if variant is not None:
+        if variant == 'sanger':
+            phred_offset = 33
+            phred_range = (0, 93)
+        elif variant == 'illumina1.3':
+            phred_offset = 64
+            phred_range = (0, 62)
+        elif variant == 'illumina1.8':
+            phred_offset = 33
+            phred_range = (0, 62)
+        elif variant == 'solexa':
+            phred_offset = 64
+            phred_range = (-5, 62)
+            raise NotImplementedError(errors[1])
+        else:
+            raise ValueError("Unrecognized variant %r." % variant)
+    else:
+        # Phred scores cannot be less than zero. There is no theoretical max to
+        # a Phred score, but more than a byte of information doesn't make
+        # sense.
+        phred_range = (0, 255)
+
+    return phred_offset, phred_range
