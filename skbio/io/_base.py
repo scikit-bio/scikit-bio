326d35650f2431f1f93bfe81f674f0b69adddefc
Jai Ram Rideout <jai.rideout@gmail.com>
2014-10-23 14:39:18 -0700

    Add Phred score decoder to skbio.io
    Needed for fastq and qseq readers and writers.

--- skbio/io/_base.py
+++ skbio/io/_base.py
@@ -21,3 +21,50 @@ def _chunk_str(s, n, char):
         raise ValueError(
             "Cannot split string into chunks with n=%d. n must be >= 1." % n)
     return char.join((s[i:i+n] for i in range(0, len(s), n)))
+
+
+def _decode_qual_to_phred(qual_str, variant=None, phred_offset=None):
+    if variant is None and phred_offset is None:
+        raise ValueError(
+            "Must provide either `variant` or `phred_offset` in order to "
+            "decode quality scores.")
+    if variant is not None and phred_offset is not None:
+        raise ValueError(
+            "Cannot provide both `variant` and `phred_offset`.")
+
+    if variant is not None:
+        if variant == 'sanger':
+            phred_offset = 33
+            phred_range = (0, 93)
+        elif variant == 'illumina1.3':
+            phred_offset = 64
+            phred_range = (0, 62)
+        elif variant == 'illumina1.8':
+            phred_offset = 33
+            phred_range = (0, 62)
+        elif variant == 'solexa':
+            phred_offset = 64
+            phred_range = (-5, 62)
+            raise NotImplementedError(
+                "Decoding Solexa quality scores is not currently supported, "
+                "as quality scores are always stored as Phred scores in "
+                "scikit-bio. Please see the following scikit-bio issue to "
+                "track progress on this:\n\t"
+                "https://github.com/biocore/scikit-bio/issues/719")
+        else:
+            raise ValueError("Unrecognized variant %r." % variant)
+    else:
+        # Phred scores cannot be less than zero. There is no theoretical max to
+        # a Phred score, but more than a byte of information doesn't make
+        # sense.
+        phred_range = (0, 255)
+
+    phred = []
+    for c in qual_str:
+        score = ord(c) - phred_offset
+        if phred_range[0] <= score <= phred_range[1]:
+            phred.append(score)
+        else:
+            raise ValueError("Decoded Phred score %d is out of range [%d, %d]."
+                             % (score, phred_range[0], phred_range[1]))
+    return phred
--- skbio/io/tests/test_base.py
+++ skbio/io/tests/test_base.py
@@ -7,10 +7,11 @@
 # ----------------------------------------------------------------------------
 
 from __future__ import absolute_import, division, print_function
+from future.builtins import range
 
 import unittest
 
-from skbio.io._base import _chunk_str
+from skbio.io._base import _chunk_str, _decode_qual_to_phred
 
 
 class ChunkStrTests(unittest.TestCase):
@@ -38,6 +39,84 @@ class ChunkStrTests(unittest.TestCase):
 
         with self.assertRaisesRegexp(ValueError, 'n=-42'):
             _chunk_str('abcdef', -42, ' ')
+
+
+class PhredDecoderTests(unittest.TestCase):
+    def test_missing_variant_and_phred_offset(self):
+        with self.assertRaises(ValueError) as cm:
+            _decode_qual_to_phred('abcd')
+        self.assertIn('`variant`', str(cm.exception))
+        self.assertIn('`phred_offset`', str(cm.exception))
+        self.assertIn('decode', str(cm.exception))
+
+    def test_variant_and_phred_offset_provided(self):
+        with self.assertRaises(ValueError) as cm:
+            _decode_qual_to_phred('abcd', variant='sanger', phred_offset=64)
+        self.assertIn('both', str(cm.exception))
+        self.assertIn('`variant`', str(cm.exception))
+        self.assertIn('`phred_offset`', str(cm.exception))
+
+    def test_solexa_variant(self):
+        with self.assertRaises(NotImplementedError) as cm:
+            _decode_qual_to_phred('abcd', variant='solexa')
+        self.assertIn('719', str(cm.exception))
+
+    def test_unrecognized_variant(self):
+        with self.assertRaises(ValueError) as cm:
+            _decode_qual_to_phred('abcd', variant='illumina')
+        self.assertIn('variant', str(cm.exception))
+        self.assertIn("'illumina'", str(cm.exception))
+
+    def test_empty_qual_str(self):
+        self.assertEqual(_decode_qual_to_phred('', variant='sanger'), [])
+
+    def test_sanger_variant(self):
+        # test entire range of possible ascii chars for sanger
+        all_sanger_ascii = ('!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOP'
+                            'QRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~')
+        obs = _decode_qual_to_phred(all_sanger_ascii, variant='sanger')
+        self.assertEqual(obs, list(range(94)))
+
+        with self.assertRaises(ValueError) as cm:
+            _decode_qual_to_phred('a b', variant='sanger')
+        self.assertIn('-1', str(cm.exception))
+        self.assertIn('[0, 93]', str(cm.exception))
+
+    def test_illumina13_variant(self):
+        # test entire range of possible ascii chars for illumina1.3
+        all_illumina13_ascii = ('@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijk'
+                                'lmnopqrstuvwxyz{|}~')
+        obs = _decode_qual_to_phred(all_illumina13_ascii,
+                                    variant='illumina1.3')
+        self.assertEqual(obs, list(range(63)))
+
+        with self.assertRaises(ValueError) as cm:
+            _decode_qual_to_phred('a!b', variant='illumina1.3')
+        self.assertIn('-31', str(cm.exception))
+        self.assertIn('[0, 62]', str(cm.exception))
+
+    def test_illumina18_variant(self):
+        # test entire range of possible ascii chars for illumina1.8
+        all_illumina18_ascii = ('!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKL'
+                                'MNOPQRSTUVWXYZ[\\]^_')
+        obs = _decode_qual_to_phred(all_illumina18_ascii,
+                                    variant='illumina1.8')
+        self.assertEqual(obs, list(range(63)))
+
+        with self.assertRaises(ValueError) as cm:
+            _decode_qual_to_phred('AaB', variant='illumina1.8')
+        self.assertIn('64', str(cm.exception))
+        self.assertIn('[0, 62]', str(cm.exception))
+
+    def test_custom_phred_offset(self):
+        ascii_chars = '*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\'
+        obs = _decode_qual_to_phred(ascii_chars, phred_offset=42)
+        self.assertEqual(obs, list(range(51)))
+
+        with self.assertRaises(ValueError) as cm:
+            _decode_qual_to_phred(ascii_chars, phred_offset=43)
+        self.assertIn('-1', str(cm.exception))
+        self.assertIn('[0, 255]', str(cm.exception))
 
 
 if __name__ == '__main__':
