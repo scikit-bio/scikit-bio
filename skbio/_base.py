edac257ea92164276d524286b6633db2f9500389
Jai Ram Rideout <jai.rideout@gmail.com>
2014-11-03 12:48:17 -0700

    Add ScikitBioABC class and tests
    Only includes __str__ for now and basic tests. Next step is to update codebase to inherit from this class (and define __str__ methods as necessary).

---
+++ skbio/_base.py
@@ -0,0 +1,25 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2013--, scikit-bio development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from __future__ import absolute_import, division, print_function
+from future.utils import with_metaclass
+
+from abc import ABCMeta, abstractmethod
+
+
+class ScikitBioABC(with_metaclass(ABCMeta, object)):
+    """Abstract base class for core API common to all scikit-bio objects.
+
+    Public scikit-bio classes should subclass this class to ensure a common,
+    core API is present. All abstract methods and properties defined here must
+    be implemented in subclasses, otherwise they will not be instantiable.
+
+    """
+    @abstractmethod
+    def __str__(self):
+        pass
---
+++ skbio/tests/test_base.py
@@ -0,0 +1,26 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2013--, scikit-bio development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from __future__ import absolute_import, division, print_function
+
+import unittest
+
+from skbio._base import ScikitBioABC
+
+
+class TestScikitBioABC(unittest.TestCase):
+    def test_no_instantiation(self):
+        class Foo(ScikitBioABC):
+            pass
+
+        with self.assertRaises(TypeError):
+            Foo()
+
+
+if __name__ == '__main__':
+    unittest.main()
