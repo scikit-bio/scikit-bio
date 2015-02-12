785d8f14eff11a4d919cde0f9330cf47e92cea89
ebolyen <ebolyen@gmail.com>
2015-02-12 08:57:29 -0700

    update version strings to 0.3.0-dev

--- skbio/__init__.py
+++ skbio/__init__.py
@@ -10,7 +10,7 @@ from __future__ import absolute_import, division, print_function
 # ----------------------------------------------------------------------------
 
 __credits__ = "https://github.com/biocore/scikit-bio/graphs/contributors"
-__version__ = "0.2.2-dev"
+__version__ = "0.3.0-dev"
 
 mottos = [
     # 03/15/2014
