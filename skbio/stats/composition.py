fde57716e2e921722c2a26785069ae6ef1107844
Jamie Morton <jamietmorton@gmail.com>
2015-02-22 14:38:58 -0700

    Reword: Adding in compositional data analysis methods for manipulating datasets containing percentages/proportions
    ENH: Adding in composition methods
    
    TST: Adding in tests for clr, zero_imputation, power and perturb_inv
    
    DOC: Updating import documentation
    
    TST: centralize test added
    
    STY: Fixed all pep8/flake8 errors
    
    DOC: Fixed sphinx erros
    
    DOC: Added examples
    
    TST: Fixing test cases
    
    DOC: Adding in Examples section
    
    TST: Adding in exception handling cases
    
    ENH: Adding more inituitive handling of 1-D arrays in multiplicative replacement
    
    STY: Fixing pep8 errors
    
    DOC: Clarifying closure operation
    
    STY: Refactoring
    
    Removing np.vstack and allowing function arguments to be array-like instead of np.ndarray
    
    STY: Refactoring documentation and code
    
    Omitting output variable names in docs, adding mat.ndim, fixing array_like syntax
    
    STY: Fixing outstanding pep8 error
    
    STY: Rearranging imports

---
+++ skbio/stats/composition.py
@@ -0,0 +1,415 @@
+r"""
+Composition Statistics (:mod:`skbio.stats.composition`)
+=======================================================
+
+.. currentmodule:: skbio.stats.composition
+
+This module provides functions for compositional data analysis.
+
+Many 'omics datasets are inheriently compositional - meaning that they are best
+interpreted as proportions or percentages rather than absolute counts.
+
+Formally, :math:`x` is a composition if :math:`\sum_{i=0}^D x_{i} = c` and
+:math:`x_{i} > 0`, :math:`1 \leq i \leq D`  and :math:`c` is a real valued
+constant and there are :math:`D` components for each composition. In this
+module :math:`c=1`. Compositional data can be analyzed using Aitchison
+geometry [1]_
+
+However, in this framework, standard real Euclidean operations such as addition
+and multiplication no longer apply.  Only operations such as perturbation and
+power can be used to manipulate this data [1]_
+
+This module allows two styles of manipulation of compositional data.
+Compositional data can be analyzed using perturbation and power operations,
+which can be useful for simulation studies. The alternative strategy is to
+transform compositional data into the real space.  Right now, the centre log
+ratio transform (clr) [1]_ can be used to accomplish this.  This transform can
+be useful for performing standard statistical tools such as parametric
+hypothesis testing, regressions and more.
+
+The major caveat of using this framework is dealing with zeros.
+In the Aitchison geometry, only compositions with nonzero components can be
+considered. The multiplicative replacement technique [2]_ can be used to
+substitute these zeros with small pseudocounts without introducing major
+distortions to the data.
+
+Functions
+---------
+
+.. autosummary::
+   :toctree: generated/
+
+   multiplicative_replacement
+   perturb
+   perturb_inv
+   power
+   clr
+   centralize
+
+Examples
+--------
+
+>>> import numpy as np
+
+Consider a very simple environment with only 3 species.
+The species in the environment are equally distributed and their
+proportions are equilvalent:
+
+>>> otus = np.array([1./3, 1./3., 1./3])
+
+Suppose that an antibiotic kills off half of the population for the
+first two species, but doesn't harm the third species.
+Then the perturbation vector would be as follows
+
+>>> antibiotic = np.array([1./2, 1./2, 1])
+
+And the resulting perturbation would be
+
+>>> perturb(otus, antibiotic)
+array([ 0.25,  0.25,  0.5 ])
+
+Reference
+---------
+.. [1] V. Pawlowsky-Glahn. "Lecture Notes on Compositional Data Analysis"
+.. [2] J. A. Martin-Fernandez. "Dealing With Zeros and Missing Values in
+       Compositional Data Sets Using Nonparametric Imputation"
+
+"""
+
+# ----------------------------------------------------------------------------
+# Copyright (c) 2013--, scikit-bio development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from __future__ import absolute_import, division, print_function
+import numpy as np
+import scipy.stats as ss
+
+
+def _closure(mat):
+    """
+    Performs closure to ensure that all elements add up to 1
+
+    Parameters
+    ----------
+    mat : array_like
+       a matrix of proportions where
+       rows = compositions
+       columns = components
+
+    Returns
+    -------
+    numpy.ndarray, np.float64
+       A matrix of proportions where all of the values
+       are nonzero and each composition (row) adds up to 1
+
+    """
+
+    mat = np.asarray(mat, dtype=np.float64)
+
+    if mat.ndim == 1:
+        total = mat.sum()
+    elif mat.ndim == 2:
+        num_samps, num_feats = mat.shape
+        total = np.reshape(mat.sum(axis=1), (num_samps, 1))
+    else:
+        raise ValueError("mat has too many dimensions")
+    return np.divide(mat, total)
+
+
+def multiplicative_replacement(mat, delta=None):
+    """
+    Performs multiplicative replacement strategy to replace
+    all of the zeros with small non-zero values.  A closure
+    operation is applied so that the compositions still
+    add up to 1
+
+    Parameters
+    ----------
+    mat: array_like
+       a matrix of proportions where
+       rows = compositions and
+       columns = components
+
+    Returns
+    -------
+    numpy.ndarray, np.float64
+       A matrix of proportions where all of the values
+       are nonzero and each composition (row) adds up to 1
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from skbio.stats.composition import multiplicative_replacement
+    >>> X = np.array([[.2,.4,.4, 0],[0,.5,.5,0]])
+    >>> multiplicative_replacement(X)
+    array([[ 0.1875,  0.375 ,  0.375 ,  0.0625],
+           [ 0.0625,  0.4375,  0.4375,  0.0625]])
+
+    """
+    mat = np.asarray(mat, dtype=np.float64)
+    z_mat = (mat == 0)
+
+    if mat.ndim == 1:
+        num_feats = len(mat)
+        num_samps = 1
+        tot = z_mat.sum()
+    elif mat.ndim == 2:
+        num_samps, num_feats = mat.shape
+        tot = z_mat.sum(axis=1)
+    else:
+        raise ValueError("mat has too many dimensions")
+
+    if delta is None:
+        delta = (1. / num_feats)**2
+
+    zcnts = 1 - np.reshape(tot * delta, (num_samps, 1))
+    mat_ = _closure(z_mat*delta + np.multiply((1-z_mat),
+                                              np.multiply(zcnts, mat)))
+    if mat.ndim == 1:
+        mat_ = np.ravel(mat_)
+    return mat_
+
+
+def perturb(x, y):
+    r"""
+    Performs the perturbation operation
+
+    This operation is defined as
+    :math:`x \oplus y = C[x_1 y_1, ..., x_D y_D]`
+
+    :math:`C[x]` is the closure operation defined as
+    :math:`C[x] = [\frac{x_1}{\sum x},...,\frac{x_D}{\sum x}]`
+    for some :math:`D` dimensional real vector :math:`x` and
+    :math:`D` is the number of components for every composition.
+
+    Parameters
+    ----------
+    x : array_like, float
+        a matrix of proportions where
+        rows = compositions and
+        columns = components
+    y : array_like, float
+        a matrix of proportions where
+        rows = compositions and
+        columns = components
+
+    Returns
+    -------
+    numpy.ndarray, np.float64
+       A matrix of proportions where all of the values
+       are nonzero and each composition (row) adds up to 1
+
+    Notes
+    -----
+    - Each row must add up to 1
+
+    - All of the values in x and y must be greater than zero
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from skbio.stats.composition import perturb
+    >>> x = np.array([.1,.3,.4, .2])
+    >>> y = np.array([1./6,1./6,1./3,1./3])
+    >>> perturb(x,y)
+    array([ 0.0625,  0.1875,  0.5   ,  0.25  ])
+
+    """
+    x = np.asarray(x, dtype=np.float64)
+    y = np.asarray(y, dtype=np.float64)
+    return _closure(np.multiply(x, y))
+
+
+def perturb_inv(x, y):
+    r"""
+    Performs the inverse perturbation operation
+
+    This operation is defined as
+    :math:`x \ominus y = C[x_1 y_1^{-1}, ..., x_D y_D^{-1}]`
+
+    :math:`C[x]` is the closure operation defined as
+    :math:`C[x] = [\frac{x_1}{\sum x},...,\frac{x_D}{\sum x}]`
+    for some :math:`D` dimensional real vector :math:`x` and
+    :math:`D` is the number of components for every composition.
+
+    Parameters
+    ----------
+    x : numpy.ndarray
+        a matrix of proportions where
+        rows = compositions and
+        columns = components
+    y : numpy.ndarray
+        rows = compositions and
+        columns = components
+
+    Returns
+    -------
+    numpy.ndarray, np.float64
+       A matrix of proportions where all of the values
+       are nonzero and each composition (row) adds up to 1
+
+    Notes
+    -----
+    - Each row must add up to 1 for x.
+
+    - y doesn't neccessary have to be a matrix of compositions
+
+    - All of the values in x and y must be greater than zero
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from skbio.stats.composition import perturb_inv
+    >>> x = np.array([.1,.3,.4, .2])
+    >>> y = np.array([1./6,1./6,1./3,1./3])
+    >>> perturb_inv(x,y)
+    array([ 0.14285714,  0.42857143,  0.28571429,  0.14285714])
+
+    """
+    x = np.asarray(x, dtype=np.float64)
+    y = np.asarray(y, dtype=np.float64)
+    _y = power(y, -1)
+    return _closure(np.multiply(x, _y))
+
+
+def power(x, a):
+    r"""
+    Performs the power operation
+
+    This operation is defined as follows
+    :math:`x \odot a = C[x_1^a, ..., x_D^a]`
+
+    :math:`C[x]` is the closure operation defined as
+    :math:`C[x] = [\frac{x_1}{\sum x},...,\frac{x_D}{\sum x}]`
+    for some :math:`D` dimensional real vector :math:`x` and
+    :math:`D` is the number of components for every composition.
+
+
+    Parameters
+    ----------
+    x : numpy.ndarray, float
+        a matrix of proportions where
+        rows = compositions and
+        columns = components
+    a : float
+        a scalar float
+
+    Returns
+    -------
+    numpy.ndarray, np.float64
+       A matrix of proportions where all of the values
+       are nonzero and each composition (row) adds up to 1
+
+    Notes
+    -----
+    - Each row must add up to 1 for x
+
+    - All of the values in x must be greater than zero
+
+    >>> import numpy as np
+    >>> from skbio.stats.composition import power
+    >>> x = np.array([.1,.3,.4, .2])
+    >>> power(x, .1)
+    array([ 0.23059566,  0.25737316,  0.26488486,  0.24714631])
+
+    """
+    x = np.asarray(x, dtype=np.float64)
+    mat = np.multiply(np.log(x), a)
+    return _closure(np.exp(mat))
+
+
+def clr(mat):
+    r"""
+    Performs centre log ratio transformation that transforms
+    compositions from Aitchison geometry to the real space.
+    This transformation is an isometry, but not an isomorphism.
+
+    This transformation is defined for a composition :math:`x` as follows
+
+    :math:`clr(x) = ln[\frac{x_1}{g_m(x)}, ..., \frac{x_D}{g_m(x)}]`
+    where :math:`g_m(x) = (\prod_{i=1}^{D} x_i)^{1/D}` is the geometric
+    mean of :math:`x`.
+
+    Parameters
+    ----------
+    mat : numpy.ndarray, float
+       a matrix of proportions where
+       rows = compositions and
+       columns = components
+
+    Returns
+    -------
+    numpy.ndarray
+         clr transformed matrix
+
+    Notes
+    -----
+    - Each row must add up to 1
+
+    - All of the values must be greater than zero for mat
+
+    >>> import numpy as np
+    >>> from skbio.stats.composition import clr
+    >>> x = np.array([.1,.3,.4, .2])
+    >>> clr(x)
+    array([-0.79451346,  0.30409883,  0.5917809 , -0.10136628])
+
+    """
+    mat = np.asarray(mat, dtype=np.float64)
+    lmat = np.log(mat)
+    if mat.ndim == 1:
+        num_samps = len(mat)
+        gm = lmat.mean()
+    elif mat.ndim == 2:
+        num_samps, num_feats = mat.shape
+        gm = lmat.mean(axis=1)
+        gm = np.reshape(gm, (num_samps, 1))
+    else:
+        raise ValueError("mat has too many dimensions")
+    return lmat - gm
+
+
+def centralize(mat):
+    """
+    This calculates the average sample and centers the data
+    around this sample.
+
+    Parameters
+    ----------
+    mat : numpy.ndarray, float
+       a matrix of proportions where
+       rows = compositions and
+       columns = components
+
+    Returns
+    -------
+    numpy.ndarray
+         centered composition matrix
+
+    Notes
+    -----
+    - Each row must add up to 1 for mat
+
+    - All of the values must be greater than zero
+
+    >>> import numpy as np
+    >>> from skbio.stats.composition import centralize
+    >>> X = np.array([[.1,.3,.4, .2],[.2,.2,.2,.4]])
+    >>> centralize(X)
+    array([[ 0.17445763,  0.30216948,  0.34891526,  0.17445763],
+           [ 0.32495488,  0.18761279,  0.16247744,  0.32495488]])
+
+    """
+    mat = np.asarray(mat, dtype=np.float64)
+    if mat.ndim == 1:
+        raise ValueError("mat needs more than 1 row")
+    if mat.ndim > 2:
+        raise ValueError("mat has too many dimensions")
+    r, c = mat.shape
+    cen = ss.gmean(mat, axis=0)
+    cen = np.tile(cen, (r, 1))
+    return perturb_inv(mat, cen)
---
+++ skbio/stats/tests/test_composition.py
@@ -0,0 +1,177 @@
+from __future__ import absolute_import, division, print_function
+
+# ----------------------------------------------------------------------------
+# Copyright (c) 2013--, scikit-bio development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from unittest import TestCase, main
+import numpy as np
+import numpy.testing as npt
+from skbio.stats.composition import (_closure, multiplicative_replacement,
+                                     perturb, perturb_inv, power,
+                                     clr, centralize)
+
+
+class CompositionTests(TestCase):
+
+    def setUp(self):
+        self.data1 = np.array([[2, 2, 6],
+                               [4, 4, 2]])
+        self.data2 = np.array([2, 2, 6])
+
+        self.data3 = np.array([[1, 2, 3, 0, 5],
+                               [1, 0, 0, 4, 5],
+                               [1, 2, 3, 4, 5]])
+        self.data4 = np.array([1, 2, 3, 0, 5])
+        self.data5 = np.array([[[1, 2, 3, 0, 5]]])
+        self.data6 = [[2, 2, 6], [4, 4, 2]]
+        self.data7 = [[1, 2, 3, 0, 5],
+                      [1, 0, 0, 4, 5],
+                      [1, 2, 3, 4, 5]]
+
+    def test_closure(self):
+        npt.assert_allclose(_closure(self.data1),
+                            np.array([[.2, .2, .6],
+                                      [.4, .4, .2]]))
+        npt.assert_allclose(_closure(self.data2),
+                            np.array([.2, .2, .6]))
+        npt.assert_allclose(_closure(self.data6),
+                            np.array([[.2, .2, .6],
+                                      [.4, .4, .2]]))
+        with self.assertRaises(ValueError):
+            _closure(self.data5)
+
+    def test_perturb(self):
+        pmat = perturb(_closure(self.data1), np.array([1, 1, 1]))
+        npt.assert_allclose(pmat,
+                            np.array([[.2, .2, .6],
+                                      [.4, .4, .2]]))
+
+        pmat = perturb(_closure(self.data1), np.array([10, 10, 20]))
+        npt.assert_allclose(pmat,
+                            np.array([[.125, .125, .75],
+                                      [1./3, 1./3, 1./3]]))
+
+        pmat = perturb(_closure(self.data1), np.array([10, 10, 20]))
+        npt.assert_allclose(pmat,
+                            np.array([[.125, .125, .75],
+                                      [1./3, 1./3, 1./3]]))
+
+        pmat = perturb(_closure(self.data2), np.array([1, 2, 1]))
+        npt.assert_allclose(pmat, np.array([1./6, 2./6, 3./6]))
+
+        pmat = perturb(_closure(self.data6), np.array([1, 1, 1]))
+        npt.assert_allclose(pmat,
+                            np.array([[.2, .2, .6],
+                                      [.4, .4, .2]]))
+
+    def test_power(self):
+        pmat = power(_closure(self.data1), 2)
+        npt.assert_allclose(pmat,
+                            np.array([[.04/.44, .04/.44, .36/.44],
+                                      [.16/.36, .16/.36, .04/.36]]))
+
+        pmat = power(_closure(self.data2), 2)
+        npt.assert_allclose(pmat, np.array([.04, .04, .36])/.44)
+
+        pmat = power(_closure(self.data6), 2)
+        npt.assert_allclose(pmat,
+                            np.array([[.04/.44, .04/.44, .36/.44],
+                                      [.16/.36, .16/.36, .04/.36]]))
+
+    def test_perturb_inv(self):
+        pmat = perturb_inv(_closure(self.data1), np.array([.1, .1, .1]))
+        imat = perturb(_closure(self.data1), np.array([10, 10, 10]))
+        npt.assert_allclose(pmat, imat)
+        pmat = perturb_inv(_closure(self.data1), np.array([1, 1, 1]))
+        npt.assert_allclose(pmat,
+                            np.array([[.2, .2, .6],
+                                      [.4, .4, .2]]))
+        pmat = perturb_inv(_closure(self.data6), np.array([.1, .1, .1]))
+        imat = perturb(_closure(self.data1), np.array([10, 10, 10]))
+        npt.assert_allclose(pmat, imat)
+
+    def test_multiplicative_replacement(self):
+        np.set_printoptions(precision=10)
+        amat = multiplicative_replacement(self.data3)
+        npt.assert_allclose(amat,
+                            np.array([[0.09056604, 0.18113208,
+                                       0.27169811, 0.00377358,
+                                       0.45283019],
+                                      [0.09913793, 0.00431034,
+                                       0.00431034, 0.39655172,
+                                       0.49568966],
+                                      [0.06666667, 0.13333333,
+                                       0.2, 0.26666667,
+                                       0.33333333]]), rtol=1e-5, atol=1e-5)
+
+        amat = multiplicative_replacement(self.data4)
+        npt.assert_allclose(amat, np.array([0.09056604, 0.18113208,
+                                            0.27169811, 0.00377358,
+                                            0.45283019]),
+                            rtol=1e-5, atol=1e-5)
+
+        amat = multiplicative_replacement(self.data7)
+        npt.assert_allclose(amat,
+                            np.array([[0.09056604, 0.18113208,
+                                       0.27169811, 0.00377358,
+                                       0.45283019],
+                                      [0.09913793, 0.00431034,
+                                       0.00431034, 0.39655172,
+                                       0.49568966],
+                                      [0.06666667, 0.13333333,
+                                       0.2, 0.26666667,
+                                       0.33333333]]), rtol=1e-5, atol=1e-5)
+
+        with self.assertRaises(ValueError):
+            multiplicative_replacement(self.data5)
+
+    def test_clr(self):
+        cmat = clr(_closure(self.data1))
+        A = np.array([.2, .2, .6])
+        B = np.array([.4, .4, .2])
+
+        npt.assert_allclose(cmat,
+                            [np.log(A / np.exp(np.log(A).mean())),
+                             np.log(B / np.exp(np.log(B).mean()))])
+        cmat = clr(_closure(self.data2))
+        A = np.array([.2, .2, .6])
+        npt.assert_allclose(cmat,
+                            np.log(A / np.exp(np.log(A).mean())))
+
+        cmat = clr(_closure(self.data6))
+        A = np.array([.2, .2, .6])
+        B = np.array([.4, .4, .2])
+
+        npt.assert_allclose(cmat,
+                            [np.log(A / np.exp(np.log(A).mean())),
+                             np.log(B / np.exp(np.log(B).mean()))])
+
+        with self.assertRaises(ValueError):
+            clr(self.data5)
+
+    def test_centralize(self):
+        cmat = centralize(_closure(self.data1))
+        npt.assert_allclose(cmat,
+                            np.array([[0.22474487, 0.22474487,
+                                       0.55051026],
+                                      [0.41523958, 0.41523958,
+                                       0.16952085]]))
+        cmat = centralize(_closure(self.data6))
+        npt.assert_allclose(cmat,
+                            np.array([[0.22474487, 0.22474487,
+                                       0.55051026],
+                                      [0.41523958, 0.41523958,
+                                       0.16952085]]))
+
+        with self.assertRaises(ValueError):
+            centralize(self.data2)
+        with self.assertRaises(ValueError):
+            centralize(self.data5)
+
+if __name__ == "__main__":
+    main()
