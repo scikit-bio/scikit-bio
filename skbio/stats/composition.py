fda2b46e999e90749edeae91dc0dc7f61aa29322
Jamie Morton <jamietmorton@gmail.com>
2015-02-22 18:07:11 -0700

    STY: Fixed all pep8/flake8 errors

--- skbio/stats/composition.py
+++ skbio/stats/composition.py
@@ -6,19 +6,50 @@ Composition Statistics (:mod:`skbio.stats.composition`)
 
 This module provides functions for compositional data analysis.
 
+Many 'omics datasets are inheriently compositional - meaning that they are best
+interpreted as proportions or percentages rather than absolute counts.
+
+Formally, :math:`x` is a composition if :math:`\sum_{i=0}^D x_i = \kappa` and
+`x_i > 0 \forall i` where :math:`\kappa` is a real valued constant and there
+are :math:`D` components for each composition.  In this module :math:`\kappa=1`
+Compositional data can be analyzed using Aitchison geometry [1]_
+
+However, in this framework, standard real Euclidean operations such as addition
+and multiplication no longer apply.  Only operations such as perturbation and
+power can be used to manipulate this data [1]_
+
+This module allows two styles of manipulation of compositional data.
+Compositional data can be analyzed using perturbation and power operations.
+This can be useful for simulation studies. The alternative strategy is to
+transform compositional data into the real space.  Right now, the centre log
+ratio transform (clr) [1]_ can be used to accomplish this.  This transform can
+be useful for performing standard statistical tools such as parametric
+hypothesis testing, regressions and more.
+
+The most major caveat of using this framework is dealing with zeros.
+In the Aitchison geometry, only compositions with nonzero components can be
+considered. The multiplicative replacement][2]_ technique can be used to
+substitute these zeros with small pseudocounts without introducing major
+distortions to the data.
+
 Functions
 ---------
 
 .. autosummary::
    :toctree: generated/
 
-   zero_imputation
+   multiplicative_replacement
    perturb
    perturb_inv
    power
    clr
    centralize
-   
+
+Reference
+---------
+.. [1] V. Pawlowsky-Glahn. "Lecture Notes on Compositional Data Analysis"
+.. [2] J. A. Martin-Fernandez. "Dealing With Zeros and Missing Values in
+       Compositional Data Sets Using Nonparametric Imputation"
 
 """
 from __future__ import absolute_import, division, print_function
@@ -38,12 +69,20 @@ import scipy.stats as ss
 
 def _closure(mat):
     """
-    Performs _closure to ensure that all elements add up to 1
-    mat: numpy.ndarray
-       columns = features
-       rows = samples
+    Performs closure to ensure that all elements add up to 1
+    mat : numpy.ndarray
+       a matrix of proportions where
+       rows = compositions
+       columns = components
+
+    Returns
+    -------
+    mat_ : numpy.ndarray, np.float64
+       A matrix of proportions where all of the values
+       are nonzero and each composition (row) adds up to 1
+
     """
-    if mat.dtype != type(0.0):
+    if not isinstance(mat.dtype, float):
         mat = mat.astype(np.float64)
 
     if len(mat.shape) == 1:
@@ -52,99 +91,220 @@ def _closure(mat):
     else:
         num_samps, num_feats = mat.shape
         total = np.reshape(mat.sum(axis=1), (num_samps, 1))
-    return np.divide(mat, total)
+    mat_ = np.divide(mat, total)
+    return mat_
 
 
 def multiplicative_replacement(mat, delta=None):
     """
-    Performs multiplicative replacement strategy
+    Performs multiplicative replacement strategy to replace
+    all of the zeros with small non-zero values.  A closure
+    operation is applied so that the compositions still
+    add up to 1
+
     mat: numpy.ndarray
-       columns = features
-       rows = samples
+       a matrix of proportions where
+       rows = compositions
+       columns = components
+
     Returns:
     --------
-    mat: numpy.ndarray
+    mat_ : numpy.ndarray, np.float64
+       A matrix of proportions where all of the values
+       are nonzero and each composition (row) adds up to 1
     """
     num_samps, num_feats = mat.shape
-    if delta==None:
+    if delta is None:
         delta = (1. / num_feats)**2
     z_mat = (mat == 0).astype(np.float32)
-    zcnts = 1 - np.reshape(z_mat.sum(axis=1) * delta, (num_samps, 1) )
-    mat = z_mat*delta + np.multiply((1-z_mat), np.multiply(zcnts,mat))
-    return _closure(mat)
+    zcnts = 1 - np.reshape(z_mat.sum(axis=1) * delta, (num_samps, 1))
+    mat_ = _closure(z_mat*delta + np.multiply((1-z_mat),
+                                              np.multiply(zcnts, mat)))
+    return mat_
+
 
 def perturb(x, y):
     """
     Performs the perturbation operation
-    x: numpy.ndarray
-    y: numpy.ndarray
-    """    
-    mat = np.multiply(x, y)
-    return _closure(mat)
+
+    This operation is defined as follows
+    :math:`x \oplus y = C\[x_1 y_1, ..., x_D y_D\]`
+    Where :math:`C\[x\]` is the closure operation on the composition
+    :math:`x` and :math:`D` is the number of components for every
+    composition.
+
+    Parameters
+    ----------
+    x : numpy.ndarray, float
+        a matrix of proportions where
+        rows = compositions
+        columns = components
+    y : numpy.ndarray, float
+        a matrix of proportions where
+        rows = compositions
+        columns = components
+
+    Returns
+    -------
+    mat_ : numpy.ndarray, np.float64
+       A matrix of proportions where all of the values
+       are nonzero and each composition (row) adds up to 1
+
+    Notes
+    -----
+    - Each row must add up to 1
+
+    - All of the values in x and y must be greater than zero
+
+    """
+    mat_ = _closure(np.multiply(x, y))
+    return mat_
+
 
 def perturb_inv(x, y):
     """
     Performs the inverse perturbation operation
-    x: numpy.ndarray
-    y: numpy.ndarray
+
+    This operation is defined as follows
+    :math:`x \ominus y = C\[x_1 y_1^{-1}, ..., x_D y_D^{-1}\]`
+    Where :math:`C\[x\]` is the closure operation on the composition
+    :math:`x` and :math:`D` is the number of components for every
+    composition.
+
+    Parameters
+    ----------
+    x : numpy.ndarray
+        a matrix of proportions where
+        rows = compositions
+        columns = components
+    y : numpy.ndarray
+        a matrix of proportions where
+        rows = compositions
+        columns = components
+
+    Returns
+    -------
+    mat_ : numpy.ndarray, np.float64
+       A matrix of proportions where all of the values
+       are nonzero and each composition (row) adds up to 1
+
+    Notes
+    -----
+    - Each row must add up to 1
+
+    - All of the values in x and y must be greater than zero
     """
-    _y = power(y,-1)
+    _y = power(y, -1)
     mat = np.multiply(x, _y)
     return _closure(mat)
 
-def power(x, y):
+
+def power(x, a):
     """
-    Performs the perturbation operation
-    x: numpy.ndarray
-    y: numpy.ndarray
+    Performs the power operation
+
+    This operation is defined as follows
+    :math:`x \odot y = C\[x_1^a, ..., x_D^a\]`
+    Where :math:`C\[x\]` is the closure operation on the composition
+    :math:`x` and :math:`D` is the number of components for every
+    composition.
+
+    Parameters
+    ----------
+    x : numpy.ndarray, float
+        a matrix of proportions where
+        rows = compositions
+        columns = components
+    a : numpy.ndarray, float
+        a matrix of proportions where
+        rows = compositions
+        columns = components
+
+    Returns
+    -------
+    mat_ : numpy.ndarray, np.float64
+       A matrix of proportions where all of the values
+       are nonzero and each composition (row) adds up to 1
+
+    Notes
+    -----
+    - Each row must add up to 1
+
+    - All of the values in x must be greater than zero
+
     """
-    mat = np.multiply(np.log(x), y)
+    mat = np.multiply(np.log(x), a)
     return _closure(np.exp(mat))
+
 
 def clr(mat):
     """
-    Performs centre log ratio transformation
+    Performs centre log ratio transformation that transforms
+    compositions from Aitchison geometry to the real space.
+    This transformation is an isometry, but not an isomorphism.
+
+    This transformation is defined as follows for a composition
+    :math:`x`
+
+    :math:`clr(x) = ln[\frac{x_1}{g_m(x)}, ..., \frac{x_D}{g_m(x)}]`
+    where :math:`g_m(x) = (\prod_{i=1}^{D} x_i)^{1/D}` is the geometric
+    mean.
+
+    mat : numpy.ndarray, float
+       a matrix of proportions where
+       rows = compositions
+       columns = components
+
+
     Returns
     =======
-    clr: numpy.ndarray
-    
+    _clr : numpy.ndarray
+         clr transformed matrix
+
+    Notes
+    -----
+    - Each row must add up to 1
+
+    - All of the values must be greater than zero
+
     """
-    lmat = np.log(mat) # Need to account for zeros
+    lmat = np.log(mat)
     if len(mat.shape) == 1:
         num_samps = len(mat)
         gm = lmat.mean()
     else:
         num_samps, num_feats = mat.shape
-        gm = lmat.mean(axis = 1)
+        gm = lmat.mean(axis=1)
         gm = np.reshape(gm, (num_samps, 1))
     _clr = lmat - gm
     return _clr
-    
+
+
 def centralize(mat):
     """
     This calculates the average sample and centers the data
     around this sample.
 
-    mat: numpy.ndarray
-       A contingency table of normalized proportions
-       columns = features
-       rows = samples
+    mat : numpy.ndarray, float
+       a matrix of proportions where
+       rows = compositions
+       columns = components
+
+    Returns
+    =======
+    centered : numpy.ndarray
+         centered proportion matrix
 
     Notes
     -----
     - Each row must add up to 1
-    
+
     - All of the values must be greater than zero
-    
-    Reference
-    ---------
-    .. [1] J.J. Ezogcue http://www.sediment.uni-goettingen.de/staff/tolosana/extra/CoDa.pdf
 
     """
-    assert len(mat.shape)>1, "Need more than 1 row"
-    r,c = mat.shape
+    assert len(mat.shape) > 1, "Need more than 1 row"
+    r, c = mat.shape
     cen = ss.gmean(mat, axis=0)
-    cen = np.tile(cen, (r,1))
+    cen = np.tile(cen, (r, 1))
     centered = perturb_inv(mat, cen)
     return centered
-
--- skbio/stats/tests/test_composition.py
+++ skbio/stats/tests/test_composition.py
@@ -11,13 +11,13 @@ from __future__ import absolute_import, division, print_function
 from unittest import TestCase, main
 
 import numpy as np
-
+from numpy.testing import assert_array_almost_equal
 from skbio.stats.composition import (_closure, multiplicative_replacement,
                                      perturb, perturb_inv, power,
                                      clr, centralize)
 
-class CompositionTests(TestCase):
 
+class CompositionTests(TestCase):
 
     def setUp(self):
         self.data1 = np.vstack((
@@ -26,92 +26,91 @@ class CompositionTests(TestCase):
         self.data2 = np.array([2, 2, 6])
         D = 5
         self.data3 = np.vstack((
-            np.array(range(1,4) + [0]*1 + [5]),
-            np.array(range(1,2) + [0]*2 + range(4,6)),
-            np.array(range(1,D+1))))
-        
-    def test__closure(self):        
-        np.testing.assert_array_almost_equal(_closure(self.data1),
-                                          np.vstack((
-                                              np.array([.2, .2, .6]),
-                                              np.array([.4, .4, .2]))))
-        np.testing.assert_array_almost_equal(_closure(self.data2),
-                                             np.array([.2, .2, .6]))
+            np.array(range(1, 4) + [0]*1 + [5]),
+            np.array(range(1, 2) + [0]*2 + range(4, 6)),
+            np.array(range(1, D+1))))
+
+    def test__closure(self):
+        assert_array_almost_equal(_closure(self.data1),
+                                  np.vstack((np.array([.2, .2, .6]),
+                                             np.array([.4, .4, .2]))))
+        assert_array_almost_equal(_closure(self.data2),
+                                  np.array([.2, .2, .6]))
 
     def test_perturb(self):
         pmat = perturb(_closure(self.data1), np.array([1, 1, 1]))
-        np.testing.assert_array_almost_equal(pmat,
-                                             np.vstack((
-                                                 np.array([.2, .2, .6]),
-                                                 np.array([.4, .4, .2]))))
-        
-        pmat = perturb(_closure(self.data1), np.array([10, 10, 20]))
-        np.testing.assert_array_almost_equal(pmat,
-                                             np.vstack((
-                                                 np.array([.125, .125, .75]),
-                                                 np.array([1./3, 1./3, 1./3]))))
+        assert_array_almost_equal(pmat,
+                                  np.vstack((np.array([.2, .2, .6]),
+                                             np.array([.4, .4, .2]))))
 
         pmat = perturb(_closure(self.data1), np.array([10, 10, 20]))
-        np.testing.assert_array_almost_equal(pmat,
-                                             np.vstack((
-                                                 np.array([.125, .125, .75]),
-                                                 np.array([1./3, 1./3, 1./3]))))
+        assert_array_almost_equal(pmat,
+                                  np.vstack((
+                                      np.array([.125, .125, .75]),
+                                      np.array([1./3, 1./3, 1./3]))))
+
+        pmat = perturb(_closure(self.data1), np.array([10, 10, 20]))
+        assert_array_almost_equal(pmat,
+                                  np.vstack((
+                                      np.array([.125, .125, .75]),
+                                      np.array([1./3, 1./3, 1./3]))))
 
         pmat = perturb(_closure(self.data2), np.array([1, 2, 1]))
-        np.testing.assert_array_almost_equal(pmat,np.array([1./6, 2./6, 3./6]))
-        
+        assert_array_almost_equal(pmat, np.array([1./6, 2./6, 3./6]))
+
     def test_power(self):
         pmat = power(_closure(self.data1), 2)
-        np.testing.assert_array_almost_equal(pmat,
-                                             np.vstack((
-                                                 np.array([.04, .04, .36])/.44,
-                                                 np.array([.16, .16, .04])/.36)))
+        assert_array_almost_equal(pmat,
+                                  np.vstack((
+                                      np.array([.04, .04, .36])/.44,
+                                      np.array([.16, .16, .04])/.36)))
 
         pmat = power(_closure(self.data2), 2)
-        np.testing.assert_array_almost_equal(pmat, np.array([.04, .04, .36])/.44)
-
-
+        assert_array_almost_equal(pmat, np.array([.04, .04, .36])/.44)
 
     def test_perturb_inv(self):
         pmat = perturb_inv(_closure(self.data1), np.array([.1, .1, .1]))
-        imat = perturb(_closure(self.data1), np.array([10, 10, 10]))        
-        np.testing.assert_array_almost_equal(pmat, imat)
-
-
+        imat = perturb(_closure(self.data1), np.array([10, 10, 10]))
+        assert_array_almost_equal(pmat, imat)
         pmat = perturb_inv(_closure(self.data1), np.array([1, 1, 1]))
-        np.testing.assert_array_almost_equal(pmat,
-                                             np.vstack((
-                                                 np.array([.2, .2, .6]),
-                                                 np.array([.4, .4, .2]))))
-    
+        assert_array_almost_equal(pmat,
+                                  np.vstack((
+                                      np.array([.2, .2, .6]),
+                                      np.array([.4, .4, .2]))))
+
     def test_multiplicative_replacement(self):
         amat = multiplicative_replacement(self.data3)
-        np.testing.assert_array_almost_equal(amat,
-                np.array([[ 0.09056604,  0.18113208,  0.27169811,  0.00377358,  0.45283019],
-                          [ 0.09913793,  0.00431034,  0.00431034,  0.39655172,  0.49568966],
-                          [ 0.06666667,  0.13333333,  0.2       ,  0.26666667,  0.33333333]]))
-
+        assert_array_almost_equal(amat,
+                                  np.array([[0.09056604, 0.18113208,
+                                             0.27169811, 0.00377358,
+                                             0.45283019],
+                                            [0.09913793, 0.00431034,
+                                             0.00431034, 0.39655172,
+                                             0.49568966],
+                                            [0.06666667, 0.13333333,
+                                             0.2, 0.26666667, 0.33333333]]))
 
     def test_clr(self):
         cmat = clr(_closure(self.data1))
         A = np.array([.2, .2, .6])
         B = np.array([.4, .4, .2])
 
-        np.testing.assert_array_almost_equal(cmat,
-                                             np.vstack((
-                                                 np.log(A / np.exp(np.log(A).mean())) ,
-                                                 np.log(B / np.exp(np.log(B).mean())) )))
+        assert_array_almost_equal(cmat,
+                                  np.vstack((
+                                      np.log(A / np.exp(np.log(A).mean())),
+                                      np.log(B / np.exp(np.log(B).mean())))))
         cmat = clr(_closure(self.data2))
         A = np.array([.2, .2, .6])
-        np.testing.assert_array_almost_equal(cmat,
-                                             np.log(A / np.exp(np.log(A).mean())) )
-                                             
+        assert_array_almost_equal(cmat,
+                                  np.log(A / np.exp(np.log(A).mean())))
+
     def test_centralize(self):
         cmat = centralize(_closure(self.data1))
-        np.testing.assert_array_almost_equal(cmat,
-                                             np.array([[ 0.22474487,  0.22474487,  0.55051026],
-                                                       [ 0.41523958,  0.41523958,  0.16952085]]))
+        assert_array_almost_equal(cmat,
+                                  np.array([[0.22474487, 0.22474487,
+                                             0.55051026],
+                                            [0.41523958, 0.41523958,
+                                             0.16952085]]))
 
-        
 if __name__ == "__main__":
     main()
