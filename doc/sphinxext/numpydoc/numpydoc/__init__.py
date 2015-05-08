1744bc9447dc470397c93bac1b3ebae9e0a825c4
ebolyen <ebolyen@gmail.com>
2015-05-07 17:24:33 -0700

    MAGIC: monkey-patch ourselves for the doc-building

--- doc/sphinxext/numpydoc/numpydoc/__init__.py
+++ doc/sphinxext/numpydoc/numpydoc/__init__.py
@@ -1,3 +1,13 @@
 from __future__ import division, absolute_import, print_function
 
 from .numpydoc import setup
+
+# This is so that our docs build.
+def _closure():
+    from skbio.util import classproperty
+    def __get__(self, cls, owner):
+        return self
+
+    classproperty.__get__ = __get__
+
+_closure()
--- skbio/util/_decorator.py
+++ skbio/util/_decorator.py
@@ -9,7 +9,6 @@
 from __future__ import absolute_import, division, print_function
 
 import abc
-import inspect
 
 from ._exception import OverrideError
 
@@ -27,39 +26,6 @@ def overrides(interface_class):
                                 (method.__name__, interface_class.__name__))
         return method
     return overrider
-
-
-def caller_name(skip=4):
-    """Get a name of a caller in the format module.class.method
-
-       `skip` specifies how many levels of stack to skip while getting caller
-       name. skip=1 means "who calls me", skip=2 "who calls my caller" etc.
-
-       An empty string is returned if skipped levels exceed stack height
-    """
-    stack = inspect.stack()
-    start = 0 + skip
-    if len(stack) < start + 1:
-      return ''
-    parentframe = stack[start][0]
-
-    name = []
-    module = inspect.getmodule(parentframe)
-    # `modname` can be None when frame is executed directly in console
-    # TODO(techtonik): consider using __main__
-    if module:
-        name.append(module.__name__)
-    # detect classname
-    if 'self' in parentframe.f_locals:
-        # I don't know any way to detect call from the object method
-        # XXX: there seems to be no way to detect static method call - it will
-        #      be just a function call
-        name.append(parentframe.f_locals['self'].__class__.__name__)
-    codename = parentframe.f_code.co_name
-    if codename != '<module>':  # top level usually
-        name.append( codename ) # function or a method
-    del parentframe
-    return ".".join(name)
 
 
 class classproperty(property):
@@ -93,10 +59,6 @@ class classproperty(property):
         self.__doc__ = doc
 
     def __get__(self, cls, owner):
-        caller = caller_name()
-        caller = caller.split('.')[0]
-        if caller == "sphinx" or caller == "numpydoc":
-            return self
         return self.fget.__get__(None, owner)()
 
     def __set__(self, obj, value):
