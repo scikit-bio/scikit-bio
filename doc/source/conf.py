28efb87cf2a57882628ceacbe3d31b5c79ee86ce
Jai Ram Rideout <jai.rideout@gmail.com>
2015-06-10 10:48:02 -0700

    DOC: make commented out code in conf.py an example

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,8 +31,9 @@ from skbio.util import classproperty
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('../sphinxext/numpydoc'))
+# documentation root, use os.path.abspath to make it absolute, like shown here:
+#
+#    sys.path.insert(0, os.path.abspath('../sphinxext/foo'))
 
 # -- General configuration ------------------------------------------------
 
