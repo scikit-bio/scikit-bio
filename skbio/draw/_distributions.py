3727627e7b8397644a255c08b23b66c90a6066cd
Greg Caporaso <gregcaporaso@gmail.com>
2015-06-25 07:34:26 -0700

    MAINT: decorated public API with stability states

--- skbio/draw/_distributions.py
+++ skbio/draw/_distributions.py
@@ -18,7 +18,12 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from matplotlib.patches import Polygon, Rectangle
 
+from skbio.util._decorator import deprecated
 
+
+@deprecated(as_of="0.4.0", until="0.4.1",
+            reason="Plots that are not specific to bioinformatics should be "
+                   "generated with seaborn or another package.")
 def boxplots(distributions, x_values=None, x_tick_labels=None, title=None,
              x_label=None, y_label=None, x_tick_labels_orientation='vertical',
              y_min=None, y_max=None, whisker_length=1.5, box_width=0.5,
@@ -140,6 +145,9 @@ def boxplots(distributions, x_values=None, x_tick_labels=None, title=None,
     return fig
 
 
+@deprecated(as_of="0.4.0", until="0.4.1",
+            reason="Plots that are not specific to bioinformatics should be "
+                   "generated with seaborn or another package.")
 def grouped_distributions(plot_type, data, x_values=None,
                           data_point_labels=None, distribution_labels=None,
                           distribution_markers=None, x_label=None,
