5e3d949017934c96a0ca1e5f2ffaf25477d42f03
Jai Ram Rideout <jai.rideout@gmail.com>
2015-07-07 21:38:13 -0700

    PERF: use np.uint8 for QUAL scores

--- skbio/io/format/fasta.py
+++ skbio/io/format/fasta.py
@@ -933,7 +933,12 @@ def _parse_quality_scores(chunks):
         raise QUALFormatError(
             "Encountered negative quality score(s). Quality scores must be "
             "greater than or equal to zero.")
-    return quality
+    if (quality > 255).any():
+        raise QUALFormatError(
+            "Encountered quality score(s) greater than 255. scikit-bio only "
+            "supports quality scores in the range 0-255 (inclusive) when "
+            "reading QUAL files.")
+    return quality.astype(np.uint8, casting='unsafe', copy=False)
 
 
 def _sequences_to_fasta(obj, fh, qual, id_whitespace_replacement,
