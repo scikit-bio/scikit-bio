1aeffa35b24a11c51ce1a57894e5bc4e4c09e454
Greg Caporaso <gregcaporaso@gmail.com>
2015-08-06 10:17:24 -0700

    DOC: added doc strings

--- skbio/diversity/beta/_stats.py
+++ skbio/diversity/beta/_stats.py
@@ -11,6 +11,57 @@ from functools import partial
 
 
 def unweighted_unifrac(u, v, otu_ids, tree):
+    """ Compute unweighted unifrac
+
+    Parameters
+    ----------
+    u, v: list, np.array
+        Vectors of counts of OTUs for two samples. Must be equal length.
+    otu_ids: list, np.array
+        Vector of OTU ids corresponding to tip names in ``tree``. Must be the
+        same length as ``u`` and ``v``.
+    tree: skbio.TreeNode
+        Tree relating the OTUs in otu_ids. The set of tip names in the tree can
+        be a superset of ``otu_ids``, but not a subset.
+
+    Returns
+    -------
+    float
+        The unweighted unifrac distance between the two samples.
+
+    Raises
+    ------
+    ValueError
+        If ``u``, ``v``, and ``otu_ids`` are not all equal in length.
+    MissingNodeError
+        If an OTU id is provided that does not correspond to a tip in the
+        tree.
+
+    See Also
+    --------
+    weighted_unifrac
+
+    Notes
+    -----
+    Unweighted unifrac was originally described in [1]_. A discussion of
+    unweighted (qualitative) versus weighted (quantitiative) diversity metrics
+    is presented in [2]_.
+
+    References
+    ----------
+    .. [1] Lozupone, C. & Knight, R. UniFrac: a new phylogenetic method for
+       comparing microbial communities. Appl. Environ. Microbiol. 71, 8228–8235
+       (2005).
+
+    .. [2] Lozupone, C. A., Hamady, M., Kelley, S. T. & Knight, R. Quantitative
+       and qualitative beta diversity measures lead to different insights into
+       factors that structure microbial communities. Appl. Environ. Microbiol.
+       73, 1576–1585 (2007).
+
+    """
+    if len(u) != len(v) != len(otu_ids):
+        raise ValueError("Input vectors u, v, and otu_ids must be equal"
+                         " length.")
     u_observed_otus = {o: u for o, u in zip(otu_ids, u) if u >= 1}
     v_observed_otus = {o: v for o, v in zip(otu_ids, v) if v >= 1}
     observed_nodes1 = set(tree.observed_node_counts(u_observed_otus))
@@ -37,6 +88,53 @@ def _sample_branch_weight(observed_nodes, total_count):
 
 
 def weighted_unifrac(u, v, otu_ids, tree, normalized=False):
+    """ Compute weighted unifrac with or without branch length normalization
+
+    Parameters
+    ----------
+    u, v: list, np.array
+        Vectors of counts of OTUs for two samples. Must be equal length.
+    otu_ids: list, np.array
+        Vector of OTU ids corresponding to tip names in ``tree``. Must be the
+        same length as ``u`` and ``v``.
+    tree: skbio.TreeNode
+        Tree relating the OTUs in otu_ids. The set of tip names in the tree can
+        be a superset of ``otu_ids``, but not a subset.
+    normalized: boolean, optional
+        If ``True``, apply branch length normalization. Resulting distances
+        will then be in the range ``[0, 1]``.
+
+    Returns
+    -------
+    float
+        The weighted unifrac distance between the two samples.
+
+    Raises
+    ------
+    ValueError
+        If ``u``, ``v``, and ``otu_ids`` are not all equal in length.
+    MissingNodeError
+        If an OTU id is provided that does not correspond to a tip in the
+        tree.
+
+    See Also
+    --------
+    unweighted_unifrac
+
+    Notes
+    -----
+    Unweighted unifrac was originally described in [1]_, which includes a
+    discussion of unweighted (qualitative) versus weighted (quantitiative)
+    diversity metrics.
+
+    References
+    ----------
+    .. [1] Lozupone, C. A., Hamady, M., Kelley, S. T. & Knight, R. Quantitative
+       and qualitative beta diversity measures lead to different insights into
+       factors that structure microbial communities. Appl. Environ. Microbiol.
+       73, 1576–1585 (2007).
+
+    """
     u_observed_otus = {o: u for o, u in zip(otu_ids, u) if u >= 1}
     u_total_count = sum(u)
     v_observed_otus = {o: v for o, v in zip(otu_ids, v) if v >= 1}
