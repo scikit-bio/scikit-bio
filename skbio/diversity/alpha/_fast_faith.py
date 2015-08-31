aa7d1d00006c57158bf8fcc59160ad4ea2288cfa
Daniel McDonald <mcdonadt@colorado.edu>
2015-08-31 13:50:29 -0600

    Forgot a file...

--- skbio/diversity/alpha/_fast_faith.py
+++ skbio/diversity/alpha/_fast_faith.py
@@ -11,7 +11,7 @@ from __future__ import absolute_import, division, print_function
 import numpy as np
 
 from skbio.util._decorator import experimental
-from skbio.diversity._fast_base import _counts_and_length
+from skbio.diversity._fast_base import counts_and_index
 from skbio.diversity._base import (_validate_counts_vector,
                                    _validate_otu_ids_and_tree)
 
@@ -32,7 +32,7 @@ def faith_pd_fast(counts, otu_ids, tree, validate=True, indexed=None):
     # If Faith's PD is being calculated for a large number of samples, the
     # count_array could be produced a single time for the samples. This would
     # be much faster than producing it for each sample.
-    count_array, indexed = _counts_and_length(counts, otu_ids, tree, indexed)
+    count_array, indexed = counts_and_index(counts, otu_ids, tree, indexed)
     length = indexed['length']
 
     count_array = np.where(count_array > 0, 1, 0)
