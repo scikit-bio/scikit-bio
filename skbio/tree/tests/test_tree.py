825d23be3f202f2bfdd3f421386aabf37a0682b7
Daniel McDonald <mcdonadt@colorado.edu>
2016-06-01 16:31:48 -0700

    DOC/TST: mention and tests for #807, #809, and #1247

--- skbio/tree/tests/test_tree.py
+++ skbio/tree/tests/test_tree.py
@@ -300,6 +300,12 @@ class TreeTests(TestCase):
         self.assertEqual(len(n.children), 2)
         self.assertNotIn(n, self.simple_t.children)
 
+    def test_prune_root_single_desc(self):
+        t = TreeNode.read(["((a,b)c)extra;"])
+        exp = "(a,b)c;\n"
+        obs = t.prune()
+        self.assertEqual(str(obs), exp)
+
     def test_prune(self):
         """Collapse single descendent nodes"""
         # check the identity case
@@ -553,6 +559,13 @@ class TreeTests(TestCase):
         with self.assertRaises(NoParentError):
             a.accumulate_to_ancestor(b)
 
+    def test_distance_nontip(self):
+        # example derived from issue #807, credit @wwood
+        tstr = "((A:1.0,B:2.0)'g__genus1':3.0)root;"
+        tree = TreeNode.read(io.StringIO(tstr))
+        with self.assertRaises(TreeError):
+            tree.find('A').distance(tree.find('g__genus1'))
+
     def test_distance(self):
         """Get the distance between two nodes"""
         t = TreeNode.read(io.StringIO(
@@ -576,6 +589,13 @@ class TreeTests(TestCase):
         self.assertEqual(tips[2].distance(tips[2]), 0.0)
         with self.assertRaises(NoLengthError):
             tips[2].distance(tips[3])
+
+    def test_lowest_common_ancestor_nontip(self):
+        # example derived from issue #807, credit @wwood
+        tstr = "((A:1.0,B:2.0)'g__genus1':3.0)root;"
+        tree = TreeNode.read(io.StringIO(tstr))
+        with self.assertRaises(TreeError):
+            tree.lca(['A', 'g__genus1'])
 
     def test_lowest_common_ancestor(self):
         """TreeNode lowestCommonAncestor should return LCA for set of tips"""
