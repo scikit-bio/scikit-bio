d10aaee45a434c16e49feb539c9d4fc266ea6c3f
Daniel McDonald <mcdonadt@colorado.edu>
2016-06-27 14:54:23 -0700

    TST: bug in TreeNode.prune when a parent has already been dropped

--- skbio/tree/tests/test_tree.py
+++ skbio/tree/tests/test_tree.py
@@ -303,6 +303,13 @@ class TreeTests(TestCase):
         self.assertEqual(len(n.children), 2)
         self.assertNotIn(n, self.simple_t.children)
 
+    def test_shear_prune_parent_dropped(self):
+        bugtree = "((a,b),((c,d),(e,f)));"
+        to_keep = ['c', 'd']
+        exp = "(c,d);"
+        obs = str(TreeNode.read(io.StringIO(bugtree)).shear(to_keep))
+        self.assertEqual(obs, exp)
+
     def test_prune_root_single_desc(self):
         t = TreeNode.read(["((a,b)c)extra;"])
         exp = "(a,b)c;\n"
