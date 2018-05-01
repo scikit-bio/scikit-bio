330cf4fba3b4db191e03c3e52920515a594d9b0e
Hannes Holste <hannes.holste@gmail.com>
2018-04-30 18:39:31 -0700

    Optimized distance matrix centering functions

--- skbio/stats/ordination/_utils.py
+++ skbio/stats/ordination/_utils.py
@@ -196,3 +196,60 @@ def f_matrix(E_matrix):
     col_means = E_matrix.mean(axis=0, keepdims=True)
     matrix_mean = E_matrix.mean()
     return E_matrix - row_means - col_means + matrix_mean
+
+
+def center_distance_matrix_optimized(distance_matrix):
+    """
+    # FYI: If the used distance was euclidean, pairwise distances
+    # needn't be computed from the data table Y because F_matrix =
+    # Y.dot(Y.T) (if Y has been centred).
+    # But since we're expecting distance_matrix to be non-euclidian,
+    # we do the following computation as per
+    # Numerical Ecology (Legendre & Legendre 1998)
+    """
+
+    return f_matrix_optimized(e_matrix_optimized(distance_matrix))
+
+
+def e_matrix_optimized(distance_matrix):
+    """
+    Compute E matrix from a distance matrix.
+    Squares and divides by -2 the input elementwise. Eq. 9.20 in
+    Legendre & Legendre 1998.
+    """
+
+    # modified from skbio e_matrix function,
+    # performing row-wise to avoid excessive memory allocations
+    for i in np.arange(len(distance_matrix)):
+        distance_matrix[i] = (distance_matrix[i] * distance_matrix[i]) / -2
+    return distance_matrix
+
+
+def f_matrix_optimized(e_matrix):
+    """
+    Compute F matrix from E matrix.
+    Centring step: for each element, the mean of the corresponding
+    row and column are substracted, and the mean of the whole
+    matrix is added. Eq. 9.21 in Legendre & Legendre 1998.
+    """
+
+    # modified from skbio f_matrix function,
+    # performing rowwise to avoid excessive memory allocations
+    row_means = np.zeros(len(e_matrix), dtype=float)
+    col_means = np.zeros(len(e_matrix), dtype=float)
+    matrix_mean = 0.0
+
+    for i in np.arange(len(e_matrix)):
+        row_means[i] = e_matrix[i].mean()
+        matrix_mean += e_matrix[i].sum()
+        col_means += e_matrix[i]
+    matrix_mean /= len(e_matrix) ** 2
+    col_means /= len(e_matrix)
+
+    for i in np.arange(len(e_matrix)):
+        v = e_matrix[i]
+        v -= row_means[i]
+        v -= col_means
+        v += matrix_mean
+        e_matrix[i] = v
+    return e_matrix
