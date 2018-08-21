49b948b897fe63237054b3252d3b1f9ae090d86e
Hannes Holste <hannes.holste@gmail.com>
2018-08-20 21:51:51 -0700

    Fix: make inplace f_matrix and e_matrix accessible to unit test again

--- skbio/stats/ordination/__init__.py
+++ skbio/stats/ordination/__init__.py
@@ -130,10 +130,12 @@ from ._canonical_correspondence_analysis import cca
 from ._principal_coordinate_analysis import pcoa, pcoa_biplot
 from ._ordination_results import OrdinationResults
 from ._utils import (mean_and_std, scale, svd_rank, corr, e_matrix, f_matrix,
-                     center_distance_matrix)
+                     center_distance_matrix, _e_matrix_inplace,
+                     _f_matrix_inplace)
 
 __all__ = ['ca', 'rda', 'cca', 'pcoa', 'pcoa_biplot', 'OrdinationResults',
            'mean_and_std', 'scale', 'svd_rank', 'corr',
-           'e_matrix', 'f_matrix', 'center_distance_matrix']
+           'e_matrix', 'f_matrix', 'center_distance_matrix',
+           '_e_matrix_inplace', '_f_matrix_inplace']
 
 test = TestRunner(__file__).test
