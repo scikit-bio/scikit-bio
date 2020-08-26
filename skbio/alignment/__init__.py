eea6d18ce52084fcc7eb99bf8641a22e2873af63
rainarit <rraina@ucsd.edu>
2020-08-26 15:13:58 -0700

    Update __init__.py

--- skbio/alignment/__init__.py
+++ skbio/alignment/__init__.py
@@ -1,41 +1,57 @@
 r"""
 Alignments (:mod:`skbio.alignment`)
 ===================================
+
 .. currentmodule:: skbio.alignment
+
 This module provides functionality for computing and manipulating sequence
 alignments. DNA, RNA, and protein sequences can be aligned, as well as
 sequences with custom alphabets.
+
 Data Structures
 ---------------
+
 .. autosummary::
    :toctree: generated/
+
    TabularMSA
+
 Optimized (i.e., production-ready) Alignment Algorithms
 -------------------------------------------------------
+
 .. autosummary::
    :toctree: generated/
+
    StripedSmithWaterman
    AlignmentStructure
    local_pairwise_align_ssw
+
 Slow (i.e., educational-purposes only) Alignment Algorithms
 -----------------------------------------------------------
+
 .. autosummary::
    :toctree: generated/
+
    global_pairwise_align_nucleotide
    global_pairwise_align_protein
    global_pairwise_align
    local_pairwise_align_nucleotide
    local_pairwise_align_protein
    local_pairwise_align
+
 General functionality
 ---------------------
+
 .. autosummary::
    :toctree: generated/
+
     make_identity_substitution_matrix
+
 Data Structure Examples
 -----------------------
 Load two DNA sequences that have been previously aligned into a ``TabularMSA``
 object, using sequence IDs as the MSA's index:
+
 >>> from skbio import TabularMSA, DNA
 >>> seqs = [DNA("ACC--G-GGTA..", metadata={'id': "seq1"}),
 ...         DNA("TCC--G-GGCA..", metadata={'id': "seq2"})]
@@ -51,11 +67,14 @@ ACC--G-GGTA..
 TCC--G-GGCA..
 >>> msa.index
 Index(['seq1', 'seq2'], dtype='object')
+
 Alignment Algorithm Examples
 ----------------------------
+
 Optimized Alignment Algorithm Examples
 --------------------------------------
 Using the convenient ``local_pairwise_align_ssw`` function:
+
 >>> from skbio.alignment import local_pairwise_align_ssw
 >>> alignment, score, start_end_positions = local_pairwise_align_ssw(
 ...     DNA("ACTAAGGCTCTCTACCCCTCTCAGAGA"),
@@ -74,7 +93,9 @@ ACTAAGGCTC-CTAACCCCCTTTTCTCAGA
 27
 >>> start_end_positions
 [(0, 24), (0, 28)]
+
 Using the ``StripedSmithWaterman`` object:
+
 >>> from skbio.alignment import StripedSmithWaterman
 >>> query = StripedSmithWaterman("ACTAAGGCTCTCTACCCCTCTCAGAGA")
 >>> alignment = query("AAAAAACTCTCTAAACTCACTAAGGCTCTCTACCCCTCTTCAGAGAAGTCGA")
@@ -83,8 +104,10 @@ ACTAAGGCTC...
 ACTAAGGCTC...
 Score: 49
 Length: 28
+
 Using the ``StripedSmithWaterman`` object for multiple targets in an efficient
 way and finding the aligned sequence representations:
+
 >>> from skbio.alignment import StripedSmithWaterman
 >>> alignments = []
 >>> target_sequences = [
@@ -108,6 +131,7 @@ Length: 30
 ACTAAGGCTC---TCTACCCCTCTCAGAGA
 >>> print(alignments[0].aligned_target_sequence)
 ACT-AGGCTCCCTTCTACCCCTCTCAGAGA
+
 Slow Alignment Algorithm Examples
 ---------------------------------
 scikit-bio also provides pure-Python implementations of Smith-Waterman and
@@ -119,17 +143,21 @@ differ in the underlying algorithm that is applied (``global*`` uses Needleman-
 Wunsch while ``local*`` uses Smith-Waterman), and ``*protein`` and
 ``*nucleotide`` differ in their default scoring of matches, mismatches, and
 gaps.
+
 Here we locally align a pair of protein sequences using gap open penalty
 of 11 and a gap extend penalty of 1 (in other words, it is much more
 costly to open a new gap than extend an existing one).
+
 >>> from skbio import Protein
 >>> from skbio.alignment import local_pairwise_align_protein
 >>> s1 = Protein("HEAGAWGHEE")
 >>> s2 = Protein("PAWHEAE")
 >>> alignment, score, start_end_positions = local_pairwise_align_protein(
 ...     s1, s2, 11, 1)
+
 This returns an ``skbio.TabularMSA`` object, the alignment score, and start/end
 positions of each aligned sequence:
+
 >>> alignment
 TabularMSA[Protein]
 ---------------------
@@ -143,7 +171,9 @@ AW-HE
 25.0
 >>> start_end_positions
 [(4, 8), (1, 4)]
+
 Similarly, we can perform global alignment of nucleotide sequences:
+
 >>> from skbio import DNA
 >>> from skbio.alignment import global_pairwise_align_nucleotide
 >>> s1 = DNA("GCGTGCCTAAGGTATGCAAG")
@@ -159,6 +189,7 @@ Stats:
 ----------------------
 GCGTGCCTAAGGTATGCAAG
 ACGTGCCTA-GGTACGCAAG
+
 """
 
 # ----------------------------------------------------------------------------
