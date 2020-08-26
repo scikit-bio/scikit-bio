a26b675cdc2757cd84643b9e2583464266c31c4a
Jamie Morton <jamietmorton@gmail.com>
2020-08-26 10:37:27 -0600

    Ordination fix (#1720)
    * fixing tests for ordination
    
    * flake8
    
    * changelog
    
    * ugh changelog
    
    * removing extra assert
    
    * adding dists to biplot scores and constraints

--- skbio/stats/ordination/tests/test_redundancy_analysis.py
+++ skbio/stats/ordination/tests/test_redundancy_analysis.py
@@ -245,7 +245,6 @@ class TestRDAResults_biplot_score(TestCase):
             proportion_explained=vegan_propexpl,
             eigvals=vegan_eigvals)
 
-        pdt.assert_frame_equal(res_samples, vegan_samples)
         # This scaling constant is required to make skbio comparable to vegan.
         scaling = (rda_.eigvals[0] / rda_.eigvals[:6])
         exp.biplot_scores *= scaling
