26a79488b73f553f9d10f18843f7a25ebda52bfa
Igor Sfiligoi <isfiligoi@ucsd.edu>
2021-02-05 08:42:24 -0800

    Revert ordination/__init__ changes; no reason too expose the cython versions to the top level

--- skbio/stats/ordination/__init__.py
+++ skbio/stats/ordination/__init__.py
@@ -136,12 +136,7 @@ from ._principal_coordinate_analysis import pcoa, pcoa_biplot
 from ._ordination_results import OrdinationResults
 from ._utils import (mean_and_std, scale, svd_rank, corr, e_matrix, f_matrix,
                      center_distance_matrix)
-from ._cutils import (e_matrix_means_cy,  f_matrix_inplace_cy,
-                      center_distance_matrix_cy)
-
 
 __all__ = ['ca', 'rda', 'cca', 'pcoa', 'pcoa_biplot', 'OrdinationResults',
            'mean_and_std', 'scale', 'svd_rank', 'corr',
-           'e_matrix', 'f_matrix', 'center_distance_matrix',
-           'e_matrix_means_cy',  'f_matrix_inplace_cy',
-           'center_distance_matrix_cy']
+           'e_matrix', 'f_matrix', 'center_distance_matrix']
