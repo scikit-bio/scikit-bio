ecc78ece42ee2fb0658cb0b6586cd5558568d30c
Daniel McDonald <mcdonadt@colorado.edu>
2022-03-31 17:25:56 -0700

    REL: denote 0.5.7 release (#1806)

--- setup.py
+++ setup.py
@@ -65,10 +65,10 @@ classes = """
     Topic :: Scientific/Engineering :: Bio-Informatics
     Programming Language :: Python :: 3
     Programming Language :: Python :: 3 :: Only
-    Programming Language :: Python :: 3.4
-    Programming Language :: Python :: 3.5
-    Programming Language :: Python :: 3.6
     Programming Language :: Python :: 3.7
+    Programming Language :: Python :: 3.8
+    Programming Language :: Python :: 3.9
+    Programming Language :: Python :: 3.10
     Operating System :: Unix
     Operating System :: POSIX
     Operating System :: MacOS :: MacOS X
