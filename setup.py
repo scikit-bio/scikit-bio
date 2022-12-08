2b96f16ed80840b6fa16f5adb072c1482d5fb362
Evan Bolyen <ebolyen@gmail.com>
2022-12-08 16:54:56 -0700

    WIP: initial tests

--- setup.py
+++ setup.py
@@ -65,10 +65,10 @@ classes = """
     Topic :: Scientific/Engineering :: Bio-Informatics
     Programming Language :: Python :: 3
     Programming Language :: Python :: 3 :: Only
-    Programming Language :: Python :: 3.7
     Programming Language :: Python :: 3.8
     Programming Language :: Python :: 3.9
     Programming Language :: Python :: 3.10
+    Programming Language :: Python :: 3.11
     Operating System :: Unix
     Operating System :: POSIX
     Operating System :: MacOS :: MacOS X
