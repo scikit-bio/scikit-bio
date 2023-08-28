8974aff8579febd384c67fc51013774300bf197c
Igor Sfiligoi <isfiligoi@sdsc.edu>
2023-08-28 15:44:26 -0700

    Remove spurious space

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ USE_CYTHON = os.environ.get('USE_CYTHON')
 if USE_CYTHON is None:
     # use CYTHON by default
     USE_CYTHON = True
-elif  USE_CYTHON.lower() in {'false', 'no'}:
+elif USE_CYTHON.lower() in {'false', 'no'}:
     USE_CYTHON = False
 else:
     USE_CYTHON = True
