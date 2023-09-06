e8f90acb2e67d1a449948636dddca65463fdcb49
Igor Sfiligoi <isfiligoi@ucsd.edu>
2023-09-06 12:26:32 -0700

    Reenable OpenMP (#1874)
    * Add openmp for stats and clean up compiler detection
    
    * Fix integer division
    
    * Add link args
    
    * Update c files
    
    * Enable CYTHON by default
    
    * Update CHANGELOG
    
    * Remove spurious space
    
    * Disable cythong during lint
    
    * Improve comments
    
    * Move compiler detection fully into the top part, and make it more thorough
    
    * Fix lint problems

--- setup.py
+++ setup.py
@@ -26,15 +26,70 @@ if sys.version_info.major != 3:
     sys.exit("scikit-bio can only be used with Python 3. You are currently "
              "running Python %d." % sys.version_info.major)
 
+
+def check_bin(ccbin, source, allow_dash):
+    # remove any parameters (e.g. gcc -I /a/b/c -> gcc)
+    source0 = source.split()[0]
+    # remove any path
+    bsource = os.path.basename(source0)
+    # now let's go search for ccbin
+    if allow_dash:
+        found = False
+        # allow for the ccbin to be between - (e.g. gcc-1.2)
+        for el in bsource.split('-'):
+            if el == ccbin:
+                found = True
+                break
+    else:
+        found = (bsource == ccbin)
+    return found
+
+
+# Note: We are looking for Apple/MacOS clang, which does not support omp
+#       Will treat "real clang" (e.g. llvm based) same as gcc
 clang = False
+
+# icc uses slightly different omp cmdline arguments
 icc = False
+
+# Are we using the default gcc as the compiler?
+gcc = True
 try:
-    if os.environ['CC'] == "clang":
-        clang = True
+    if os.environ['CC'] == "gcc":
+        gcc = True
+    elif os.environ['CC'] != "":
+        gcc = False
 except KeyError:
     pass
 
-if not clang:
+
+if not gcc:
+    try:
+        if check_bin('clang', os.environ['CC'], False):
+            # note, the conda provideed clang is not detected here
+            # and this is on purpose, as MacOS clang is very different
+            # than conda-provised one (which is llvm based)
+            # so do not look for substrings
+            # (e.g. do not match x86_64-apple-darwin13.4.0-clang)
+            clang = True
+        elif check_bin('icc', os.environ['CC'], True):
+            icc = True
+    except KeyError:
+        pass
+else:
+    try:
+        if check_bin('clang', sysconfig.get_config_vars()['CC'], False):
+            # as above
+            clang = True
+            gcc = False
+        elif check_bin('icc', sysconfig.get_config_vars()['CC'], True):
+            icc = True
+            gcc = False
+    except KeyError:
+        pass
+
+if gcc:
+    # check if the default gcc is just a wrapper around clang
     try:
         if subprocess.check_output(
                 ["gcc", "--version"],
@@ -42,12 +97,6 @@ if not clang:
             clang = True
     except (subprocess.CalledProcessError, FileNotFoundError):
         pass
-
-try:
-    if os.environ['CC'] == "icc":
-        icc = True
-except KeyError:
-    pass
 
 # version parsing from __init__ pulled from Flask's setup.py
 # https://github.com/mitsuhiko/flask/blob/master/setup.py
@@ -83,7 +132,10 @@ with open('README.rst') as f:
 
 # Dealing with Cython
 USE_CYTHON = os.environ.get('USE_CYTHON')
-if USE_CYTHON is None or USE_CYTHON.lower() in {'false', 'no'}:
+if USE_CYTHON is None:
+    # use CYTHON by default
+    USE_CYTHON = True
+elif USE_CYTHON.lower() in {'false', 'no'}:
     USE_CYTHON = False
 else:
     USE_CYTHON = True
@@ -93,14 +145,24 @@ ext = '.pyx' if USE_CYTHON else '.c'
 ssw_extra_compile_args = ['-I.']
 
 if platform.system() != 'Windows':
-    if icc or sysconfig.get_config_vars()['CC'] == 'icc':
+    if icc:
         ssw_extra_compile_args.extend(['-qopenmp-simd',
                                        '-DSIMDE_ENABLE_OPENMP'])
-    elif not (clang or sysconfig.get_config_vars()['CC'] == 'clang'):
+    elif not clang:
         ssw_extra_compile_args.extend(['-fopenmp-simd',
                                        '-DSIMDE_ENABLE_OPENMP'])
 elif platform.system() == 'Windows':
     ssw_extra_compile_args.extend(['-openmp:experimental'])
+
+stats_extra_compile_args = []+ssw_extra_compile_args
+stats_extra_link_args = []
+if platform.system() != 'Windows':
+    if icc:
+        stats_extra_compile_args.extend(['-qopenmp'])
+        stats_extra_link_args.extend(['-qopenmp'])
+    elif not clang:
+        stats_extra_compile_args.extend(['-fopenmp'])
+        stats_extra_link_args.extend(['-fopenmp'])
 
 # Users with i686 architectures have reported that adding this flag allows
 # SSW to be compiled. See https://github.com/biocore/scikit-bio/issues/409 and
@@ -124,10 +186,12 @@ extensions = [
               include_dirs=[np.get_include()]),
     Extension("skbio.stats.ordination._cutils",
               ["skbio/stats/ordination/_cutils" + ext],
-              extra_compile_args=ssw_extra_compile_args),
+              extra_compile_args=stats_extra_compile_args,
+              extra_link_args=stats_extra_link_args),
     Extension("skbio.stats.distance._cutils",
               ["skbio/stats/distance/_cutils" + ext],
-              extra_compile_args=ssw_extra_compile_args),
+              extra_compile_args=stats_extra_compile_args,
+              extra_link_args=stats_extra_link_args),
 ]
 
 if USE_CYTHON:
