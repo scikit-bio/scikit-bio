cd142e46905409b97c390e41f2cf9f1c78931945
LarsHunger <Lars.Hunger.314@gmail.com>
2024-03-18 16:50:30 -0400

    Added qiime2 copyright information to _metadata.py

--- skbio/metadata/_metadata.py
+++ skbio/metadata/_metadata.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2023, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
 import abc
 import collections
 import itertools
