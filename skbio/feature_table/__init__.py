247f6ec22b6f6123f766aa1d3034f520e1e1c9a0
Daniel McDonald <danielmcdonald@ucsd.edu>
2024-03-18 20:41:41 -0700

    STY/DOC: docstring for feature_table module

--- skbio/feature_table/__init__.py
+++ skbio/feature_table/__init__.py
@@ -1,3 +1,12 @@
+r"""Feature Table (:mod:`skbio.feature_table`)
+==========================
+
+.. currentmodule:: skbio.feature_table
+
+This module provides support for interaction with BIOM Tables.
+
+"""  # noqa: D205, D415
+
 # ----------------------------------------------------------------------------
 # Copyright (c) 2013--, scikit-bio development team.
 #
@@ -5,7 +14,6 @@
 #
 # The full license is in the file LICENSE.txt, distributed with this software.
 # ----------------------------------------------------------------------------
-
 from skbio.feature_table._base import Table
 
 __all__ = ['Table']
