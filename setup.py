3510d4bfd53aa61811502296483713fff564c3b9
Matt Aton <matthewaton2@gmail.com>
2024-04-05 09:46:40 -0700

    Remove all hdmedians imports and requirements (#2004)

--- setup.py
+++ setup.py
@@ -241,7 +241,6 @@ setup(
         "pandas >= 1.5.0",
         "scipy >= 1.9.0",
         "h5py >= 3.6.0",
-        "hdmedians >= 0.14.1",
         "biom-format",
     ],
     classifiers=classifiers,
