028e0a4c08de812a25cafe01a97c2353c31a5925
Alex Cagle <ajcagle@ucsd.edu>
2024-07-05 10:02:18 -0700

    Fixing test error

--- skbio/stats/ordination/tests/test_ordination_results.py
+++ skbio/stats/ordination/tests/test_ordination_results.py
@@ -149,7 +149,7 @@ class TestOrdinationResults(unittest.TestCase):
                          ['abc_0', 'def_0', 'Z_0'])
 
         # Testing rename when features is None
-        errmsg = ("'features' were not provided on the construction of this"
+        errmsg = ("`features` were not provided on the construction of this"
                   " object")
         self.ordination_results.features = None
         with self.assertRaisesRegex(ValueError, errmsg):
