c4277a60687298918723479deedbb42526ab5ffb
Paarth Batra <paarthb@icloud.com>
2024-08-04 16:41:04 -0700

    Added Test for Write Error (#2097)

--- skbio/io/format/tests/test_embed.py
+++ skbio/io/format/tests/test_embed.py
@@ -8,11 +8,13 @@
 from unittest import TestCase, main
 from pathlib import Path
 import tempfile
+from unittest.mock import MagicMock
 
 import h5py
 import numpy as np
 
 import skbio
+from skbio.io import read, write
 from skbio import Protein
 from skbio.util import get_data_path
 from skbio.embedding._protein import ProteinEmbedding
@@ -24,6 +26,60 @@ from skbio.io.format.embed import (
     _embed_to_protein, _protein_to_embed,
     _protein_to_vector, _vector_to_protein
 )
+
+from transformers import T5Tokenizer, T5EncoderModel
+from tqdm import tqdm
+import torch
+import re
+
+class TestWriteError(TestCase):
+    def test_write_function(self):
+        model_name = "Rostlab/prot_t5_xl_uniref50"
+        tokenizer_name = "Rostlab/prot_t5_xl_uniref50"
+        sequence_list = ["MKTVRQERLKSIVRILERSKEPVSGAQLAEELSVSRQVIVQDIAYLRSLGYNIVATPRGYVLAGG", "MKTVRQERLKSIVRILERSKEPVSGAQLAEELSVSRQVIVQDIAYLRSLGYNIVATPRGYVLAGG"]
+
+        # Mock implementation of to_embeddings function
+        def mock_load_protein_t5_embedding(sequence, model_name, tokenizer_name):   
+            tokenizer = T5Tokenizer.from_pretrained(tokenizer_name)
+            model = T5EncoderModel.from_pretrained(model_name)
+            
+            # convert sequence to formatted list of strings
+            seq_list = []
+            seq_list.append(sequence)
+            seqs = [" ".join(list(re.sub(r"[UZOB]", "X", str(seq)))) for seq in seq_list]
+            
+            # tokenize sequences and pad up to the longest sequence in the batch
+            ids = tokenizer.batch_encode_plus(seqs, add_special_tokens=True, padding="longest")
+            input_ids = torch.tensor(ids['input_ids'])
+            attention_mask = torch.tensor(ids['attention_mask'])
+            
+            # generate embeddings
+            with torch.no_grad():
+                embedding_repr = model(input_ids=input_ids,attention_mask=attention_mask)
+            emb = embedding_repr.last_hidden_state[0, :-1, :].squeeze().detach().cpu().numpy()
+            
+            return ProteinEmbedding(emb, sequence)
+
+        # Mock implementation of load_protein_t5_embedding function
+        def mock_to_embeddings(sequences : list, model_name, tokenizer_name):
+            # Embed the random/inputted protein sequence(s)
+            for sequence in tqdm(sequences):
+                test_embed = load_protein_t5_embedding(str(sequence), model_name, tokenizer_name)
+                #reshape embeddings to fit the skbio format
+                yield test_embed
+
+        # Replace the to_embeddings and load_protein_t5_embedding functions with mock implementations
+        to_embeddings = MagicMock(side_effect=mock_to_embeddings)
+        load_protein_t5_embedding = MagicMock(side_effect=mock_load_protein_t5_embedding)
+
+        model_name = "Rostlab/prot_t5_xl_uniref50"
+        tokenizer_name = "Rostlab/prot_t5_xl_uniref50"
+
+        # Parse bagel.fa
+        sequence_list = read("pdb_hits.fa", format='fasta')
+        embed_list = to_embeddings(sequence_list, model_name, tokenizer_name)
+        write(embed_list, format='embed', into='data/test_pdb_hits.h5')
+        # Add assertions to check if the file is created and contains the expected data      
 
 
 class EmbedTests(TestCase):
