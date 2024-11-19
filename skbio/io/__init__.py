e6085c4cd0c320dcdedc7eb722f5080c444dd5f0
Matt Aton <matthewaton2@gmail.com>
2024-11-19 15:44:46 -0700

    Added links at top of io module doc to point to relevant resources

--- skbio/io/__init__.py
+++ skbio/io/__init__.py
@@ -5,6 +5,18 @@ r"""Input and Output (:mod:`skbio.io`)
 
 This module provides input/output (I/O) functionality for scikit-bio.
 
+For general guidance on reading and writing files and working with scikit-bio objects,
+see the :ref:`tutorial` section and the
+`Reading and writing files <https://github.com/scikit-bio/scikit-bio-cookbook/blob/\
+master/Reading%20and%20writing%20files.ipynb>`_
+notebook. For guidance on a specific format or scikit-bio object,
+see the documentation for that specific format or object.
+
+See the
+`IORegistry docs <https://scikit.bio/docs/latest/generated/skbio.io.registry.html\
+#creating-a-new-format-for-scikit-bio>`_
+for guidance on custom format creation and registering custom readers, writers, and
+sniffers.
 
 Supported file formats
 ----------------------
@@ -108,8 +120,28 @@ Exceptions and warnings
    StockholmFormatError
 
 
+.. _tutorial:
+
 Tutorial
 --------
+
+The :mod:`skbio.io` module was developed to handle the many-to-many relationships which
+exist between scikit-bio objects and scikit-bio formats. The main feature of the module
+is the :class:`skbio.io.registry.IORegistry` class, which is implemented as a single
+instance as ``io_registry``. This registry contains all the information regarding the
+relationships between scikit-bio supported formats and scikit-bio objects. For example,
+the FASTA format (:mod:`skbio.io.format.fasta`) could be read into several scikit-bio
+objects:  :class:`skbio.alignment.TabularMSA`, :class:`skbio.sequence.DNA`, and
+:class:`skbio.sequence.Protein`, just to name a few.
+
+Additionally, any given scikit-bio object may write to several different supported
+formats. The :class:`skbio.alignment.TabularMSA` object may be written to 5 different
+formats, not including any custom formats that you the user may create. For more
+information regarding **custom format creation in scikit-bio**, see
+:mod:`skbio.io.registry`.
+
+The examples in this tutorial are relatively simple. For more extensive examples,
+see
 
 Reading and writing files (I/O) can be a complicated task:
 
@@ -129,7 +161,7 @@ Reading and writing files (I/O) can be a complicated task:
 
 To address these issues (and others), scikit-bio provides a simple, powerful
 interface for dealing with I/O. We accomplish this by using a single I/O
-registry.
+registry defined in :class:`skbio.io.registry.IORegistry`.
 
 What kinds of files scikit-bio can use
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
