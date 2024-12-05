07daca3b5694cd13d123fe324b77f6adb898a823
Qiyun Zhu <qiyunzhu@gmail.com>
2024-12-04 17:27:25 -0700

    dropped support for Python 3.8

--- setup.py
+++ setup.py
@@ -124,7 +124,6 @@ classes = """
     Topic :: Scientific/Engineering :: Bio-Informatics
     Programming Language :: Python :: 3
     Programming Language :: Python :: 3 :: Only
-    Programming Language :: Python :: 3.8
     Programming Language :: Python :: 3.9
     Programming Language :: Python :: 3.10
     Programming Language :: Python :: 3.11
