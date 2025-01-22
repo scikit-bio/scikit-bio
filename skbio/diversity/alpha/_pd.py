ad9fa23452751b3c9f484218e4b19540878e2879
Matt Aton <matthewaton2@gmail.com>
2025-01-22 11:53:35 -0700

    Remove unused sys import

--- skbio/diversity/alpha/_pd.py
+++ skbio/diversity/alpha/_pd.py
@@ -6,8 +6,6 @@
 # The full license is in the file LICENSE.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 
-from sys import modules
-
 import numpy as np
 
 from skbio.diversity._util import (
