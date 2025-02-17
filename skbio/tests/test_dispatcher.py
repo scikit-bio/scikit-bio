73ec508d71faf14f9305d007d3216a18e002a8bc
Matt Aton <matthewaton2@gmail.com>
2025-02-17 15:25:32 -0700

    Add unit tests for create_table_1d function

--- skbio/tests/test_dispatcher.py
+++ skbio/tests/test_dispatcher.py
@@ -11,6 +11,7 @@ import unittest
 import numpy as np
 import numpy.testing as npt
 import pandas as pd
+import pandas.testing as pdt
 
 try:
     import polars as pl
@@ -29,6 +30,7 @@ class TestPandas(unittest.TestCase):
     def setUp(self):
         set_option("tabular_backend", "pandas")
         self.data = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+        self.data_1d = self.data[0]
         self.index = ["A", "B", "C"]
         self.columns = ["f1", "f2", "f3"]
 
@@ -60,11 +62,36 @@ class TestPandas(unittest.TestCase):
         with self.assertRaisesRegex(ValueError, "Unsupported backend: 'nonsense'"):
             create_table(self.data, backend="nonsense")
 
+    def test_create_table_1d_no_backend(self):
+        obs = create_table_1d(self.data_1d, index=self.index)
+        exp = pd.Series(self.data_1d, index=self.index)
+        pdt.assert_series_equal(obs, exp)
+
+    def test_create_table_1d_no_optionals(self):
+        obs = create_table_1d(self.data_1d)
+        exp = pd.Series(self.data_1d)
+        pdt.assert_series_equal(obs, exp)
+
+    def test_create_table_1d_same_backend(self):
+        obs = create_table_1d(self.data_1d, index=self.index, backend="pandas")
+        exp = pd.Series(self.data_1d, index=self.index)
+        pdt.assert_series_equal(obs, exp)
+
+    def test_create_table_1d_numpy_backend(self):
+        obs = create_table_1d(self.data_1d, index=self.index, backend="numpy")
+        exp = np.array(self.data_1d)
+        npt.assert_array_equal(obs, exp)
+
+    def test_create_table_1d_bad_backend(self):
+        with self.assertRaisesRegex(ValueError, "Unsupported backend: 'nonsense'"):
+            create_table_1d(self.data, backend="nonsense")
+
 
 class TestNumpy(unittest.TestCase):
     def setUp(self):
         set_option("tabular_backend", "numpy")
         self.data = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+        self.data_1d = self.data[0]
         self.index = ["A", "B", "C"]
         self.columns = ["f1", "f2", "f3"]
 
@@ -99,12 +126,37 @@ class TestNumpy(unittest.TestCase):
         with self.assertRaisesRegex(ValueError, "Unsupported backend: 'nonsense'"):
             create_table(self.data, backend="nonsense")
 
+    def test_create_table_1d_no_backend(self):
+        obs = create_table_1d(self.data_1d, index=self.index)
+        exp = np.array(self.data_1d)
+        npt.assert_array_equal(obs, exp)
+
+    def test_create_table_1d_no_optionals(self):
+        obs = create_table_1d(self.data_1d)
+        exp = np.array(self.data_1d)
+        npt.assert_array_equal(obs, exp)
+
+    def test_create_table_1d_same_backend(self):
+        obs = create_table_1d(self.data_1d, index=self.index, backend="numpy")
+        exp = np.array(self.data_1d)
+        npt.assert_array_equal(obs, exp)
+
+    def test_create_table_1d_pandas_backend(self):
+        obs = create_table_1d(self.data_1d, index=self.index, backend="pandas")
+        exp = pd.Series(self.data_1d, index=self.index)
+        pdt.assert_series_equal(obs, exp)
+
+    def test_create_table_1d_bad_backend(self):
+        with self.assertRaisesRegex(ValueError, "Unsupported backend: 'nonsense'"):
+            create_table_1d(self.data, backend="nonsense")
+
 
 @unittest.skipIf(not has_polars, "Polars is not available for unit tests.")
 class TestPolars(unittest.TestCase):
     def setUp(self):
         set_option("tabular_backend", "polars")
         self.data = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+        self.data_1d = self.data[0]
         self.index = ["A", "B", "C"]
         self.columns = ["f1", "f2", "f3"]
 
@@ -145,6 +197,37 @@ class TestPolars(unittest.TestCase):
     def test_create_table_bad_backend(self):
         with self.assertRaisesRegex(ValueError, "Unsupported backend: 'nonsense'"):
             create_table(self.data, backend="nonsense")
+
+    def test_create_table_1d_no_backend(self):
+        obs = create_table_1d(self.data_1d, index=self.index)
+        exp = pl.Series(self.data_1d)
+        plt.assert_series_equal(obs, exp)
+
+    def test_create_table_1d_no_optionals(self):
+        obs = create_table_1d(self.data_1d)
+        exp = pl.Series(self.data_1d)
+        plt.assert_series_equal(obs, exp)
+
+    def test_create_table_1d_same_backend(self):
+        obs = create_table_1d(self.data_1d, index=self.index, backend="polars")
+        exp = pl.Series(self.data_1d)
+        plt.assert_series_equal(obs, exp)
+
+    def test_create_table_1d_pandas_backend(self):
+        obs = create_table_1d(self.data_1d, index=self.index, backend="pandas")
+        exp = pd.Series(self.data_1d, index=self.index)
+        pdt.assert_series_equal(obs, exp)
+
+    def test_create_table_1d_numpy_backend(self):
+        obs = create_table(
+            data=self.data, columns=self.columns, index=self.index, backend="numpy"
+        )
+        exp = np.array(self.data)
+        npt.assert_array_equal(obs, exp)
+
+    def test_create_table_1d_bad_backend(self):
+        with self.assertRaisesRegex(ValueError, "Unsupported backend: 'nonsense'"):
+            create_table_1d(self.data, backend="nonsense")
 
 
 if __name__ == "__main__":
