27460b77a0c590de6cb0ae025b6fafec927f9abb
Matt Aton <matthewaton2@gmail.com>
2025-07-11 13:49:57 -0700

    Skip binaries unit tests for now

--- skbio/binaries/_distance.py
+++ skbio/binaries/_distance.py
@@ -12,8 +12,9 @@ from numbers import Integral
 from ..binaries._util import (
     get_api_version as _skbb_get_api_version,
     get_dll as _get_skbb_dll,
-    py_to_bin_random_seed
+    py_to_bin_random_seed,
 )
+
 
 def permanova_available(
     distance_matrix,
@@ -53,17 +54,18 @@ def permanova_available(
 
     """
     # v1 is the minimum version that support permanova
-    if _skbb_get_api_version()>=1: # includes check for library existence
+    # includes check for library existence
+    if _skbb_get_api_version() >= 1:  # pragma: no cover
         # Do some basic sanity checks
         # check it is a positive number
         if not isinstance(permutations, Integral):
             return False
-        elif permutations<1:
+        elif permutations < 1:
             return False
-        if not isinstance(grouping,np.ndarray):
+        if not isinstance(grouping, np.ndarray):
             return False
         return True
-    else:
+    else:  # pragma: no cover
         return False
 
 
@@ -126,11 +128,12 @@ def permanova(
     .. [2] http://cran.r-project.org/web/packages/vegan/index.html
 
     """
-    if _skbb_get_api_version()>=1: # minimum version that support pcoa
-        if permutations<1:
+    # minimum version that support pcoa
+    if _skbb_get_api_version() >= 1:  # pragma: no cover
+        if permutations < 1:
             raise ValueError("permutations must be a positive number")
         int_seed = py_to_bin_random_seed(seed)
-        if isinstance(distance_matrix,np.ndarray):
+        if isinstance(distance_matrix, np.ndarray):
             # already a raw matrix, just use
             distance_matrix_data = distance_matrix
         else:
@@ -138,18 +141,20 @@ def permanova(
             # get internal representations
             distance_matrix_data = distance_matrix.data
         distance_matrix_shape0 = distance_matrix_data.shape[0]
-        if (len(distance_matrix_data.shape)!=2 or
-            distance_matrix_data.shape[1] != distance_matrix_shape0):
-               raise TypeError("distance_matrix not square")
-        if not isinstance(grouping,np.ndarray):
+        if (
+            len(distance_matrix_data.shape) != 2
+            or distance_matrix_data.shape[1] != distance_matrix_shape0
+        ):
+            raise TypeError("distance_matrix not square")
+        if not isinstance(grouping, np.ndarray):
             raise TypeError("grouping not ndarray")
-        if len(grouping.shape)!=1:
+        if len(grouping.shape) != 1:
             raise TypeError("grouping not 1D ndarray")
         distance_matrix_shape0 = distance_matrix_data.shape[0]
-        if grouping.shape[0]!=distance_matrix_shape0:
+        if grouping.shape[0] != distance_matrix_shape0:
             raise TypeError("grouping not the same size as distance_matrix")
         # skbb expects uint32 array, so convert if needed (cheap)
-        if grouping.dtype == np.dtype('uint32'):
+        if grouping.dtype == np.dtype("uint32"):
             grouping_data = grouping
         else:
             grouping_data = grouping.astype(np.uint32)
@@ -160,22 +165,33 @@ def permanova(
         i_grp = grouping_data.ctypes.data_as(ctypes.c_void_p)
         i_n_perm = ctypes.c_uint(permutations)
         i_seed = ctypes.c_int(int_seed)
-        if distance_matrix_data.dtype == np.dtype('float64'):
+        if distance_matrix_data.dtype == np.dtype("float64"):
             o_fstat = ctypes.c_double()
             o_pvalue = ctypes.c_double()
             dll.skbb_permanova_fp64(
-                    i_mdim, i_mat, i_grp, i_n_perm, i_seed,
-                    ctypes.byref(o_fstat), ctypes.byref(o_pvalue))
-        elif distance_matrix_data.dtype == np.dtype('float32'):
+                i_mdim,
+                i_mat,
+                i_grp,
+                i_n_perm,
+                i_seed,
+                ctypes.byref(o_fstat),
+                ctypes.byref(o_pvalue),
+            )
+        elif distance_matrix_data.dtype == np.dtype("float32"):
             o_fstat = ctypes.c_float()
             o_pvalue = ctypes.c_float()
             dll.skbb_permanova_fp32(
-                    i_mdim, i_mat, i_grp, i_n_perm, i_seed,
-                    ctypes.byref(o_fstat), ctypes.byref(o_pvalue))
+                i_mdim,
+                i_mat,
+                i_grp,
+                i_n_perm,
+                i_seed,
+                ctypes.byref(o_fstat),
+                ctypes.byref(o_pvalue),
+            )
         else:
             raise TypeError("distance_matrix type must be either float32 or float64")
         # if we got here, everything went well
         return o_fstat.value, o_pvalue.value
-    else:
+    else:  # pragma: no cover
         raise ImportError("skbb_permanova not available")
-
