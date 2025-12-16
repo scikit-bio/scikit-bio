158a35c3e3f77ad5b4896acc9e2f5323b36c9e96
Matt Aton <matthewaton2@gmail.com>
2025-12-16 16:52:38 -0700

    Begin adding strict format support

--- skbio/io/format/phylip_dm.py
+++ skbio/io/format/phylip_dm.py
@@ -1,20 +1,104 @@
 """PHYLIP distance matrix format (:mod:`skbio.io.format.phylip_dm`)
-===============================================================
+================================================================
 
 .. currentmodule:: skbio.io.format.phylip_dm
 
 The PHYLIP file format can store a distance matrix.
 
-An example PHYLIP-formatted distance matrix taken from
+An example PHYLIP-formatted distance matrix::
 
-        7
-Bovine    0.0000 1.6866 1.7198 1.6606 1.5243 1.6043 1.5905
-Mouse     1.6866 0.0000 1.5232 1.4841 1.4465 1.4389 1.4629
-Gibbon    1.7198 1.5232 0.0000 0.7115 0.5958 0.6179 0.5583
-Orang     1.6606 1.4841 0.7115 0.0000 0.4631 0.5061 0.4710
-Gorilla   1.5243 1.4465 0.5958 0.4631 0.0000 0.3484 0.3083
-Chimp     1.6043 1.4389 0.6179 0.5061 0.3484 0.0000 0.2692
-Human     1.5905 1.4629 0.5583 0.4710 0.3083 0.2692 0.0000"""
+    4
+    Seq1      0.0000 1.6866 1.7198 1.6606
+    Seq2      1.6866 0.0000 1.5232 1.4841
+    Seq3      1.7198 1.5232 0.0000 0.7115
+    Seq4      1.6606 1.4841 0.7115 0.0000
+
+Format Support
+--------------
+**Has Sniffer: Yes**
+
++------+------+---------------------------------------------------------------+
+|Reader|Writer|                          Object Class                         |
++======+======+===============================================================+
+|Yes   |Yes   |:mod:`skbio.stats.distance.DistanceMatrix`                     |
++------+------+---------------------------------------------------------------+
+
+Format Specification
+--------------------
+PHYLIP format is a plain text format containing exactly two sections: a header
+describing the number of sequences in the matrix, followed by the matrix itself.
+
+The format described here is "relaxed" PHYLIP. Relaxed PHYLIP does not require
+that each sequence identifier is exactly 10 characters long like strict PHYLIP does.
+Sequence identifiers and matrix data are separated by whitespace (spaces or tabs).
+
+The format described here is "sequential" format. The original PHYLIP specification
+describes both sequential and interleaved formats.
+
+.. note:: scikit-bio currently supports reading and writing relaxed, sequential
+   PHYLIP-formatted files. Strict and/or interleaved PHYLIP formats are not
+   supported.
+
+Header Section
+^^^^^^^^^^^^^^
+The header consists of a single line with a single positive integer (``n``) that
+specifies the number of sequences in the matrix. This **must** be the first line
+in the file. The integer may be preceded by optional whitespace.
+
+.. note:: scikit-bio writes the PHYLIP format header without preceding spaces.
+
+   PHYLIP format does not support blank line(s) between the header and the matrix.
+
+Matrix Section
+^^^^^^^^^^^^^^
+The matrix section immediately follows the header. It consists of ``n`` lines (rows),
+one for each sequence. Each row consists of a sequence identifier (ID) followed by
+the distance values for that sequence, separated by whitespace.
+
+**Square matrices**: Each row contains ``n`` distance values (the full row of the
+distance matrix).
+
+**Lower triangular matrices**: Row ``i`` contains ``i`` distance values (only the
+values below the diagonal). The first row contains no distance values, just the ID.
+
+Sequence IDs
+^^^^^^^^^^^^
+- IDs can have arbitrary length (relaxed format)
+- IDs **must not** contain whitespace characters (spaces, tabs, newlines)
+- IDs **must not** be empty
+- All characters except whitespace and newlines are valid in IDs
+
+.. note:: For strict PHYLIP format compatibility, use IDs of 10 characters or fewer.
+
+Examples
+--------
+Lower triangular matrix::
+
+    4
+    Seq1
+    Seq2	1.5
+    Seq3	2.0	1.0
+    Seq4	3.0	2.5	1.8
+
+Square matrix::
+
+    4
+    Seq1	0.0	1.5	2.0	3.0
+    Seq2	1.5	0.0	1.0	2.5
+    Seq3	2.0	1.0	0.0	1.8
+    Seq4	3.0	2.5	1.8	0.0
+
+
+    https://phylipweb.github.io/phylip/doc/distance.html
+"""
+
+# ----------------------------------------------------------------------------
+# Copyright (c) 2013--, scikit-bio development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+# ----------------------------------------------------------------------------
 
 import numpy as np
 
@@ -27,27 +111,42 @@ phylip_dm = create_format("phylip_dm")
 @phylip_dm.sniffer()
 def _phylip_dm_sniffer(fh):
     # Strategy:
-    #  Read the header and a single sequence; verify that the sequence length matches
-    #  the header information. Don't verify that  the total number of lines matches the
-    #  header information, since that would require reading the whole file.
+    #  Try to parse as relaxed format first (more common).
+    #  If that fails, try strict format.
+    #  Return True if either works.
     try:
         header = next(_line_generator(fh))
         n_seqs = _validate_header(header)
-        # We check the first three lines after the header to enable sniffing of
-        # lower triangle AND square matrices.
+
+        # Collect the first 3 lines for validation
+        lines = []
         for line_no in range(3):
             line = next(_line_generator(fh))
-            _validate_line(line, n_seqs, line_no)
+            lines.append(line)
+
+        # Try relaxed format first
+        try:
+            # print('trying relaxed')
+            for line_no, line in enumerate(lines):
+                _validate_line(line, n_seqs, line_no, strict=False)
+            return True, {}
+        except PhylipFormatError:
+            # print('trying strict')
+            # Relaxed failed, try strict
+            for line_no, line in enumerate(lines):
+                _validate_line(line, n_seqs, line_no, strict=True)
+            return True, {}
+
     except (StopIteration, PhylipFormatError):
+        # Neither format worked
         return False, {}
-    return True, {}
 
 
 @phylip_dm.reader(DistanceMatrix)
-def _phylip_dm_to_distance_matrix(fh, cls=None):
+def _phylip_dm_to_distance_matrix(fh, cls=None, strict=False):
     if cls is None:
         cls = DistanceMatrix
-    return _phylip_to_dm(cls, fh)
+    return _phylip_to_dm(cls, fh, strict=strict)
 
 
 @phylip_dm.writer(DistanceMatrix)
@@ -55,9 +154,11 @@ def _distance_matrix_to_phylip(obj, fh):
     _matrix_to_phylip(obj, fh, delimiter="\t")
 
 
-def _phylip_to_dm(cls, fh):
-    data = _parse_phylip_dm_raw(fh)
+def _phylip_to_dm(cls, fh, strict=False):
+    data = _parse_phylip_dm_raw(fh, strict=strict)
+    print("\n\n\n", data, "\n\n\n")
     dists = [x[0] for x in data]
+    print(dists)
     ids = [x[1] for x in data]
     # If it's in lower triangular form we convert it into condensed form to pass to
     # DistanceMatrix.
@@ -67,36 +168,24 @@ def _phylip_to_dm(cls, fh):
             for col in range(len(dists))
             for row in range(col + 1, len(dists))
         ]
+        print(dists)
     return cls(np.array(dists, dtype=float), ids)
 
 
-def _matrix_to_phylip(obj, fh, delimiter, strict=True):
+def _matrix_to_phylip(obj, fh, delimiter):
     n_samples = obj.shape[0]
     if n_samples < 2:
         raise PhylipFormatError(
             "DistanceMatrix can only be written in PHYLIP format if there are at least"
-            "two samples in the matrix."
+            " two samples in the matrix."
         )
-
-    # For now, sticking with strict PHYLIP format, may implement relaxed in the future
-    chunk_size = 10
     ids = obj.ids
-    if strict:
-        for id in ids:
-            if len(id) > chunk_size:
-                raise PhylipFormatError(
-                    "DistanceMatrix can only be written in PHYLIP format if all ids "
-                    f"have {chunk_size} or fewer characters. Id found that exceeds "
-                    f"this limit: {id}. Use DistanceMatrix.rename to assign shorter "
-                    "ids."
-                )
-
     fh.write(f"{str(n_samples)}\n")
 
     # if the DistanceMatrix is in redundant form, it will be written in redundant form
     if not obj._flags["CONDENSED"]:
         for id_, vals in zip(ids, obj.data):
-            fh.write("%s" % id_)
+            fh.write(id_)
             fh.write(delimiter)
             fh.write(delimiter.join(np.asarray(vals, dtype=str)))
             fh.write("\n")
@@ -104,7 +193,7 @@ def _matrix_to_phylip(obj, fh, delimiter, strict=True):
     # trianglular form
     else:
         for i, id_ in enumerate(ids):
-            fh.write("%s" % id_)
+            fh.write(id_)
             if i > 0:
                 fh.write(delimiter)
                 fh.write(delimiter.join(np.asarray(obj[id_][:i], dtype=str)))
@@ -131,47 +220,90 @@ def _validate_header(header):
     return n_seqs
 
 
-def _validate_line(line, n_seqs, n_dists):
+def _validate_line(line, n_seqs, n_dists, strict=False):
+    """Check that each line contains the expected number of values.
+
+    Parameters
+    ----------
+    line
+        The line of text being validated.
+    n_seqs
+        The number of sequences in the matrix.
+    n_dists
+        The expected number of distances in the line. When a matrix is square, n_dists
+        is equal to n_seqs. When a matrix is lower triangle, n_dists is equal to the
+        current line number (minus the header), i.e. the 0th non-header line should have
+        0 distances, the 1st non-header line should have 1 distance, the 2nd non-header
+        line should have 2 distances.
+
+    """
     if not line:
         raise PhylipFormatError("Empty lines are not allowed.")
-    split_line = line.split()
-    id = split_line[0]
-    dists = split_line[1:]
+    # print(f"strict: {str(strict)}")
+    if strict:
+        id = line[:10].strip()
+        # print(f"id: {str(id)}")
+        dists = line[10:].split()
+        # print(dists)
+    else:
+        split_line = line.split()
+        # IDs are separated from values by whitespace.
+        id = split_line[0]
+        dists = split_line[1:]
     # This check handles lower triangle matrices. We expects 0 distances on the first
     # non-header line, a single distance on the second non-header line, and two
     # distances on the third non-header line.
-    if len(dists) != n_dists:
+    print(f"dists: {dists}")
+    print(f"expected # of dists: {n_dists}")
+    dists_len = len(dists)
+    if dists_len != n_dists:
         # If there are more distances than expected for a lower triangle matrix, we
         # expect that it is a square matrix. In this case we check that the number of
         # distances matches the value specified in the header.
-        if len(dists) != n_seqs:
+        if dists_len != n_seqs:
             raise PhylipFormatError(
                 f"The number of distances {dists} is not {n_seqs} as specified in the "
                 "header."
             )
 
-    return (dists, id)
+    return (dists, id), dists_len
 
 
-def _parse_phylip_dm_raw(fh):
-    """Raw parser for PHYLIP formatted distance matrix files.
-
-    Returns a list of raw (dists, id) values."""
-    # File should have a single header on the first line.
+def _parse_phylip_dm_raw(fh, strict=False):
+    """Raw parser for PHYLIP formatted distance matrix files."""
     try:
         header = next(_line_generator(fh))
     except StopIteration:
         raise PhylipFormatError("This file is empty.")
     n_seqs = _validate_header(header)
 
-    # All following lines should be ID+distances. No blank lines are allowed.
     n_dists = 0
     data = []
+    lengths = []
+
     for line in _line_generator(fh):
-        data.append(_validate_line(line, n_seqs, n_dists))
+        data_, length = _validate_line(line, n_seqs, n_dists, strict=strict)
+        data.append(data_)
+        lengths.append(length)
         n_dists += 1
+
     if len(data) != n_seqs:
         raise PhylipFormatError(
             f"The number of sequences is not {n_seqs} as specified in the header."
         )
+
+    # Validate pattern consistency
+    if len(lengths) >= 3:  # Only check if we have enough data
+        is_square = all(L == lengths[0] for L in lengths)
+        is_lower_tri = all(lengths[i] == i for i in range(len(lengths)))
+
+        if not (is_square or is_lower_tri):
+            raise PhylipFormatError(
+                f"Inconsistent distance counts detected: {lengths}. "
+                f"This may indicate that sequence IDs contain numeric characters "
+                f"being parsed as distances in strict format. "
+                f"Expected either all {n_seqs} (square) or 0,1,2,... "
+                f"(lower triangular)."
+            )
+
     return data
--- skbio/io/format/tests/test_phylip_dm.py
+++ skbio/io/format/tests/test_phylip_dm.py
@@ -8,35 +8,49 @@
 
 import unittest
 import io
+import tempfile
 
 import numpy as np
 
 from skbio.util import get_data_path
-from skbio.io.format.phylip_dm import _matrix_to_phylip, _phylip_dm_sniffer, _phylip_dm_to_distance_matrix
+from skbio.io.format.phylip_dm import (
+    _matrix_to_phylip,
+    _phylip_dm_sniffer,
+    _phylip_dm_to_distance_matrix,
+    _parse_phylip_dm_raw,
+)
+from skbio.io import PhylipFormatError
 from skbio.stats.distance import DistanceMatrix
+
 
 class TestSniffer(unittest.TestCase):
     def setUp(self):
-        self.positives = [get_data_path(e) for e in [
-            "96_lt_phylip_amazon.dist",
-            "98_lt_phylip_amazon.dist",
-            "98_sq_phylip_amazon.dist"
-        ]]
+        self.positives = [
+            get_data_path(e)
+            for e in [
+                "phylip_dm_valid_96_lt_phylip_amazon.dist",
+                "phylip_dm_valid_98_lt_phylip_amazon.dist",
+                "phylip_dm_valid_98_sq_phylip_amazon.dist",
+            ]
+        ]
 
-        self.negatives = [get_data_path(e) for e in [
-            "empty",
-            "whitespace_only",
-            "phylip_dm_invalid_empty_line_after_header.dist",
-            "phylip_dm_invalid_empty_line_before_header.dist",
-            "phylip_dm_sq_invalid_empty_line_after_header.dist",
-            "phylip_dm_sq_invalid_empty_line_before_header.dist",
-            "phylip_dm_invalid_header_too_long.dist",
-            "phylip_dm_invalid_no_header.dist",
-            "phylip_dm_invalid_wrong_number_dists.dist",
-            "phylip_dm_invalid_wrong_number_dists_sq.dist",
-            "phylip_dm_invalid_no_dists.dist",
-            "phylip_dm_invalid_zero_header.dist"
-        ]]
+        self.negatives = [
+            get_data_path(e)
+            for e in [
+                "empty",
+                "whitespace_only",
+                "phylip_dm_invalid_empty_line_after_header.dist",
+                "phylip_dm_invalid_empty_line_before_header.dist",
+                "phylip_dm_sq_invalid_empty_line_after_header.dist",
+                "phylip_dm_sq_invalid_empty_line_before_header.dist",
+                "phylip_dm_invalid_header_too_long.dist",
+                "phylip_dm_invalid_no_header.dist",
+                "phylip_dm_invalid_wrong_number_dists.dist",
+                "phylip_dm_invalid_wrong_number_dists_sq.dist",
+                "phylip_dm_invalid_no_dists.dist",
+                "phylip_dm_invalid_zero_header.dist",
+            ]
+        ]
 
     def test_positives(self):
         for fp in self.positives:
@@ -51,31 +65,47 @@ class TestReaders(unittest.TestCase):
     def setUp(self):
         self.expected_square_matrix = np.loadtxt(get_data_path("dm_raw_data.txt"))
 
-        self.positive_fps = [get_data_path(e) for e in [
-            # "96_lt_phylip_amazon.dist",
-            "98_lt_phylip_amazon.dist",
-            "98_sq_phylip_amazon.dist"
-        ]]
+        self.positive_fps = [
+            get_data_path(e)
+            for e in [
+                "phylip_dm_valid_98_lt_phylip_amazon.dist",
+                "phylip_dm_valid_98_sq_phylip_amazon.dist",
+            ]
+        ]
 
     def test_phylip_dm_to_distance_matrix_valid_files(self):
         for fp in self.positive_fps:
             dm = _phylip_dm_to_distance_matrix(fp)
             self.assertTrue((dm.data == self.expected_square_matrix).all())
 
+    def test_error_empty_file(self):
+        with self.assertRaises(PhylipFormatError) as e:
+            with open(get_data_path("empty"), "r") as f:
+                data = _parse_phylip_dm_raw(f)
+        self.assertEqual(str(e.exception), "This file is empty.")
+
+    def test_error_too_many_seqs(self):
+        with self.assertRaises(PhylipFormatError) as e:
+            with open(get_data_path("phylip_dm_invalid_too_many_seqs.dist"), "r") as f:
+                data = _parse_phylip_dm_raw(f)
+        self.assertEqual(
+            str(e.exception),
+            "The number of sequences is not 8 as specified in the header.",
+        )
+
 
 class TestWriters(unittest.TestCase):
     def setUp(self):
-        simple_dm = DistanceMatrix([[0, 1, 2, 3],
-                                    [1, 0, 4, 5],
-                                    [2, 4, 0, 6],
-                                    [3, 5, 6, 0]])
+        simple_dm = DistanceMatrix(
+            [[0, 1, 2, 3], [1, 0, 4, 5], [2, 4, 0, 6], [3, 5, 6, 0]]
+        )
         simple_dm_condensed = DistanceMatrix([1, 2, 3, 4, 5, 6], condensed=True)
-        
+
         self.objs = [simple_dm, simple_dm_condensed]
-        self.fps = [get_data_path("phylip_dm_simple_sq.dist"),
-                    get_data_path("phylip_dm_simple_lt.dist")]
-
-
+        self.fps = [
+            get_data_path("phylip_dm_simple_sq.dist"),
+            get_data_path("phylip_dm_simple_lt.dist"),
+        ]
 
     def test_write(self):
         for fp, obj in zip(self.fps, self.objs):
@@ -87,6 +117,28 @@ class TestWriters(unittest.TestCase):
                 exp = fh.read()
 
             self.assertEqual(obs, exp)
+
+    def test_decorator_write(self):
+        for fp, obj in zip(self.fps, self.objs):
+            with tempfile.TemporaryFile(mode="w+t") as fh:
+                obj.write(fh, format="phylip_dm")
+                fh.seek(0)
+                obs = fh.read()
+            with io.open(fp) as fh:
+                exp = fh.read()
+
+            self.assertEqual(obs, exp)
+
+    def test_not_enough_sequences(self):
+        with self.assertRaises(PhylipFormatError) as e:
+            fh = io.StringIO()
+            _matrix_to_phylip(DistanceMatrix([]), fh, "\t")
+            fh.close()
+        self.assertEqual(
+            str(e.exception),
+            "DistanceMatrix can only be written in PHYLIP format if there are at least"
+            " two samples in the matrix.",
+        )
 
 
 if __name__ == "__main__":
